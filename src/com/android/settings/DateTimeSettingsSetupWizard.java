f7f47d001b2e7faf9aacfcfe546bd209482149b8
Ben Clark <benbread@gmail.com>
2010-07-21 21:44:59 +0100

    Removed unused import android.widget.LinearLayout
    Change-Id: I904f5e3a2054f99bbc7c685e9d7a805cb621819a

--- src/com/android/settings/DateTimeSettingsSetupWizard.java
+++ src/com/android/settings/DateTimeSettingsSetupWizard.java
@@ -20,7 +20,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
-import android.widget.LinearLayout;
 
 public class DateTimeSettingsSetupWizard extends DateTimeSettings implements OnClickListener {
     private View mNextButton;
