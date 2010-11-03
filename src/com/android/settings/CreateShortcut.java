de64df5fb9fbd148de66f86be42814182316439d
Amith Yamasani <yamasani@google.com>
2010-11-03 12:01:58 -0700

    Updated visual look for Shortcuts selection.

--- src/com/android/settings/CreateShortcut.java
+++ src/com/android/settings/CreateShortcut.java
@@ -1,19 +1,39 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.settings;
 
 import android.app.LauncherActivity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 
 public class CreateShortcut extends LauncherActivity {
-    @Override protected Intent getTargetIntent() {
+
+    @Override
+    protected Intent getTargetIntent() {
         Intent targetIntent = new Intent(Intent.ACTION_MAIN, null);
         targetIntent.addCategory("com.android.settings.SHORTCUT");
         targetIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         return targetIntent;
     }
-    
-    @Override protected void onListItemClick(ListView l, View v, int position, long id) {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
         Intent shortcutIntent = intentForPosition(position);
         shortcutIntent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
         Intent intent = new Intent();
@@ -23,5 +43,10 @@ public class CreateShortcut extends LauncherActivity {
         intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, itemForPosition(position).label);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    @Override
+    protected boolean onEvaluateShowIcons() {
+        return false;
     }
 }
