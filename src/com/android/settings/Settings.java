fe0be7fcbfe4937133fe169e2f8d823dee7137ac
Daisuke Miyakawa <dmiyakawa@google.com>
2011-06-28 15:58:34 -0700

    Remove call settings menu
    It will be shown in Contacts app instead
    
    Bug: 4971637
    Change-Id: I081708d4b2cf296d0e37dd20f6ac82f37438d9f0

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -305,9 +305,6 @@ public class Settings extends PreferenceActivity implements ButtonBarHandler {
                     target.remove(header);
             } else if (id == R.id.operator_settings || id == R.id.manufacturer_settings) {
                 Utils.updateHeaderToSpecificActivityFromMetaDataOrRemove(this, target, header);
-            } else if (id == R.id.call_settings) {
-                if (!Utils.isVoiceCapable(this))
-                    target.remove(header);
             } else if (id == R.id.wifi_settings) {
                 // Remove WiFi Settings if WiFi service is not available.
                 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI)) {
