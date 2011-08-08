c1eab91f073313d84e67b4626f008aa3520c9f3a
Gilles Debunne <debunne@google.com>
2011-08-08 11:54:27 -0700

    Wifi state reset when disconnected
    https://review.source.android.com/19086
    
    Change-Id: Ie7367f2240b94569fed3120244f1cb268bb09206

--- src/com/android/settings/wifi/WifiSettings.java
+++ src/com/android/settings/wifi/WifiSettings.java
@@ -615,6 +615,8 @@ public class WifiSettings extends SettingsPreferenceFragment
                 break;
         }
 
+        mLastInfo = null;
+        mLastState = null;
         mScanner.pause();
     }
 
