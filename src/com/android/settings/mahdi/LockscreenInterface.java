b8121f8c11b21acbe63e8c014dece0ca0e4706fb
blunden <blunden2@gmail.com>
2012-07-30 15:31:21 +0200

    Settings: Add option to always show battery status on lockscreen (2/2)
    Based on the feature by burnsra in CM7
    Authored by blunden
    Tweaked by dvtonder
    Change-Id: I1c8c8eef3d0c913c621f735e7d5e395c8768fe76
    
    lockscreen battery status: add off option (1/2)
    Change-Id: I06dd1b81826bdb1c1e35b72d99970f97dcad1a34
    
    Conflicts:
    
    	res/values/cm_arrays.xml
    	res/values/cm_strings.xml
    	res/xml/lockscreen_interface_settings.xml
    	src/com/android/settings/cyanogenmod/LockscreenInterface.java

--- src/com/android/settings/mahdi/LockscreenInterface.java
+++ src/com/android/settings/mahdi/LockscreenInterface.java
@@ -44,24 +44,24 @@ import java.io.File;
 import java.io.IOException;
 
 public class LockscreenInterface extends SettingsPreferenceFragment implements
-OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "LockscreenInterface";
 
     private static final String LOCKSCREEN_SHORTCUTS_CATEGORY = "lockscreen_shortcuts_category";
     private static final String KEY_ADDITIONAL_OPTIONS = "options_group";
+    private static final String KEY_BATTERY_STATUS = "lockscreen_battery_status";
     private static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
     private static final String KEY_LOCKSCREEN_BUTTONS = "lockscreen_buttons";
         
     private PreferenceCategory mAdditionalOptions;
+    private ListPreference mBatteryStatus;
     private CheckBoxPreference mLockRingBattery;
 
     private boolean mCheckPreferences;
 
     private Activity mActivity;
     private ContentResolver mResolver;
-    private File wallpaperImage;
-    private File wallpaperTemporary;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -89,14 +89,28 @@ OnPreferenceChangeListener {
         mAdditionalOptions = (PreferenceCategory) 
                 prefs.findPreference(KEY_ADDITIONAL_OPTIONS);
 
-    // Remove lockscreen button actions if device doesn't have hardware keys
-        if (!hasButtons()) {
-            generalCategory.removePreference(findPreference(KEY_LOCKSCREEN_BUTTONS));
+        mBatteryStatus = (ListPreference) findPreference(KEY_BATTERY_STATUS);
+        if (mBatteryStatus != null) {
+            mBatteryStatus.setOnPreferenceChangeListener(this);
         }                
 
         mLockRingBattery = (CheckBoxPreference)findPreference(BATTERY_AROUND_LOCKSCREEN_RING);
         mLockRingBattery.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.BATTERY_AROUND_LOCKSCREEN_RING, 0) == 1);
+
+    // Remove lockscreen button actions if device doesn't have hardware keys
+        if (!hasButtons()) {
+            generalCategory.removePreference(findPreference(KEY_LOCKSCREEN_BUTTONS));
+        }
+
+        // Update battery status
+        if (mBatteryStatus != null) {
+            ContentResolver cr = getActivity().getContentResolver();
+            int batteryStatus = Settings.System.getInt(cr,
+                    Settings.System.LOCKSCREEN_BATTERY_VISIBILITY, 0);
+            mBatteryStatus.setValueIndex(batteryStatus);
+            mBatteryStatus.setSummary(mBatteryStatus.getEntries()[batteryStatus]);
+        }
                 
         final int unsecureUnlockMethod = Settings.Secure.getInt(getActivity().getContentResolver(),
                 Settings.Secure.LOCKSCREEN_UNSECURE_USED, 1);
@@ -140,7 +154,15 @@ OnPreferenceChangeListener {
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (!mCheckPreferences) {
+        ContentResolver cr = getActivity().getContentResolver();
+
+        if (preference == mBatteryStatus) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mBatteryStatus.findIndexOfValue((String) objValue);
+            Settings.System.putInt(cr, Settings.System.LOCKSCREEN_BATTERY_VISIBILITY, value);
+            mBatteryStatus.setSummary(mBatteryStatus.getEntries()[index]);
+            return true;
+        } else if (!mCheckPreferences) {
         return false;
         }
      return true;
