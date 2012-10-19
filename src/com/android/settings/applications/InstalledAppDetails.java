aa214801096d025e0e5de19f7416de5cfeac6f30
Zhijun Peng <pengzhj@marvell.com>
2012-10-19 11:38:25 +0800

    Settings: Fix the memory leak in Settings
    Fixed the memory leak in both InstalledAppDetails
    
    Change-Id: I3520b3c1fe1bf98e3baec2ee989eb215164a7cf2
    Signed-off-by: Zhijun Peng <pengzhj@marvell.com>

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -451,6 +451,12 @@ public class InstalledAppDetails extends Fragment
     }
 
     @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mSession.release();
+    }
+
+    @Override
     public void onAllSizesComputed() {
     }
 
