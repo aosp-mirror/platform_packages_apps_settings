bdc0024a6c49fa4ca46b6bfd02fc7f11e184ece5
jt1134 <jt1134@gmail.com>
2012-11-24 23:10:33 -0600

    Settings: add Advanced menu for device-specific settings app
    Change-Id: Ib022a224fe71522eb2432a112d2bdf64bc08c926
    
    Conflicts:
    	res/values/strings.xml
    
    Conflicts:
    
    	res/values/strings.xml
    	res/xml/settings_headers.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -553,7 +553,8 @@ public class Settings extends PreferenceActivity
             Header header = target.get(i);
             // Ids are integers, so downcasting
             int id = (int) header.id;
-            if (id == R.id.operator_settings || id == R.id.manufacturer_settings || id == R.id.superuser_settings) {
+            if (id == R.id.operator_settings || id == R.id.manufacturer_settings ||
+                    id == R.id.advanced_settings || id == R.id.superuser_settings) {
                 Utils.updateHeaderToSpecificActivityFromMetaDataOrRemove(this, target, header);		
             } else if (id == R.id.wifi_settings) {
                 // Remove WiFi Settings if WiFi service is not available.
