7dee75c424d13fc7518d6ecc31a5bfd4b466b78b
Daniel Sandler <dsandler@android.com>
2012-11-27 22:41:30 -0500

    Add android.settings.DREAM_SETTINGS for Daydream settings.
    Bug: 7480927
    Change-Id: I5777f7c4aec9ad7d7eb9f7eeb882d2342cb481c3

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -833,4 +833,5 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class DreamSettingsActivity extends Settings { /* empty */ }
 }
