4166ec39ecd8a2066f4426e1b359a0b1f95af05e
Jeff Sharkey <jsharkey@android.com>
2012-11-28 17:41:16 -0800

    Use uniform font size for APNs and timezones. DO NOT MERGE.
    Also get rid of some orange highlight!
    
    Bug: 7632945
    Change-Id: I6a4b59f4eef81b128d128f5a429fb96f6366843a

--- src/com/android/settings/ZonePicker.java
+++ src/com/android/settings/ZonePicker.java
@@ -81,7 +81,7 @@ public class ZonePicker extends ListFragment {
     public static SimpleAdapter constructTimezoneAdapter(Context context,
             boolean sortedByName) {
         return constructTimezoneAdapter(context, sortedByName,
-                android.R.layout.simple_list_item_2);
+                R.layout.date_time_setup_custom_list_item_2);
     }
 
     /**
