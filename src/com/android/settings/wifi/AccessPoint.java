bbb4e26415195330d6b7dba193eccd5dd268b78c
Deepthi Gowri <deepthi@codeaurora.org>
2013-06-18 12:40:58 +0530

    WifiSettings: Handle DISABLED_ASSOCIATION_REJECT.
    Upon receiving multiple association rejects for a network,
    framework disables corresponding network providing reason
    code as DISABLE_ASSOCIATION_REJECT. this is handled
    in order to provide appropriate user experience.
    
    CRs-fixed: 484247
    Change-Id: I3d344068a58d2dfd82c9bc8b23f43fdf980cc4d9

--- src/com/android/settings/wifi/AccessPoint.java
+++ src/com/android/settings/wifi/AccessPoint.java
@@ -348,6 +348,9 @@ class AccessPoint extends Preference {
                 case WifiConfiguration.DISABLED_DNS_FAILURE:
                     setSummary(context.getString(R.string.wifi_disabled_network_failure));
                     break;
+                case WifiConfiguration.DISABLED_ASSOCIATION_REJECT:
+                    setSummary(context.getString(R.string.wifi_disabled_association_rejected));
+                    break;
                 case WifiConfiguration.DISABLED_UNKNOWN_REASON:
                     setSummary(context.getString(R.string.wifi_disabled_generic));
             }
