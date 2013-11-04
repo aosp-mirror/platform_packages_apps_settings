463aa2aaa5e9304bd7e4ffaed8cda26731a84824
Mahdi-Rom <mahdi.rom@gmail.com>
2013-11-04 19:18:26 +0000

    add Customization Settings Menu

---
+++ src/com/android/settings/mahdi/CustomizationSettings.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2012 The Mahdi Rom project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mahdi;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.IWindowManager;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class CustomizationSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "CustomizationSettings";      
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.customization_settings);                
+    }    
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
