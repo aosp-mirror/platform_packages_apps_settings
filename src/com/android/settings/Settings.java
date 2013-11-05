szunyi77/android_packages_apps_Settings

add Cyanogenmod Profiles (2/2)
Conflicts:

	AndroidManifest.xml
	res/values/mahdi_arrays.xml
	res/values/mahdi_strings.xml
	src/com/android/settings/Settings.java

src/com/android/settings/Settings.java
@@ -61,6 +61,10 @@ import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.wifi.WifiEnabler;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.profiles.AppGroupConfig;
+import com.android.settings.profiles.ProfileConfig;
+import com.android.settings.profiles.ProfilesSettings;
+import com.android.settings.profiles.ProfileEnabler;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -387,6 +391,9 @@ public class Settings extends PreferenceActivity
                 WifiDisplaySettings.class.getName().equals(fragmentName) ||
                 BluetoothSettings.class.getName().equals(fragmentName) ||
                 DreamSettings.class.getName().equals(fragmentName) ||
+		ProfilesSettings.class.getName().equals(fragmentName) ||
+                ProfileConfig.class.getName().equals(fragmentName) ||
+                AppGroupConfig.class.getName().equals(fragmentName) ||
                 ToggleAccessibilityServicePreferenceFragment.class.getName().equals(fragmentName)) {
             intent.putExtra(EXTRA_UI_OPTIONS, ActivityInfo.UIOPTION_SPLIT_ACTION_BAR_WHEN_NARROW);
         }
@@ -580,6 +587,7 @@ public class Settings extends PreferenceActivity
 
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
+	private final ProfileEnabler mProfileEnabler;
         private AuthenticatorHelper mAuthHelper;
 
         private static class HeaderViewHolder {
@@ -594,7 +602,9 @@ public class Settings extends PreferenceActivity
         static int getHeaderType(Header header) {
             if (header.fragment == null && header.intent == null) {
                 return HEADER_TYPE_CATEGORY;
-            } else if (header.id == R.id.wifi_settings || header.id == R.id.bluetooth_settings) {
+            } else if (header.id == R.id.wifi_settings
+                     || header.id == R.id.bluetooth_settings
+                     || header.id == R.id.profiles_settings) {
                 return HEADER_TYPE_SWITCH;
             } else {
                 return HEADER_TYPE_NORMAL;
@@ -638,6 +648,7 @@ public class Settings extends PreferenceActivity
             // Switches inflated from their layouts. Must be done before adapter is set in super
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
+	    mProfileEnabler = new ProfileEnabler(context, new Switch(context));
         }
 
         @Override
@@ -694,8 +705,10 @@ public class Settings extends PreferenceActivity
                     // Would need a different treatment if the main menu had more switches
                     if (header.id == R.id.wifi_settings) {
                         mWifiEnabler.setSwitch(holder.switch_);
-                    } else {
+                    } else if (header.id == R.id.bluetooth_settings) {
                         mBluetoothEnabler.setSwitch(holder.switch_);
+		    } else if (header.id == R.id.profiles_settings) {
+                        mProfileEnabler.setSwitch(holder.switch_);
                     }
                     // No break, fall through on purpose to update common fields
 
@@ -732,11 +745,13 @@ public class Settings extends PreferenceActivity
         public void resume() {
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
+	    mProfileEnabler.resume();
         }
 
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
+	    mProfileEnabler.pause();
         }
     }
 
@@ -841,5 +856,5 @@ public class Settings extends PreferenceActivity
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class NotificationStationActivity extends Settings { /* empty */ }
     public static class UserSettingsActivity extends Settings { /* empty */ }
-    public static class NotificationAccessSettingsActivity extends Settings { /* empty */ }
+    public static class NotificationAccessSettingsActivity extends Settings { /* empty */ }    
 }

src/com/android/settings/profiles/TriggersFragment.java
@@ -1,0 +1,229 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.profiles;
+
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Profile;
+import android.app.Profile.ProfileTrigger;
+import android.app.Profile.TriggerType;
+import android.app.ProfileManager;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Settings Preference to configure triggers to switch profiles base on Wi-Fi events
+ */
+public class TriggersFragment extends SettingsPreferenceFragment implements ActionBar.OnNavigationListener {
+    private Profile mProfile;
+    private Preference mSelectedTrigger;
+    private ProfileManager mProfileManager;
+    private WifiManager mWifiManager;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private int mTriggerFilter = 0;
+    private static final int WIFI_TRIGGER = 1;
+    private static final int BT_TRIGGER = 2;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mProfileManager = (ProfileManager) getSystemService(Context.PROFILE_SERVICE);
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();;
+        addPreferencesFromResource(R.xml.wifi_settings);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadPreferences();
+        loadActionBar();
+    }
+
+    private void loadActionBar() {
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        Resources res = getResources();
+
+        String[] navItems = res.getStringArray(R.array.profile_trigger_filters);
+        ArrayAdapter<String> navAdapter = new ArrayAdapter<String>(
+                actionBar.getThemedContext(), android.R.layout.simple_list_item_1, navItems);
+
+        // Set up the dropdown list navigation in the action bar.
+        actionBar.setListNavigationCallbacks(navAdapter, this);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP
+                | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            mProfile = args.getParcelable("profile");
+        }
+    }
+
+    private void initPreference(AbstractTriggerPreference pref, int state, Resources res, int icon) {
+        String summary = res.getStringArray(R.array.profile_trigger_wifi_options)[state];
+        pref.setSummary(summary);
+        pref.setTriggerState(state);
+        pref.setIcon(icon);
+    }
+
+    private void loadPreferences() {
+        final List<WifiConfiguration> configs = mWifiManager.getConfiguredNetworks();
+        final Resources res = getResources();
+        final List<AbstractTriggerPreference> prefs = new ArrayList<AbstractTriggerPreference>();
+
+        getPreferenceScreen().removeAll();
+
+        if (mTriggerFilter == WIFI_TRIGGER || mTriggerFilter == 0) {
+            if (configs != null ) {
+                for (WifiConfiguration config : configs) {
+                    WifiTriggerAPPreference accessPoint =
+                            new WifiTriggerAPPreference(getActivity(), config);
+                    int state = mProfile.getTrigger(Profile.TriggerType.WIFI, accessPoint.getSSID());
+                    initPreference(accessPoint, state, res, R.drawable.ic_wifi_signal_4);
+                    prefs.add(accessPoint);
+                }
+            } else {
+                final List<ProfileTrigger> triggers = mProfile.getTriggersFromType(TriggerType.WIFI);
+                for (ProfileTrigger trigger : triggers) {
+                    WifiTriggerAPPreference accessPoint =
+                            new WifiTriggerAPPreference(getActivity(), trigger.getName());
+                    initPreference(accessPoint, trigger.getState(), res, R.drawable.ic_wifi_signal_4);
+                    prefs.add(accessPoint);
+                }
+            }
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (mTriggerFilter == BT_TRIGGER || mTriggerFilter == 0) {
+            if (!pairedDevices.isEmpty()) {
+                for (BluetoothDevice device : pairedDevices) {
+                    BluetoothTriggerPreference bt =
+                            new BluetoothTriggerPreference(getActivity(), device);
+                    int state = mProfile.getTrigger(Profile.TriggerType.BLUETOOTH, bt.getAddress());
+                    initPreference(bt, state, res, R.drawable.ic_settings_bluetooth2);
+                    prefs.add(bt);
+                }
+            } else {
+                final List<ProfileTrigger> triggers = mProfile.getTriggersFromType(TriggerType.BLUETOOTH);
+                for (ProfileTrigger trigger : triggers) {
+                    BluetoothTriggerPreference bt = new BluetoothTriggerPreference(getActivity(),
+                            trigger.getName(), trigger.getId());
+                    initPreference(bt, trigger.getState(), res, R.drawable.ic_settings_bluetooth2);
+                    prefs.add(bt);
+                }
+            }
+        }
+
+        Collections.sort(prefs, new Comparator<AbstractTriggerPreference>() {
+            @Override
+            public int compare(AbstractTriggerPreference o1, AbstractTriggerPreference o2) {
+                if (o1.getTriggerState() == o2.getTriggerState()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.getTriggerState() < o2.getTriggerState() ? -1 : 1;
+            }
+        });
+        for (Preference pref: prefs) {
+            getPreferenceScreen().addPreference(pref);
+        }
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen screen, Preference preference) {
+        mSelectedTrigger = preference;
+        if (preference instanceof WifiTriggerAPPreference) {
+            showDialog(WIFI_TRIGGER);
+            return true;
+        } else if (preference instanceof BluetoothTriggerPreference) {
+            showDialog(BT_TRIGGER);
+            return true;
+        }
+        return super.onPreferenceTreeClick(screen, preference);
+    }
+
+    @Override
+    public Dialog onCreateDialog(final int dialogId) {
+        final String id;
+        final String triggerName = mSelectedTrigger.getTitle().toString();
+        final int triggerType;
+
+        switch (dialogId) {
+            case WIFI_TRIGGER:
+                WifiTriggerAPPreference pref = (WifiTriggerAPPreference) mSelectedTrigger;
+                id = pref.getSSID();
+                triggerType = Profile.TriggerType.WIFI;
+                break;
+            case BT_TRIGGER:
+                BluetoothTriggerPreference btpref = (BluetoothTriggerPreference) mSelectedTrigger;
+                id = btpref.getAddress();
+                triggerType = Profile.TriggerType.BLUETOOTH;
+                break;
+            default:
+                return super.onCreateDialog(dialogId);
+        }
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.profile_trigger_configure)
+                .setSingleChoiceItems(R.array.profile_trigger_wifi_options,
+                        mProfile.getTrigger(triggerType, id),
+                        new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mProfile.setTrigger(triggerType, id, which, triggerName);
+                        mProfileManager.updateProfile(mProfile);
+                        loadPreferences();
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        mTriggerFilter = itemPosition;
+        loadPreferences();
+        return true;
+    }
+}

aosp-mirror/platform_packages_apps_settings

	res/values/styles.xml

src/com/android/settings/Settings.java
@@ -92,6 +92,10 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.WifiEnabler;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.profiles.AppGroupConfig;
+import com.android.settings.profiles.ProfileConfig;
+import com.android.settings.profiles.ProfilesSettings;
+import com.android.settings.profiles.ProfileEnabler;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -509,6 +513,9 @@ public class Settings extends PreferenceActivity
                 WifiDisplaySettings.class.getName().equals(fragmentName) ||
                 BluetoothSettings.class.getName().equals(fragmentName) ||
                 DreamSettings.class.getName().equals(fragmentName) ||
+		ProfilesSettings.class.getName().equals(fragmentName) ||
+                ProfileConfig.class.getName().equals(fragmentName) ||
+                AppGroupConfig.class.getName().equals(fragmentName) ||
                 LocationSettings.class.getName().equals(fragmentName) ||
                 ToggleAccessibilityServicePreferenceFragment.class.getName().equals(fragmentName) ||
                 PrintSettingsFragment.class.getName().equals(fragmentName) ||
@@ -784,6 +791,7 @@ public class Settings extends PreferenceActivity
         private final BluetoothEnabler mBluetoothEnabler;
         private AuthenticatorHelper mAuthHelper;
         private DevicePolicyManager mDevicePolicyManager;
+	private final ProfileEnabler mProfileEnabler;
 
         private static class HeaderViewHolder {
             ImageView icon;
@@ -799,7 +807,9 @@ public class Settings extends PreferenceActivity
         static int getHeaderType(Header header) {
             if (header.fragment == null && header.intent == null) {
                 return HEADER_TYPE_CATEGORY;
-            } else if (header.id == R.id.wifi_settings || header.id == R.id.bluetooth_settings) {
+            } else if (header.id == R.id.wifi_settings
+                     || header.id == R.id.bluetooth_settings
+                     || header.id == R.id.profiles_settings) {
                 return HEADER_TYPE_SWITCH;
             } else if (header.id == R.id.security_settings) {
                 return HEADER_TYPE_BUTTON;
@@ -846,6 +856,7 @@ public class Settings extends PreferenceActivity
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
             mDevicePolicyManager = dpm;
+	    mProfileEnabler = new ProfileEnabler(context, new Switch(context));
         }
 
         @Override
@@ -914,8 +925,10 @@ public class Settings extends PreferenceActivity
                     // Would need a different treatment if the main menu had more switches
                     if (header.id == R.id.wifi_settings) {
                         mWifiEnabler.setSwitch(holder.switch_);
-                    } else {
+                    } else if (header.id == R.id.bluetooth_settings) {
                         mBluetoothEnabler.setSwitch(holder.switch_);
+		    } else if (header.id == R.id.profiles_settings) {
+                        mProfileEnabler.setSwitch(holder.switch_);
                     }
                     updateCommonHeaderView(header, holder);
                     break;
@@ -989,11 +1002,13 @@ public class Settings extends PreferenceActivity
         public void resume() {
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
+	    mProfileEnabler.resume();
         }
 
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
+	    mProfileEnabler.pause();
         }
     }
 
@@ -1111,5 +1126,5 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
-    public static class PowerMenuSettingsActivity extends Settings { /* empty */ }
+    public static class PowerMenuSettingsActivity extends Settings { /* empty */ }     
 }

src/com/android/settings/profiles/TriggersFragment.java
@@ -1,0 +1,229 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.profiles;
+
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Profile;
+import android.app.Profile.ProfileTrigger;
+import android.app.Profile.TriggerType;
+import android.app.ProfileManager;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Settings Preference to configure triggers to switch profiles base on Wi-Fi events
+ */
+public class TriggersFragment extends SettingsPreferenceFragment implements ActionBar.OnNavigationListener {
+    private Profile mProfile;
+    private Preference mSelectedTrigger;
+    private ProfileManager mProfileManager;
+    private WifiManager mWifiManager;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private int mTriggerFilter = 0;
+    private static final int WIFI_TRIGGER = 1;
+    private static final int BT_TRIGGER = 2;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mProfileManager = (ProfileManager) getSystemService(Context.PROFILE_SERVICE);
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();;
+        addPreferencesFromResource(R.xml.wifi_settings);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadPreferences();
+        loadActionBar();
+    }
+
+    private void loadActionBar() {
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        Resources res = getResources();
+
+        String[] navItems = res.getStringArray(R.array.profile_trigger_filters);
+        ArrayAdapter<String> navAdapter = new ArrayAdapter<String>(
+                actionBar.getThemedContext(), android.R.layout.simple_list_item_1, navItems);
+
+        // Set up the dropdown list navigation in the action bar.
+        actionBar.setListNavigationCallbacks(navAdapter, this);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP
+                | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            mProfile = args.getParcelable("profile");
+        }
+    }
+
+    private void initPreference(AbstractTriggerPreference pref, int state, Resources res, int icon) {
+        String summary = res.getStringArray(R.array.profile_trigger_wifi_options)[state];
+        pref.setSummary(summary);
+        pref.setTriggerState(state);
+        pref.setIcon(icon);
+    }
+
+    private void loadPreferences() {
+        final List<WifiConfiguration> configs = mWifiManager.getConfiguredNetworks();
+        final Resources res = getResources();
+        final List<AbstractTriggerPreference> prefs = new ArrayList<AbstractTriggerPreference>();
+
+        getPreferenceScreen().removeAll();
+
+        if (mTriggerFilter == WIFI_TRIGGER || mTriggerFilter == 0) {
+            if (configs != null ) {
+                for (WifiConfiguration config : configs) {
+                    WifiTriggerAPPreference accessPoint =
+                            new WifiTriggerAPPreference(getActivity(), config);
+                    int state = mProfile.getTrigger(Profile.TriggerType.WIFI, accessPoint.getSSID());
+                    initPreference(accessPoint, state, res, R.drawable.ic_wifi_signal_4_dark);
+                    prefs.add(accessPoint);
+                }
+            } else {
+                final List<ProfileTrigger> triggers = mProfile.getTriggersFromType(TriggerType.WIFI);
+                for (ProfileTrigger trigger : triggers) {
+                    WifiTriggerAPPreference accessPoint =
+                            new WifiTriggerAPPreference(getActivity(), trigger.getName());
+                    initPreference(accessPoint, trigger.getState(), res, R.drawable.ic_wifi_signal_4_dark);
+                    prefs.add(accessPoint);
+                }
+            }
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (mTriggerFilter == BT_TRIGGER || mTriggerFilter == 0) {
+            if (!pairedDevices.isEmpty()) {
+                for (BluetoothDevice device : pairedDevices) {
+                    BluetoothTriggerPreference bt =
+                            new BluetoothTriggerPreference(getActivity(), device);
+                    int state = mProfile.getTrigger(Profile.TriggerType.BLUETOOTH, bt.getAddress());
+                    initPreference(bt, state, res, R.drawable.ic_settings_bluetooth2);
+                    prefs.add(bt);
+                }
+            } else {
+                final List<ProfileTrigger> triggers = mProfile.getTriggersFromType(TriggerType.BLUETOOTH);
+                for (ProfileTrigger trigger : triggers) {
+                    BluetoothTriggerPreference bt = new BluetoothTriggerPreference(getActivity(),
+                            trigger.getName(), trigger.getId());
+                    initPreference(bt, trigger.getState(), res, R.drawable.ic_settings_bluetooth2);
+                    prefs.add(bt);
+                }
+            }
+        }
+
+        Collections.sort(prefs, new Comparator<AbstractTriggerPreference>() {
+            @Override
+            public int compare(AbstractTriggerPreference o1, AbstractTriggerPreference o2) {
+                if (o1.getTriggerState() == o2.getTriggerState()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.getTriggerState() < o2.getTriggerState() ? -1 : 1;
+            }
+        });
+        for (Preference pref: prefs) {
+            getPreferenceScreen().addPreference(pref);
+        }
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen screen, Preference preference) {
+        mSelectedTrigger = preference;
+        if (preference instanceof WifiTriggerAPPreference) {
+            showDialog(WIFI_TRIGGER);
+            return true;
+        } else if (preference instanceof BluetoothTriggerPreference) {
+            showDialog(BT_TRIGGER);
+            return true;
+        }
+        return super.onPreferenceTreeClick(screen, preference);
+    }
+
+    @Override
+    public Dialog onCreateDialog(final int dialogId) {
+        final String id;
+        final String triggerName = mSelectedTrigger.getTitle().toString();
+        final int triggerType;
+
+        switch (dialogId) {
+            case WIFI_TRIGGER:
+                WifiTriggerAPPreference pref = (WifiTriggerAPPreference) mSelectedTrigger;
+                id = pref.getSSID();
+                triggerType = Profile.TriggerType.WIFI;
+                break;
+            case BT_TRIGGER:
+                BluetoothTriggerPreference btpref = (BluetoothTriggerPreference) mSelectedTrigger;
+                id = btpref.getAddress();
+                triggerType = Profile.TriggerType.BLUETOOTH;
+                break;
+            default:
+                return super.onCreateDialog(dialogId);
+        }
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.profile_trigger_configure)
+                .setSingleChoiceItems(R.array.profile_trigger_wifi_options,
+                        mProfile.getTrigger(triggerType, id),
+                        new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mProfile.setTrigger(triggerType, id, which, triggerName);
+                        mProfileManager.updateProfile(mProfile);
+                        loadPreferences();
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        mTriggerFilter = itemPosition;
+        loadPreferences();
+        return true;
+    }
+}