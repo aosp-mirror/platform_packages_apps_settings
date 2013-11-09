a02153f9b77875e6f4ba334342bf64a1762fbf7c
Mahdi-Rom <mahdi.rom@gmail.com>
2013-11-09 20:11:55 +0000

    Settings: Add bridge to PerformanceControl

--- Android.mk
+++ Android.mk
@@ -6,9 +6,16 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := \
-        $(call all-java-files-under, src) \
-        src/com/android/settings/EventLogTags.logtags
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../PerformanceControl/src)
+
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += packages/apps/PerformanceControl/res
+
+LOCAL_ASSET_DIR += packages/apps/PerformanceControl/assets
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages com.brewcrewfoo.performance
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -610,6 +610,10 @@ public class Settings extends PreferenceActivity
                 if (!showDev) {
                     target.remove(i);
                 }
+	    } else if (id == R.id.performance_controls) {
+                if (!showDev) {
+                    target.remove(i);
+                }
             } else if (id == R.id.account_add) {
                 if (um.hasUserRestriction(UserManager.DISALLOW_MODIFY_ACCOUNTS)) {
                     target.remove(i);
@@ -1130,5 +1134,6 @@ public class Settings extends PreferenceActivity
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
     public static class PowerMenuSettingsActivity extends Settings { /* empty */ }
-    public static class BlacklistSettingsActivity extends Settings { /* empty */ }     
+    public static class BlacklistSettingsActivity extends Settings { /* empty */ }
+    public static class PerformanceSettingsActivity extends Settings { /* empty */ }     
 }
