d683ca22509d3fd9e1a9fd66ee7c428d85aed01c
Mahdi-Rom <mahdi.rom@gmail.com>
2013-11-10 14:31:03 +0000

    Android.mk: this should not have been removed

--- Android.mk
+++ Android.mk
@@ -6,8 +6,10 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += $(call all-java-files-under, ../PerformanceControl/src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src) \
+src/com/android/settings/EventLogTags.logtags
+
+LOCAL_SRC_FILES += $(call all-java-files-under, ../PerformanceControl/src) 
 
 LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 LOCAL_RESOURCE_DIR += packages/apps/PerformanceControl/res
