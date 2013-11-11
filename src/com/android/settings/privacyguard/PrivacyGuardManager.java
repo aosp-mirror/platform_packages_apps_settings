721f258418c26963da0c8452a42fafa672fde72c
Sam Mortimer <sam@mortimer.me.uk>
2013-11-11 00:19:00 -0800

    AppOps: Add advanced menu link from privacyguard
    Forward port from cm-10.2
    
    Change-Id: I09b06eca34366526e5b1cd39763f31414e93a0c0
    
    Conflicts:
    
    	res/values/cm_strings.xml

--- src/com/android/settings/privacyguard/PrivacyGuardManager.java
+++ src/com/android/settings/privacyguard/PrivacyGuardManager.java
@@ -51,6 +51,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.settings.R;
+import com.android.settings.Settings.AppOpsSummaryActivity;
 import com.android.settings.applications.AppOpsDetails;
 import com.android.settings.applications.AppOpsState;
 import com.android.settings.applications.AppOpsState.OpsTemplate;
@@ -382,6 +383,11 @@ public class PrivacyGuardManager extends Fragment
                 item.setChecked(!item.isChecked());
                 mPreferences.edit().putBoolean(prefName, item.isChecked()).commit();
                 loadApps();
+                return true;
+            case R.id.advanced:
+                Intent i = new Intent(Intent.ACTION_MAIN);
+                i.setClass(mActivity, AppOpsSummaryActivity.class);
+                mActivity.startActivity(i);
                 return true;
             default:
                 return super.onContextItemSelected(item);
