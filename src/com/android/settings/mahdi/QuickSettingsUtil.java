30a8c48da3cf7fe37450132792963c8f42d57027
Mahdi-Rom <mahdi.rom@gmail.com>
2013-11-17 23:55:45 +0000

    Quicksettings: Power button tile (2/2)
    Change the 'Go to Sleep' button to perform activities
    similar to the physical power button.

--- src/com/android/settings/mahdi/QuickSettingsUtil.java
+++ src/com/android/settings/mahdi/QuickSettingsUtil.java
@@ -89,8 +89,8 @@ public class QuickSettingsUtil {
                 TILE_IMMERSIVEMODE, R.string.title_tile_immersive_mode,
                 "com.android.systemui:drawable/ic_qs_immersive_mode_off"));
         registerTile(new QuickSettingsUtil.TileInfo(
-                TILE_SLEEP, R.string.title_tile_sleep,
-                "com.android.systemui:drawable/ic_qs_sleep"));
+                TILE_SLEEP, R.string.title_tile_power,
+                "com.android.systemui:drawable/ic_qs_power"));
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_GPS, R.string.title_tile_gps,
                 "com.android.systemui:drawable/ic_qs_gps_neutral"));
