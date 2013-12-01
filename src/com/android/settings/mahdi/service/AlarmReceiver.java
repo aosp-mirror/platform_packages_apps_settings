SlimRoms/packages_apps_Settings

Settings: QuietHours AutoSMS && Bypass Alarm
AOKP kanged DarkSlim...
	An original Slim feature.

Hai Slim =]
	Have an original AOKP feature.

PS2: Whitespace
PS3: Use slim_arrays
PS4: Remove unused imports &&
	Use StateListDrawable
	for Alarm Dismiss (color-
	changing button)
PS5: moved services into slim folder to have all seperated
     minor code style corrections
     add dialog instance handling in QuietHours.java
PS6: the right class folder is a good idea sometimes :p
PS8: added cancel button to the dialogs
PS9: Holo Gray

Change-Id: Ia24450ac6aa60e74f8c93113a35d43b4602de189

src/com/android/settings/slim/service/AlarmReceiver.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private final static String TAG = "AlarmReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SmsCallHelper.scheduleService(context);
+    }
+
+}

src/com/android/settings/slim/service/AlarmService.java
@@ -1,0 +1,171 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.service;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnSeekCompleteListener;
+import android.media.MediaPlayer.OnErrorListener;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import com.android.settings.R;
+
+public class AlarmService extends Service {
+
+    private static final int NOTI_ID = 286;
+
+    private NotificationManager mManager;
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    private int mAlarmVolumeSetting;
+    private boolean mPlaying = false;
+
+    @Override
+    public void onCreate() {
+        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        mManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopAlarm();
+        mManager.cancel(NOTI_ID);
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try {
+            startAlarmSound();
+        } catch (Exception e) {
+            // Do nothing
+        }
+
+        Bundle extras = intent.getExtras();
+        String names = extras.getString("number");
+        String title = getResources().getString(
+                R.string.quiet_hours_alarm_dialog_title);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_quiethours);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setTicker(title)
+                .setContentTitle(title)
+                .setContentText(names)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_quiethours)
+                .setLargeIcon(bm)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(names + getResources().getString(
+                                R.string.quiet_hours_alarm_message)));
+
+        Intent alarmDialog = new Intent();
+        alarmDialog.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        alarmDialog.setClass(this, com.android.settings.slim.service.BypassAlarm.class);
+        alarmDialog.putExtra("number", names);
+        alarmDialog.putExtra("norun", true);
+
+        PendingIntent result = PendingIntent.getActivity(
+                this, 0, alarmDialog, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        builder.setContentIntent(result);
+        mManager.notify(NOTI_ID, builder.build());
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void startAlarmSound()
+            throws java.io.IOException, IllegalArgumentException, IllegalStateException {
+
+        Uri alertSound = SmsCallHelper.returnUserRingtone(this);
+
+        if (mPlaying) {
+            stopAlarm();
+        } else {
+            mAlarmVolumeSetting = mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+        }
+
+        if (mMediaPlayer == null) {
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setOnErrorListener(new OnErrorListener() {
+                @Override
+                public boolean onError(MediaPlayer mp, int what, int extra) {
+                    mp.stop();
+                    mp.release();
+                    mMediaPlayer = null;
+                    return true;
+                }
+            });
+        }
+
+        mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM,
+                mAudioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+
+        mMediaPlayer.setDataSource(this, alertSound);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+
+        if (SmsCallHelper.returnUserRingtoneLoop(this)) {
+            mMediaPlayer.setLooping(true);
+        } else {
+            mMediaPlayer.setLooping(false);
+            mMediaPlayer.setOnSeekCompleteListener(stopSelf);
+        }
+        mMediaPlayer.prepare();
+        mMediaPlayer.start();
+        mPlaying = true;
+    }
+
+    public void stopAlarm() {
+        if (mPlaying) {
+
+            if (mMediaPlayer != null) {
+                mMediaPlayer.stop();
+                mMediaPlayer.release();
+                mMediaPlayer = null;
+                mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM,
+                        mAlarmVolumeSetting, 0);
+            }
+
+            mPlaying = false;
+        }
+    }
+
+    final OnSeekCompleteListener stopSelf = new OnSeekCompleteListener() {
+        public void onSeekComplete(MediaPlayer mp) {
+            stopAlarm();
+        }
+    };
+}

src/com/android/settings/slim/service/BootReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SmsCallHelper.scheduleService(context);
+    }
+}

src/com/android/settings/slim/service/BypassAlarm.java
@@ -1,0 +1,131 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.service;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class BypassAlarm extends Activity {
+
+    private FrameLayout mDismissButton;
+
+    private String mNumbers;
+
+    private boolean mUserDestroy;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mUserDestroy = false;
+
+        final Window win = getWindow();
+        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_FULLSCREEN
+                | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+
+        if (SmsCallHelper.returnUserRingtoneLoop(this)) {
+            win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
+        Bundle extras = getIntent().getExtras();
+        mNumbers = extras.getString("number");
+
+        final LayoutInflater inflater = LayoutInflater.from(this);
+        final View view = inflater.inflate(getLayoutResId(), null);
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        setContentView(view);
+
+        mDismissButton = ((FrameLayout)
+                this.findViewById(R.id.dismissalert));
+        mDismissButton.setOnClickListener(mDismissButtonListener);
+
+        setAlertText(mNumbers);
+
+        startService();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mUserDestroy) {
+            stopService();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Bundle extras = intent.getExtras();
+        boolean noSound = intent.getBooleanExtra("norun", false);
+        String newNumber = extras.getString("number");
+        if (!mNumbers.contains(newNumber)) {
+            mNumbers += getResources().getString(
+                    R.string.quiet_hours_alarm_and) + newNumber;
+            setAlertText(mNumbers);
+        }
+        if (!noSound) {
+            startService();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Don't allow dismissal
+        return;
+    }
+
+    protected int getLayoutResId() {
+        return R.layout.bypass_alarm;
+    }
+
+    private void setAlertText(String numbers) {
+        TextView alertText = (TextView) findViewById(R.id.bypasstext);
+        alertText.setText(numbers + getResources().getString(
+                R.string.quiet_hours_alarm_message));
+    }
+
+    private FrameLayout.OnClickListener mDismissButtonListener = new FrameLayout.OnClickListener() {
+        public void onClick(View v) {
+            mUserDestroy = true;
+            finish();
+        }
+    };
+
+    private void startService() {
+        Intent serviceIntent = new Intent(this, AlarmService.class);
+        serviceIntent.putExtra("number", mNumbers);
+        this.startService(serviceIntent);
+    }
+
+    private void stopService() {
+        Intent serviceIntent = new Intent(this, AlarmService.class);
+        this.stopService(serviceIntent);
+    }
+}

aosp-mirror/platform_packages_apps_settings

src/com/android/settings/mahdi/service/AlarmReceiver.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mahdi.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private final static String TAG = "AlarmReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SmsCallHelper.scheduleService(context);
+    }
+
+}

src/com/android/settings/mahdi/service/AlarmService.java
@@ -1,0 +1,171 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mahdi.service;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnSeekCompleteListener;
+import android.media.MediaPlayer.OnErrorListener;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import com.android.settings.R;
+
+public class AlarmService extends Service {
+
+    private static final int NOTI_ID = 286;
+
+    private NotificationManager mManager;
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    private int mAlarmVolumeSetting;
+    private boolean mPlaying = false;
+
+    @Override
+    public void onCreate() {
+        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        mManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopAlarm();
+        mManager.cancel(NOTI_ID);
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try {
+            startAlarmSound();
+        } catch (Exception e) {
+            // Do nothing
+        }
+
+        Bundle extras = intent.getExtras();
+        String names = extras.getString("number");
+        String title = getResources().getString(
+                R.string.quiet_hours_alarm_dialog_title);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_quiethours);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setTicker(title)
+                .setContentTitle(title)
+                .setContentText(names)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_quiethours)
+                .setLargeIcon(bm)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(names + getResources().getString(
+                                R.string.quiet_hours_alarm_message)));
+
+        Intent alarmDialog = new Intent();
+        alarmDialog.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        alarmDialog.setClass(this, com.android.settings.mahdi.service.BypassAlarm.class);
+        alarmDialog.putExtra("number", names);
+        alarmDialog.putExtra("norun", true);
+
+        PendingIntent result = PendingIntent.getActivity(
+                this, 0, alarmDialog, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        builder.setContentIntent(result);
+        mManager.notify(NOTI_ID, builder.build());
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void startAlarmSound()
+            throws java.io.IOException, IllegalArgumentException, IllegalStateException {
+
+        Uri alertSound = SmsCallHelper.returnUserRingtone(this);
+
+        if (mPlaying) {
+            stopAlarm();
+        } else {
+            mAlarmVolumeSetting = mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+        }
+
+        if (mMediaPlayer == null) {
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setOnErrorListener(new OnErrorListener() {
+                @Override
+                public boolean onError(MediaPlayer mp, int what, int extra) {
+                    mp.stop();
+                    mp.release();
+                    mMediaPlayer = null;
+                    return true;
+                }
+            });
+        }
+
+        mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM,
+                mAudioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+
+        mMediaPlayer.setDataSource(this, alertSound);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+
+        if (SmsCallHelper.returnUserRingtoneLoop(this)) {
+            mMediaPlayer.setLooping(true);
+        } else {
+            mMediaPlayer.setLooping(false);
+            mMediaPlayer.setOnSeekCompleteListener(stopSelf);
+        }
+        mMediaPlayer.prepare();
+        mMediaPlayer.start();
+        mPlaying = true;
+    }
+
+    public void stopAlarm() {
+        if (mPlaying) {
+
+            if (mMediaPlayer != null) {
+                mMediaPlayer.stop();
+                mMediaPlayer.release();
+                mMediaPlayer = null;
+                mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM,
+                        mAlarmVolumeSetting, 0);
+            }
+
+            mPlaying = false;
+        }
+    }
+
+    final OnSeekCompleteListener stopSelf = new OnSeekCompleteListener() {
+        public void onSeekComplete(MediaPlayer mp) {
+            stopAlarm();
+        }
+    };
+}

src/com/android/settings/mahdi/service/BootReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mahdi.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SmsCallHelper.scheduleService(context);
+    }
+}

src/com/android/settings/mahdi/service/BypassAlarm.java
@@ -1,0 +1,131 @@
+/*
+ * Copyright (C) 2013 Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mahdi.service;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class BypassAlarm extends Activity {
+
+    private FrameLayout mDismissButton;
+
+    private String mNumbers;
+
+    private boolean mUserDestroy;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mUserDestroy = false;
+
+        final Window win = getWindow();
+        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_FULLSCREEN
+                | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+
+        if (SmsCallHelper.returnUserRingtoneLoop(this)) {
+            win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
+        Bundle extras = getIntent().getExtras();
+        mNumbers = extras.getString("number");
+
+        final LayoutInflater inflater = LayoutInflater.from(this);
+        final View view = inflater.inflate(getLayoutResId(), null);
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        setContentView(view);
+
+        mDismissButton = ((FrameLayout)
+                this.findViewById(R.id.dismissalert));
+        mDismissButton.setOnClickListener(mDismissButtonListener);
+
+        setAlertText(mNumbers);
+
+        startService();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mUserDestroy) {
+            stopService();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Bundle extras = intent.getExtras();
+        boolean noSound = intent.getBooleanExtra("norun", false);
+        String newNumber = extras.getString("number");
+        if (!mNumbers.contains(newNumber)) {
+            mNumbers += getResources().getString(
+                    R.string.quiet_hours_alarm_and) + newNumber;
+            setAlertText(mNumbers);
+        }
+        if (!noSound) {
+            startService();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Don't allow dismissal
+        return;
+    }
+
+    protected int getLayoutResId() {
+        return R.layout.bypass_alarm;
+    }
+
+    private void setAlertText(String numbers) {
+        TextView alertText = (TextView) findViewById(R.id.bypasstext);
+        alertText.setText(numbers + getResources().getString(
+                R.string.quiet_hours_alarm_message));
+    }
+
+    private FrameLayout.OnClickListener mDismissButtonListener = new FrameLayout.OnClickListener() {
+        public void onClick(View v) {
+            mUserDestroy = true;
+            finish();
+        }
+    };
+
+    private void startService() {
+        Intent serviceIntent = new Intent(this, AlarmService.class);
+        serviceIntent.putExtra("number", mNumbers);
+        this.startService(serviceIntent);
+    }
+
+    private void stopService() {
+        Intent serviceIntent = new Intent(this, AlarmService.class);
+        this.stopService(serviceIntent);
+    }
+}