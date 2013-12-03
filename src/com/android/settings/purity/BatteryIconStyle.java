157c6ae78ab1c1ba6b89bfa2258c997c2565cae3
AndroidRul3z <ferrettidario@gmail.com>
2013-12-03 15:12:08 +0100

    Settings for CM battery customization

--- src/com/android/settings/purity/BatteryIconStyle.java
+++ src/com/android/settings/purity/BatteryIconStyle.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Slimroms Project
+ * Copyright (C) 2012 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,305 +16,49 @@
 
 package com.android.settings.purity;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Color;
+import android.content.ContentResolver;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
-
-import java.util.Date;
 
 public class BatteryIconStyle extends SettingsPreferenceFragment
-    implements OnPreferenceChangeListener {
+ implements OnPreferenceChangeListener {
 
-    private static final String TAG = "BatteryIconStyle";
-
-    private static final String PREF_STATUS_BAR_BATTERY = "battery_icon";
-    private static final String PREF_STATUS_BAR_BATTERY_COLOR = "battery_color";
-    private static final String PREF_STATUS_BAR_BATTERY_TEXT_COLOR = "battery_text_color";
-    private static final String PREF_STATUS_BAR_BATTERY_TEXT_CHARGING_COLOR = "battery_text_charging_color";
-    private static final String PREF_STATUS_BAR_CIRCLE_BATTERY_ANIMATIONSPEED = "circle_battery_animation_speed";
-
-
-    private static final int MENU_RESET = Menu.FIRST;
-
-    private static final int DLG_RESET = 0;
+    private static final String STATUS_BAR_BATTERY = "status_bar_battery";
 
     private ListPreference mStatusBarBattery;
-    private ColorPickerPreference mBatteryColor;
-    private ColorPickerPreference mBatteryTextColor;
-    private ColorPickerPreference mBatteryTextChargingColor;
-    private ListPreference mCircleAnimSpeed;
-
-    private boolean mCheckPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        createCustomView();
-    }
-
-    private PreferenceScreen createCustomView() {
-        mCheckPreferences = false;
-        PreferenceScreen prefSet = getPreferenceScreen();
-        if (prefSet != null) {
-            prefSet.removeAll();
-        }
 
         addPreferencesFromResource(R.xml.purity_battery_style);
-        prefSet = getPreferenceScreen();
 
-        int intColor;
-        String hexColor;
+        ContentResolver resolver = getActivity().getContentResolver();
 
-        PackageManager pm = getPackageManager();
-        Resources systemUiResources;
-        try {
-            systemUiResources = pm.getResourcesForApplication("com.android.systemui");
-        } catch (Exception e) {
-            Log.e(TAG, "can't access systemui resources",e);
-            return null;
-        }
+        mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY);
 
-        mStatusBarBattery = (ListPreference) prefSet.findPreference(PREF_STATUS_BAR_BATTERY);
-        mStatusBarBattery.setOnPreferenceChangeListener(this);
-        int statusBarBattery = Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.STATUS_BAR_BATTERY, 0);
-        mStatusBarBattery.setValue(String.valueOf(statusBarBattery));
+        int batteryStyle = Settings.System.getInt(resolver, Settings.System.STATUS_BAR_BATTERY, 0);
+        mStatusBarBattery.setValue(String.valueOf(batteryStyle));
         mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
-
-        mBatteryColor = (ColorPickerPreference) findPreference(PREF_STATUS_BAR_BATTERY_COLOR);
-        mBatteryColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_COLOR, -2);
-        if (intColor == -2) {
-            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
-                    "com.android.systemui:color/batterymeter_charge_color", null, null));
-            mBatteryColor.setSummary(getResources().getString(R.string.default_string));
-        } else {
-            hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mBatteryColor.setSummary(hexColor);
-        }
-        mBatteryColor.setNewPreviewColor(intColor);
-
-        mBatteryTextColor =
-            (ColorPickerPreference) findPreference(PREF_STATUS_BAR_BATTERY_TEXT_COLOR);
-        mBatteryTextColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_TEXT_COLOR, -2);
-        mBatteryTextColor.setSummary(getResources().getString(R.string.default_string));
-        if (intColor == -2 && statusBarBattery == 2) {
-            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
-                    "com.android.systemui:color/batterymeter_bolt_color", null, null));
-        } else if (intColor == -2) {
-            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
-                    "com.android.systemui:color/batterymeter_charge_color", null, null));
-        } else {
-            hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mBatteryTextColor.setSummary(hexColor);
-        }
-        mBatteryTextColor.setNewPreviewColor(intColor);
-
-        mBatteryTextChargingColor = (ColorPickerPreference)
-            findPreference(PREF_STATUS_BAR_BATTERY_TEXT_CHARGING_COLOR);
-        mBatteryTextChargingColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_TEXT_CHARGING_COLOR, -2);
-        mBatteryTextChargingColor.setSummary(getResources().getString(R.string.default_string));
-        if (intColor == -2 && (statusBarBattery == 2 || statusBarBattery == 0)) {
-            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
-                    "com.android.systemui:color/batterymeter_bolt_color", null, null));
-        } else if (intColor == -2) {
-            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
-                    "com.android.systemui:color/batterymeter_charge_color", null, null));
-        } else {
-            hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mBatteryTextChargingColor.setSummary(hexColor);
-        }
-        mBatteryTextChargingColor.setNewPreviewColor(intColor);
-
-        mCircleAnimSpeed =
-            (ListPreference) findPreference(PREF_STATUS_BAR_CIRCLE_BATTERY_ANIMATIONSPEED);
-        mCircleAnimSpeed.setOnPreferenceChangeListener(this);
-        mCircleAnimSpeed.setValue((Settings.System
-                .getInt(getActivity().getContentResolver(),
-                        Settings.System.STATUS_BAR_CIRCLE_BATTERY_ANIMATIONSPEED, 3))
-                + "");
-        mCircleAnimSpeed.setSummary(mCircleAnimSpeed.getEntry());
-
-        updateBatteryIconOptions(statusBarBattery);
-
-        setHasOptionsMenu(true);
-        mCheckPreferences = true;
-        return prefSet;
+        mStatusBarBattery.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_settings_backup) // use the backup icon
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                showDialogInner(DLG_RESET);
-                return true;
-             default:
-                return super.onContextItemSelected(item);
-        }
-    }
-
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (!mCheckPreferences) {
-            return false;
-        }
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mStatusBarBattery) {
-            int statusBarBattery = Integer.valueOf((String) newValue);
+            int batteryStyle = Integer.valueOf((String) newValue);
             int index = mStatusBarBattery.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY, statusBarBattery);
+            Settings.System.putInt(resolver, Settings.System.STATUS_BAR_BATTERY, batteryStyle);
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
-            createCustomView();
-            return true;
-        } else if (preference == mBatteryColor) {
-            String hex = ColorPickerPreference.convertToARGB(Integer
-                    .valueOf(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_COLOR, intHex);
-            return true;
-        } else if (preference == mBatteryTextColor) {
-            String hex = ColorPickerPreference.convertToARGB(Integer
-                    .valueOf(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_TEXT_COLOR, intHex);
-            return true;
-        } else if (preference == mBatteryTextChargingColor) {
-            String hex = ColorPickerPreference.convertToARGB(Integer
-                    .valueOf(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_TEXT_CHARGING_COLOR, intHex);
-            return true;
-        } else if (preference == mCircleAnimSpeed) {
-            int val = Integer.parseInt((String) newValue);
-            int index = mCircleAnimSpeed.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_CIRCLE_BATTERY_ANIMATIONSPEED, val);
-            mCircleAnimSpeed.setSummary(mCircleAnimSpeed.getEntries()[index]);
             return true;
         }
         return false;
     }
-
-    private void updateBatteryIconOptions(int batteryIconStat) {
-        mBatteryTextChargingColor.setTitle(R.string.battery_text_charging_color);
-        if (batteryIconStat == 0) {
-            mBatteryColor.setEnabled(true);
-            mBatteryTextColor.setEnabled(false);
-            mBatteryTextChargingColor.setEnabled(true);
-            mBatteryTextChargingColor.setTitle(R.string.battery_bolt_color);
-            mCircleAnimSpeed.setEnabled(false);
-        } else if (batteryIconStat == 2) {
-            mBatteryColor.setEnabled(true);
-            mBatteryTextColor.setEnabled(true);
-            mBatteryTextChargingColor.setEnabled(true);
-            mCircleAnimSpeed.setEnabled(false);
-        } else if (batteryIconStat == 3 || batteryIconStat == 5) {
-            mBatteryColor.setEnabled(true);
-            mBatteryTextColor.setEnabled(false);
-            mBatteryTextChargingColor.setEnabled(false);
-            mCircleAnimSpeed.setEnabled(true);
-        } else if (batteryIconStat == 4 || batteryIconStat == 6) {
-            mBatteryColor.setEnabled(true);
-            mBatteryTextColor.setEnabled(true);
-            mBatteryTextChargingColor.setEnabled(true);
-            mCircleAnimSpeed.setEnabled(true);
-        } else {
-            mBatteryColor.setEnabled(false);
-            mBatteryTextColor.setEnabled(true);
-            mBatteryTextChargingColor.setEnabled(true);
-            mCircleAnimSpeed.setEnabled(false);
-        }
-    }
-
-    private void showDialogInner(int id) {
-        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
-        newFragment.setTargetFragment(this, 0);
-        newFragment.show(getFragmentManager(), "dialog " + id);
-    }
-
-    public static class MyAlertDialogFragment extends DialogFragment {
-
-        public static MyAlertDialogFragment newInstance(int id) {
-            MyAlertDialogFragment frag = new MyAlertDialogFragment();
-            Bundle args = new Bundle();
-            args.putInt("id", id);
-            frag.setArguments(args);
-            return frag;
-        }
-
-        BatteryIconStyle getOwner() {
-            return (BatteryIconStyle) getTargetFragment();
-        }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            int id = getArguments().getInt("id");
-            switch (id) {
-                case DLG_RESET:
-                    return new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.reset)
-                    .setMessage(R.string.battery_style_reset_message)
-                    .setNegativeButton(R.string.cancel, null)
-                    .setPositiveButton(R.string.dlg_ok,
-                        new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            Settings.System.putInt(getActivity().getContentResolver(),
-                                Settings.System.STATUS_BAR_BATTERY_COLOR, -2);
-                            Settings.System.putInt(getActivity().getContentResolver(),
-                                Settings.System.STATUS_BAR_BATTERY_TEXT_COLOR, -2);
-                            Settings.System.putInt(getActivity().getContentResolver(),
-                                Settings.System.STATUS_BAR_BATTERY_TEXT_CHARGING_COLOR, -2);
-                            getOwner().createCustomView();
-                        }
-                    })
-                    .create();
-            }
-            throw new IllegalArgumentException("unknown id " + id);
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialog) {
-
-        }
-    }
-
 }
