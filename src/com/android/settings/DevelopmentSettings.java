b43e774425cdc84e7cfca0e3728cd652a61a4fed
Abhisek Devkota <ciwrl@cyanogenmod.com>
2013-12-10 07:42:15 -0800

    Address the 'ART' Issue
    Rather than outright remove this completely, address primary concerns:
    
    * Add a more verbose warning message on alert dialog
    * Explicitly state experimental form
    * Explicitly state not supported (no bug reports)
    * Explicitly state issues (crashes, bootloop, etc)
    
    Additionally, move option to 'Apps' header (bottom), as this is it's
    primary impact.
    
    See prior discussion: http://review.cyanogenmod.org/#/c/55609/
    
    PS2: Linkify
    
    Change-Id: Icf103b5576c8e93f8a30ccd348476bf207401355
    
    Conflicts:
    
    	res/values/cm_strings.xml

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -56,7 +56,10 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.text.Html;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.HardwareRenderer;
@@ -1402,8 +1405,11 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             if (!newRuntimeValue.equals(oldRuntimeValue)) {
                 final Context context = getActivity();
                 final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setMessage(context.getResources().getString(R.string.select_runtime_warning_message,
-                                                                    oldRuntimeValue, newRuntimeValue));
+                //builder.setMessage(context.getResources().getString(R.string.select_runtime_warning_message,
+                //                                                    oldRuntimeValue, newRuntimeValue));
+                builder.setMessage(Html.fromHtml(context.getResources().getString(R.string.custom_runtime_warning_message,
+                        oldRuntimeValue, newRuntimeValue)));
+                builder.setTitle(context.getResources().getString(R.string.custom_runtime_warning_title));
                 builder.setPositiveButton(android.R.string.ok, new OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -1420,7 +1426,9 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
                         updateRuntimeValue();
                     }
                 });
-                builder.show();
+                final AlertDialog dialog = builder.show();
+                ((TextView) dialog.findViewById(android.R.id.message))
+                    .setMovementMethod(LinkMovementMethod.getInstance());
             }
             return true;
         } else if (HDCP_CHECKING_KEY.equals(preference.getKey())) {
