5b5d8c6514e5d35e48822bf4932b39a8e827d20c
Mahdi-Rom <mahdi.rom@gmail.com>
2013-12-11 18:22:32 +0000

    [2/2] Settings: Change of default stream type controlled by volume rocker

--- src/com/android/settings/mahdi/ButtonSettings.java
+++ src/com/android/settings/mahdi/ButtonSettings.java
@@ -18,19 +18,31 @@
 
 package com.android.settings.mahdi;
 
+import java.util.prefs.PreferenceChangeListener;
+
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.media.AudioSystem;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.view.VolumePanel;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ButtonSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -39,13 +51,13 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
     private static final String CATEGORY_VOLUME = "button_volume_keys";
     private static final String BUTTON_VOLUME_WAKE = "button_volume_wake_screen";
-    private static final String KEY_VOLBTN_MUSIC_CTRL = "volbtn_music_controls";
+    private static final String BUTTON_VOLUME_DEFAULT = "button_volume_default_screen";
     private static final String KEY_HARDWARE_KEYS_CATEGORY = "hardware_keys_category";
     private static final String KEY_HARDWARE_KEYS = "hardware_keys";
     private static final String CATEGORY_BACK = "button_back_key";    
 
     private CheckBoxPreference mVolumeWake;
-    private CheckBoxPreference mVolBtnMusicCtrl;
+    private ListPreference mVolumeDefault;
     private PreferenceScreen mHardwareKeys;    
 
     @Override
@@ -59,12 +71,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         final Resources res = getResources();
 
         final PreferenceCategory volumeCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_VOLUME);
-
-	mVolBtnMusicCtrl = (CheckBoxPreference) findPreference(KEY_VOLBTN_MUSIC_CTRL);
-        mVolBtnMusicCtrl.setChecked(Settings.System.getInt(getContentResolver(),
-                Settings.System.VOLUME_MUSIC_CONTROLS, 1) != 0);
-        mVolBtnMusicCtrl.setOnPreferenceChangeListener(this);	
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_VOLUME);		
 
     // Only show the hardware keys config on a device that does not have a navbar
         mHardwareKeys = (PreferenceScreen) findPreference(KEY_HARDWARE_KEYS);
@@ -76,16 +83,32 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         } 
 
         if (hasVolumeRocker()) {
+            mVolumeWake = (CheckBoxPreference) findPreference(BUTTON_VOLUME_WAKE);
+            mVolumeDefault = (ListPreference) findPreference(BUTTON_VOLUME_DEFAULT);
             if (!res.getBoolean(R.bool.config_show_volumeRockerWake)) {
-                prefScreen.removePreference(volumeCategory);
+                prefScreen.removePreference(mVolumeWake);
             } else {
-                mVolumeWake = (CheckBoxPreference) findPreference(BUTTON_VOLUME_WAKE);
                 mVolumeWake.setChecked(Settings.System.getInt(resolver,
-                        Settings.System.VOLUME_WAKE_SCREEN, 0) != 0);
+                    Settings.System.VOLUME_WAKE_SCREEN, 0) != 0);
             }
+            String currentDefault = Settings.System.getString(resolver, Settings.System.VOLUME_KEYS_DEFAULT);
+
+            if (!Utils.isVoiceCapable(getActivity())) {
+                removeListEntry(mVolumeDefault, String.valueOf(AudioSystem.STREAM_RING));
+            }
+
+            if (currentDefault == null) {
+                currentDefault = mVolumeDefault.getEntryValues()[mVolumeDefault.getEntryValues().length - 1].toString();
+            }
+            mVolumeDefault.setValue(currentDefault);
+            mVolumeDefault.setOnPreferenceChangeListener(this);
         } else {
             prefScreen.removePreference(volumeCategory);
         }
+    }
+
+    private boolean hasVolumeRocker() {
+        return getActivity().getResources().getBoolean(R.bool.config_has_volume_rocker);
     }
     
     @Override
@@ -94,25 +117,34 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             boolean checked = ((CheckBoxPreference)preference).isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.VOLUME_WAKE_SCREEN, checked ? 1:0);
-            return true;	
+            return true;
         }
-
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
-    private boolean hasVolumeRocker() {
-        return getActivity().getResources().getBoolean(R.bool.config_has_volume_rocker);
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mVolumeDefault) {
+            String value = (String)newValue;
+            Settings.System.putString(getActivity().getContentResolver(), Settings.System.VOLUME_KEYS_DEFAULT, value);
+            return true;
+        }
+        return false;
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
-        if (KEY_VOLBTN_MUSIC_CTRL.equals(key)) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.VOLUME_MUSIC_CONTROLS,
-                    (Boolean) objValue ? 1 : 0);
-                
-	    return true;
+    public void removeListEntry(ListPreference list, String valuetoRemove) {
+        ArrayList<CharSequence> entries = new ArrayList<CharSequence>();
+        ArrayList<CharSequence> values = new ArrayList<CharSequence>();
+
+        for (int i = 0; i < list.getEntryValues().length; i++) {
+            if (list.getEntryValues()[i].toString().equals(valuetoRemove)) {
+                continue;
+            } else {
+                entries.add(list.getEntries()[i]);
+                values.add(list.getEntryValues()[i]);
+            }
         }
-	return false;
+
+        list.setEntries(entries.toArray(new CharSequence[entries.size()]));
+        list.setEntryValues(values.toArray(new CharSequence[values.size()]));
     }
 }
