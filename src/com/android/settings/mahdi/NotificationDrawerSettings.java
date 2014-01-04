65487b5a5995f5e5ccffad834299d888a2924961
Mahdi-Rom <mahdi.rom@gmail.com>
2014-01-04 22:31:35 +0000

    [2/2] Settings: Add a custom notification panel header
    Thanks to @xplodwild

--- src/com/android/settings/mahdi/NotificationDrawerSettings.java
+++ src/com/android/settings/mahdi/NotificationDrawerSettings.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.mahdi;
 
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -37,8 +38,10 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
     public static final String TAG = "NotificationDrawerSettings";
     private static final String PREF_NOTIFICATION_OPTIONS = "options";
     private static final String PREF_NOTIFICATION_HIDE_CARRIER = "notification_hide_carrier";
+    private static final String STATUS_BAR_CUSTOM_HEADER = "custom_status_bar_header";
 
-    CheckBoxPreference mHideCarrier;
+    private CheckBoxPreference mHideCarrier;
+    private CheckBoxPreference mStatusBarCustomHeader;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,6 +56,11 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
                 Settings.System.NOTIFICATION_HIDE_CARRIER, 0) == 1;
         mHideCarrier.setChecked(hideCarrier);
         mHideCarrier.setOnPreferenceChangeListener(this);
+
+        mStatusBarCustomHeader = (CheckBoxPreference) findPreference(STATUS_BAR_CUSTOM_HEADER);
+        mStatusBarCustomHeader.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+            Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1);
+        mStatusBarCustomHeader.setOnPreferenceChangeListener(this);
 
         PreferenceCategory additionalOptions =
             (PreferenceCategory) prefs.findPreference(PREF_NOTIFICATION_OPTIONS);
@@ -76,11 +84,17 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mHideCarrier) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.NOTIFICATION_HIDE_CARRIER,
-                    (Boolean) newValue ? 1 : 0);
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.NOTIFICATION_HIDE_CARRIER, value ? 1 : 0);
+            return true;
+        } else if (preference == mStatusBarCustomHeader) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(resolver,
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
             return true;
         }
         return false;
