abe1d8632e30f63fc20c154c987c49a4a47ec438
AndroidRul3z <ferrettidario@gmail.com>
2014-01-06 14:32:58 +0100

    settings: Add text plain battery style
    http://review.cyanogenmod.org/#/c/56039/
    
    Patch Set 4

--- src/com/android/settings/purity/BatteryIconStyle.java
+++ src/com/android/settings/purity/BatteryIconStyle.java
@@ -18,20 +18,25 @@ package com.android.settings.purity;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class BatteryIconStyle extends SettingsPreferenceFragment
- implements OnPreferenceChangeListener {
-
+public class BatteryIconStyle extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
     private static final String STATUS_BAR_BATTERY = "status_bar_battery";
+    private static final String STATUS_BAR_BATTERY_SHOW_PERCENT =
+            "status_bar_battery_show_percent";
+
+    private static final String STATUS_BAR_STYLE_HIDDEN = "4";
+    private static final String STATUS_BAR_STYLE_TEXT = "6";
 
     private ListPreference mStatusBarBattery;
+    private CheckBoxPreference mStatusBarBatteryShowPercent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,11 +47,14 @@ public class BatteryIconStyle extends SettingsPreferenceFragment
         ContentResolver resolver = getActivity().getContentResolver();
 
         mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY);
+        mStatusBarBatteryShowPercent =
+                (CheckBoxPreference) findPreference(STATUS_BAR_BATTERY_SHOW_PERCENT);
 
         int batteryStyle = Settings.System.getInt(resolver, Settings.System.STATUS_BAR_BATTERY, 0);
         mStatusBarBattery.setValue(String.valueOf(batteryStyle));
         mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
         mStatusBarBattery.setOnPreferenceChangeListener(this);
+        enableStatusBarBatteryDependents(mStatusBarBattery.getValue());
     }
 
     @Override
@@ -57,8 +65,15 @@ public class BatteryIconStyle extends SettingsPreferenceFragment
             int index = mStatusBarBattery.findIndexOfValue((String) newValue);
             Settings.System.putInt(resolver, Settings.System.STATUS_BAR_BATTERY, batteryStyle);
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
+            enableStatusBarBatteryDependents((String)newValue);
             return true;
         }
         return false;
+    }
+
+    private void enableStatusBarBatteryDependents(String value) {
+        boolean enabled = !value.equals(STATUS_BAR_STYLE_TEXT)
+                && !value.equals(STATUS_BAR_STYLE_HIDDEN);
+        mStatusBarBatteryShowPercent.setEnabled(enabled);
     }
 }
