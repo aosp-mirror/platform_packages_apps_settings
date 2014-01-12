fa8b8396c3f50235fe9e54716982cf924a3431fd
Mahdi-Rom <mahdi.rom@gmail.com>
2014-01-12 10:38:57 +0000

    Settings: remove Active Display (1/2)
    commit b24ccc06cfa2a188e03e74b08d781b439607a79f
    Author: Mahdi-Rom <mahdi.rom@gmail.com>
    Date:   Sun Jan 12 10:16:52 2014 +0000
    
        Revert "Add active display from ChameleonOS [2/2]"
    
        This reverts commit 7a6770933152254c19cdbe5256303bf463173917.
    
    commit b24ccc06cfa2a188e03e74b08d781b439607a79f
    Author: Mahdi-Rom <mahdi.rom@gmail.com>
    Date:   Sun Jan 12 10:16:52 2014 +0000
    
        Revert "[2/2] Settings: Pimp up ActiveDisplay"
    
        This reverts commit fbe6c64aa13d052c295efa9549d9fc17624e126e.
    
    commit 374b936189823c86b42e34cab6b32fc8bfd2d016
    Author: Mahdi-Rom <mahdi.rom@gmail.com>
    Date:   Sun Jan 12 10:16:27 2014 +0000
    
        Revert "[1/2] Settings: Fix pimp up ActiveDisplay"
    
        This reverts commit af8233fc2ef6f5ff213726c3d87ddfcf3cf4b75c.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1166,7 +1166,6 @@ public class Settings extends PreferenceActivity
     public static class PowerMenuSettingsActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }    
     public static class LockscreenSettingsActivity extends Settings { /* empty */ }
-    public static class ActiveDisplaySettingsActivity extends Settings { /* empty */ }
     public static class ApnSettingsActivity extends Settings { /* empty */ }
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class CustomizationSettingsActivity extends Settings { /* empty */ }
