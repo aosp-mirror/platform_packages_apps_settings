4c820bd22fcd22d39ace440a5bcffc5f3888e560
Mahdi-Rom <mahdi.rom@gmail.com>
2014-02-02 15:35:51 +0000

    Revert all recent app opps changes
    Revert "[1/2] Settings AppOps: Forward port wifi/bluetooth/mobile/wakeup ops"
    Revert "[1/2] Settings AppOps: show/hide app type and allow/deny counters"
    Revert "[1/2] Settings: AppOps: Add bootup template"

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -16,7 +16,6 @@
 
 package com.android.settings.applications;
 
-import android.app.Activity;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
@@ -24,7 +23,6 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
-import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -54,15 +52,12 @@ public class AppOpsState {
 
     List<AppOpEntry> mApps;
 
-    private SharedPreferences mPreferences;
-
     public AppOpsState(Context context) {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries_cm);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_cm);
-        mPreferences = context.getSharedPreferences("appops_manager", Activity.MODE_PRIVATE);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
     }
 
     public static class OpsTemplate implements Parcelable {
@@ -193,31 +188,18 @@ public class AppOpsState {
                     AppOpsManager.OP_CALL_PHONE,
                     AppOpsManager.OP_WRITE_SETTINGS,
                     AppOpsManager.OP_SYSTEM_ALERT_WINDOW,
-                    AppOpsManager.OP_WAKE_LOCK,
-                    AppOpsManager.OP_WIFI_CHANGE,
-                    AppOpsManager.OP_BLUETOOTH_CHANGE,
-                    AppOpsManager.OP_DATA_CONNECT_CHANGE,
-                    AppOpsManager.OP_ALARM_WAKEUP, },
+                    AppOpsManager.OP_WAKE_LOCK },
             new boolean[] { false,
                     true,
                     true,
                     true,
                     true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true }
-            );
-
-    public static final OpsTemplate BOOTUP_TEMPLATE = new OpsTemplate(
-            new int[] { AppOpsManager.OP_BOOT_COMPLETED },
-            new boolean[] { true, }
+                    true,  }
             );
 
     public static final OpsTemplate[] ALL_TEMPLATES = new OpsTemplate[] {
             LOCATION_TEMPLATE, PERSONAL_TEMPLATE, MESSAGING_TEMPLATE,
-            MEDIA_TEMPLATE, DEVICE_TEMPLATE, BOOTUP_TEMPLATE
+            MEDIA_TEMPLATE, DEVICE_TEMPLATE
     };
 
     /**
@@ -373,59 +355,30 @@ public class AppOpsState {
         }
 
         private CharSequence getCombinedText(ArrayList<AppOpsManager.OpEntry> ops,
-                CharSequence[] items, Resources res, boolean withTerseCounts) {
-            StringBuilder builder = new StringBuilder();
-            for (int i=0; i<ops.size(); i++) {
-                if (i > 0) {
-                    builder.append(", ");
+                CharSequence[] items) {
+            if (ops.size() == 1) {
+                return items[ops.get(0).getOp()];
+            } else {
+                StringBuilder builder = new StringBuilder();
+                for (int i=0; i<ops.size(); i++) {
+                    if (i > 0) {
+                        builder.append(", ");
+                    }
+                    builder.append(items[ops.get(i).getOp()]);
                 }
-                AppOpsManager.OpEntry op = ops.get(i);
-                int count = op.getAllowedCount() + op.getIgnoredCount();
-
-                if (withTerseCounts && count > 0) {
-                    String quantity = res.getQuantityString(R.plurals.app_ops_count,
-                            count, count);
-                    builder.append(res.getString(R.string.app_ops_entry_summary,
-                            items[op.getOp()], quantity));
-                } else {
-                    builder.append(items[op.getOp()]);
-                }
+                return builder.toString();
             }
-            return builder.toString();
-        }
-
-        public CharSequence getCountsText(Resources res) {
-            AppOpsManager.OpEntry op = mOps.get(0);
-            int allowed = op.getAllowedCount();
-            int denied = op.getIgnoredCount();
-
-            if (allowed == 0 && denied == 0) {
-                return null;
-            }
-
-            CharSequence allowedQuantity = res.getQuantityString(R.plurals.app_ops_count,
-                    allowed, allowed);
-            CharSequence deniedQuantity = res.getQuantityString(R.plurals.app_ops_count,
-                    denied, denied);
-
-            if (denied == 0) {
-                return res.getString(R.string.app_ops_allowed_count, allowedQuantity);
-            } else if (allowed == 0) {
-                return res.getString(R.string.app_ops_ignored_count, deniedQuantity);
-            }
-            return res.getString(R.string.app_ops_both_count, allowedQuantity, deniedQuantity);
         }
 
         public CharSequence getSummaryText(AppOpsState state) {
-            return getCombinedText(mOps, state.mOpSummaries, state.mContext.getResources(), true);
+            return getCombinedText(mOps, state.mOpSummaries);
         }
 
         public CharSequence getSwitchText(AppOpsState state) {
-            Resources res = state.mContext.getResources();
             if (mSwitchOps.size() > 0) {
-                return getCombinedText(mSwitchOps, state.mOpLabels, res, false);
+                return getCombinedText(mSwitchOps, state.mOpLabels);
             } else {
-                return getCombinedText(mOps, state.mOpLabels, res, false);
+                return getCombinedText(mOps, state.mOpLabels);
             }
         }
 
@@ -509,47 +462,24 @@ public class AppOpsState {
     }
 
     private AppEntry getAppEntry(final Context context, final HashMap<String, AppEntry> appEntries,
-            final String packageName, ApplicationInfo appInfo, boolean applyFilters) {
-
-        if (appInfo == null) {
-            try {
-                appInfo = mPm.getApplicationInfo(packageName,
-                        PackageManager.GET_DISABLED_COMPONENTS
-                        | PackageManager.GET_UNINSTALLED_PACKAGES);
-            } catch (PackageManager.NameNotFoundException e) {
-                Log.w(TAG, "Unable to find info for package " + packageName);
-                return null;
-            }
-        }
-
-        if (applyFilters) {
-            // Hide user apps if needed
-            if (!shouldShowUserApps() &&
-                    (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                return null;
-            }
-            // Hide system apps if needed
-            if (!shouldShowSystemApps() &&
-                     (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
-                return null;
-            }
-        }
-
+            final String packageName, ApplicationInfo appInfo) {
         AppEntry appEntry = appEntries.get(packageName);
         if (appEntry == null) {
+            if (appInfo == null) {
+                try {
+                    appInfo = mPm.getApplicationInfo(packageName,
+                            PackageManager.GET_DISABLED_COMPONENTS
+                            | PackageManager.GET_UNINSTALLED_PACKAGES);
+                } catch (PackageManager.NameNotFoundException e) {
+                    Log.w(TAG, "Unable to find info for package " + packageName);
+                    return null;
+                }
+            }
             appEntry = new AppEntry(this, appInfo);
             appEntry.loadLabel(context);
             appEntries.put(packageName, appEntry);
         }
         return appEntry;
-    }
-
-    private boolean shouldShowUserApps() {
-        return mPreferences.getBoolean("show_user_apps", true);
-    }
-
-    private boolean shouldShowSystemApps() {
-        return mPreferences.getBoolean("show_system_apps", true);
     }
 
     public List<AppOpEntry> buildState(OpsTemplate tpl, int uid, String packageName) {
@@ -572,9 +502,6 @@ public class AppOpsState {
             }
         }
 
-        // Whether to apply hide user / system app filters
-        final boolean applyFilters = (packageName == null);
-
         List<AppOpsManager.PackageOps> pkgs;
         if (packageName != null) {
             pkgs = mAppOps.getOpsForPackage(uid, packageName, tpl.ops);
@@ -585,8 +512,7 @@ public class AppOpsState {
         if (pkgs != null) {
             for (int i=0; i<pkgs.size(); i++) {
                 AppOpsManager.PackageOps pkgOps = pkgs.get(i);
-                AppEntry appEntry = getAppEntry(context, appEntries, pkgOps.getPackageName(), null,
-                        applyFilters);
+                AppEntry appEntry = getAppEntry(context, appEntries, pkgOps.getPackageName(), null);
                 if (appEntry == null) {
                     continue;
                 }
@@ -614,7 +540,7 @@ public class AppOpsState {
         for (int i=0; i<apps.size(); i++) {
             PackageInfo appInfo = apps.get(i);
             AppEntry appEntry = getAppEntry(context, appEntries, appInfo.packageName,
-                    appInfo.applicationInfo, applyFilters);
+                    appInfo.applicationInfo);
             if (appEntry == null) {
                 continue;
             }
@@ -648,7 +574,7 @@ public class AppOpsState {
 
                         }
                         AppOpsManager.OpEntry opEntry = new AppOpsManager.OpEntry(
-                                permOps.get(k), AppOpsManager.MODE_ALLOWED, 0, 0, 0, 0, 0);
+                                permOps.get(k), AppOpsManager.MODE_ALLOWED, 0, 0, 0);
                         dummyOps.add(opEntry);
                         addOp(entries, pkgOps, appEntry, opEntry, packageName == null,
                                 packageName == null ? 0 : opToOrder[opEntry.getOp()]);
--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -16,23 +16,15 @@
 
 package com.android.settings.applications;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.AppOpsManager;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFrameLayout;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -46,19 +38,13 @@ public class AppOpsSummary extends Fragment {
     private View mRootView;
     private ViewPager mViewPager;
 
-    private MyPagerAdapter mAdapter;
-
-    private Activity mActivity;
-    private SharedPreferences mPreferences;
-
     CharSequence[] mPageNames;
     static AppOpsState.OpsTemplate[] sPageTemplates = new AppOpsState.OpsTemplate[] {
         AppOpsState.LOCATION_TEMPLATE,
         AppOpsState.PERSONAL_TEMPLATE,
         AppOpsState.MESSAGING_TEMPLATE,
         AppOpsState.MEDIA_TEMPLATE,
-        AppOpsState.DEVICE_TEMPLATE,
-        AppOpsState.BOOTUP_TEMPLATE
+        AppOpsState.DEVICE_TEMPLATE
     };
 
     int mCurPos;
@@ -93,24 +79,12 @@ public class AppOpsSummary extends Fragment {
             mCurPos = position;
         }
 
-        public int getCurrentPage() {
-            return mCurPos;
-        }
-
         @Override
         public void onPageScrollStateChanged(int state) {
             if (state == ViewPager.SCROLL_STATE_IDLE) {
                 //updateCurrentTab(mCurPos);
             }
         }
-    }
-
-    private void resetAdapter() {
-        // trigger adapter load, preserving the selected page
-        int curPos = mAdapter.getCurrentPage();
-        mViewPager.setAdapter(mAdapter);
-        mViewPager.setOnPageChangeListener(mAdapter);
-        mViewPager.setCurrentItem(curPos);
     }
 
     @Override
@@ -123,12 +97,12 @@ public class AppOpsSummary extends Fragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories_cm);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
-        mAdapter = new MyPagerAdapter(getChildFragmentManager());
-        mViewPager.setAdapter(mAdapter);
-        mViewPager.setOnPageChangeListener(mAdapter);
+        MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());
+        mViewPager.setAdapter(adapter);
+        mViewPager.setOnPageChangeListener(adapter);
         PagerTabStrip tabs = (PagerTabStrip) rootView.findViewById(R.id.tabs);
         tabs.setTabIndicatorColorResource(android.R.color.holo_blue_light);
 
@@ -138,88 +112,6 @@ public class AppOpsSummary extends Fragment {
             ((PreferenceFrameLayout.LayoutParams) rootView.getLayoutParams()).removeBorders = true;
         }
 
-        mActivity = getActivity();
-
         return rootView;
-    }
-
-    private boolean shouldShowUserApps() {
-        return mPreferences.getBoolean("show_user_apps", true);
-    }
-
-    private boolean shouldShowSystemApps() {
-        return mPreferences.getBoolean("show_system_apps", true);
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        // get shared preferences
-        mPreferences = mActivity.getSharedPreferences("appops_manager", Activity.MODE_PRIVATE);
-
-        setHasOptionsMenu(true);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.appops_manager, menu);
-        menu.findItem(R.id.show_user_apps).setChecked(shouldShowUserApps());
-        menu.findItem(R.id.show_system_apps).setChecked(shouldShowSystemApps());
-    }
-
-    private void resetCounters() {
-        final AppOpsManager appOps =
-                (AppOpsManager) mActivity.getSystemService(Context.APP_OPS_SERVICE);
-        if (appOps == null) {
-            return;
-        }
-        appOps.resetCounters();
-        // reload content
-        resetAdapter();
-    }
-
-    private void resetCountersConfirm() {
-        new AlertDialog.Builder(getActivity())
-            .setIcon(android.R.drawable.ic_dialog_alert)
-            .setTitle(R.string.app_ops_reset_confirm_title)
-            .setMessage(R.string.app_ops_reset_confirm_mesg)
-            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
-                {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        resetCounters();
-                    }
-                })
-            .setNegativeButton(android.R.string.cancel, null)
-            .show();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.show_user_apps:
-                final String prefNameUserApps = "show_user_apps";
-                // set the menu checkbox and save it in shared preference
-                item.setChecked(!item.isChecked());
-                mPreferences.edit().putBoolean(prefNameUserApps, item.isChecked()).commit();
-                // reload content
-                resetAdapter();
-                return true;
-            case R.id.show_system_apps:
-                final String prefNameSysApps = "show_system_apps";
-                // set the menu checkbox and save it in shared preference
-                item.setChecked(!item.isChecked());
-                mPreferences.edit().putBoolean(prefNameSysApps, item.isChecked()).commit();
-                // reload view content
-                resetAdapter();
-                return true;
-            case R.id.reset_counters:
-                resetCountersConfirm();
-                return true;
-            default:
-                return super.onContextItemSelected(item);
-        }
     }
 }
