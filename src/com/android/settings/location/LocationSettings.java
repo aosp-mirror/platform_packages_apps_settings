6068967fd946ccfcc8c69a698c0988f6e6f56ea4
Fabrice Di Meglio <fdimeglio@google.com>
2014-04-04 17:02:46 -0700

    Re-enable the switch into Location Settings
    - get rid of that onIsHidingHeaders()
    
    Change-Id: If0b3fc6900f70cb6dfb54094d1b46b44aba6290f

--- src/com/android/settings/location/LocationSettings.java
+++ src/com/android/settings/location/LocationSettings.java
@@ -86,24 +86,20 @@ public class LocationSettings extends LocationSettingsBase
         final SettingsActivity activity = (SettingsActivity) getActivity();
 
         // Only show the master switch when we're not being used as Setup Wizard.
-        if (!activity.onIsHidingHeaders()) {
-            activity.getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM,
-                    ActionBar.DISPLAY_SHOW_CUSTOM);
-            activity.getActionBar().setCustomView(mSwitch, new ActionBar.LayoutParams(
-                    ActionBar.LayoutParams.WRAP_CONTENT,
-                    ActionBar.LayoutParams.WRAP_CONTENT,
-                    Gravity.CENTER_VERTICAL | Gravity.END));
-        }
+        activity.getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM,
+                ActionBar.DISPLAY_SHOW_CUSTOM);
+        activity.getActionBar().setCustomView(mSwitch, new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                Gravity.CENTER_VERTICAL | Gravity.END));
     }
 
     @Override
     public void onStop() {
         super.onStop();
         final SettingsActivity activity = (SettingsActivity) getActivity();
-        if (!activity.onIsHidingHeaders()) {
-            activity.getActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_CUSTOM);
-            activity.getActionBar().setCustomView(null);
-        }
+        activity.getActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_CUSTOM);
+        activity.getActionBar().setCustomView(null);
     }
 
     @Override
