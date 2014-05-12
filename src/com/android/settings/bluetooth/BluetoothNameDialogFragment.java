1c1320a84d22419efb3bf45d970f80b33658edc2
Srinu Jella <sjella@codeaurora.org>
2014-05-12 19:53:12 +0530

    Bluetooth: Fix to avoid taking space as name upon orientation
    Use case: Renaming the DUT name on orientation chnage
    
    STR:
    1) Go for the option of rename phone.
    2) give only space characters.
    3) The rename option shows disabled.
    4) Change the orientation of the phone.
    5) Check whether it accepts space as a name.
    
    Failure: Renaming a phone with space characters gets
    accepted during change in orientation.Accepted
    space as BT name upon changing orientation
    
    Root cause: There is no check for validating the input
    string while renaming the device name on orientation change.
    
    Fix: This fix is to avoid taking space character as a name in the
    BluetoothNameDialogFragment upon orientation change from
    PORTRAIT mode to LANDSCAPE mode and vice versa.
    
    Change-Id: Ia66f27bb9b06739e42d73b15745c12a42512a1fc

--- src/com/android/settings/bluetooth/BluetoothNameDialogFragment.java
+++ src/com/android/settings/bluetooth/BluetoothNameDialogFragment.java
@@ -25,6 +25,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -192,6 +193,13 @@ public final class BluetoothNameDialogFragment extends DialogFragment implements
             if (mOkButton != null) {
                 mOkButton.setEnabled(s.toString().trim().length() != 0);
             }
+        }
+    }
+
+    public void onConfigurationChanged(Configuration newConfig, CharSequence s) {
+        super.onConfigurationChanged(newConfig);
+        if (mOkButton != null) {
+            mOkButton.setEnabled(s.length() != 0 && !(s.toString().trim().isEmpty()));
         }
     }
 
