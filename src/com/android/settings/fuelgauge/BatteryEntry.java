6fd48e224f89e4d4e154dcf2e494c201293cc167
Dianne Hackborn <hackbod@google.com>
2014-06-30 16:58:52 -0700

    Issue #15986092: Add power tracking of flashlight.
    Also improve drawing of date markers to allow a middle
    marker for really looong durations.
    
    Change-Id: I0c12ec9b6de102360d9171627787462dfa58b7ef

--- src/com/android/settings/fuelgauge/BatteryEntry.java
+++ src/com/android/settings/fuelgauge/BatteryEntry.java
@@ -150,6 +150,10 @@ public class BatteryEntry {
                 name = context.getResources().getString(R.string.power_screen);
                 iconId = R.drawable.ic_settings_display;
                 break;
+            case FLASHLIGHT:
+                name = context.getResources().getString(R.string.power_flashlight);
+                iconId = R.drawable.ic_settings_display;
+                break;
             case APP:
                 name = sipper.packageWithHighestDrain;
                 break;
