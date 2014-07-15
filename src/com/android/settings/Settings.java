3033ce0723103b55b1b5a53145c05e143b5bbfd6
Amith Yamasani <yamasani@google.com>
2014-07-14 18:03:15 -0700

    Handle android.settings.HOME_SETTINGS intent
    Launch the Home picker activity for the new public intent.
    
    Bug: 16303152
    Change-Id: I91cfffdec6c8ed13d3b0e57d7903b4a06dcbb6e6

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -40,6 +40,7 @@ public class Settings extends SettingsActivity {
     public static class SpellCheckersSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LocalePickerActivity extends SettingsActivity { /* empty */ }
     public static class UserDictionarySettingsActivity extends SettingsActivity { /* empty */ }
+    public static class HomeSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DisplaySettingsActivity extends SettingsActivity { /* empty */ }
     public static class DeviceInfoSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ApplicationSettingsActivity extends SettingsActivity { /* empty */ }
