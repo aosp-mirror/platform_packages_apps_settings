4d49e3f5b1736e2bdef1445c6c57809435ce8e80
Amit Mahajan <amitmahajan@google.com>
2014-10-10 14:23:22 -0700

    Fix to show signal strength as 0 if out of service.
    Bug: 17942583
    Change-Id: I399e5352b4585cff42ee5c7dd164315c1797110a

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -546,6 +546,7 @@ public class Status extends PreferenceActivity {
             if ((ServiceState.STATE_OUT_OF_SERVICE == state) ||
                     (ServiceState.STATE_POWER_OFF == state)) {
                 mSignalStrength.setSummary("0");
+                return;
             }
 
             int signalDbm = mPhoneStateReceiver.getSignalStrengthDbm();
