7781ef68703995c6724eb618c8c0c3eb10cb2a73
Dianne Hackborn <hackbod@google.com>
2015-04-04 17:48:18 -0700

    Update with new assist app ops.
    Change-Id: I796845dd1cb3ccfe951e0cbdb7cab55ec476f128

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -191,7 +191,9 @@ public class AppOpsState {
                     AppOpsManager.OP_SYSTEM_ALERT_WINDOW,
                     AppOpsManager.OP_WAKE_LOCK,
                     AppOpsManager.OP_PROJECT_MEDIA,
-                    AppOpsManager.OP_ACTIVATE_VPN, },
+                    AppOpsManager.OP_ACTIVATE_VPN,
+                    AppOpsManager.OP_ASSIST_STRUCTURE,
+                    AppOpsManager.OP_ASSIST_SCREENSHOT},
             new boolean[] { false,
                     true,
                     true,
@@ -199,7 +201,9 @@ public class AppOpsState {
                     true,
                     true,
                     false,
-                    false, }
+                    false,
+                    false,
+                    false }
             );
 
     public static final OpsTemplate[] ALL_TEMPLATES = new OpsTemplate[] {
