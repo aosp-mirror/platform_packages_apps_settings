dc868a18f80fae26ae2c472163af707ad462482f
Yorke Lee <yorkelee@google.com>
2015-04-21 10:18:51 -0700

    Add default phone app to system settings
    Bug: 20303635
    
    Change-Id: Ibf052fa92347bff49a638c4042d899c9d6587410

--- src/com/android/settings/applications/ManageDefaultApps.java
+++ src/com/android/settings/applications/ManageDefaultApps.java
@@ -26,6 +26,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.Preference;
 import android.provider.SearchIndexableResource;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -47,8 +48,9 @@ public class ManageDefaultApps extends SettingsPreferenceFragment
     private static final String TAG = ManageDefaultApps.class.getSimpleName();
 
     private static final String KEY_DEFAULT_BROWSER = "default_browser";
-    private static final String KEY_SMS_APPLICATION = "default_sms_app";
+    private static final String KEY_DEFAULT_DIALER = "default_dialer";
     private static final String KEY_DEFAULT_EMERGENCY_APP = "default_emergency_app";
+    private static final String KEY_SMS_APPLICATION = "default_sms_app";
 
     private DefaultBrowserPreference mDefaultBrowserPreference;
     private PackageManager mPm;
@@ -82,6 +84,10 @@ public class ManageDefaultApps extends SettingsPreferenceFragment
         // Remove SMS Application if the device does not support SMS
         if (isRestrictedUser || !DefaultSmsPreference.isAvailable(getActivity())) {
             removePreference(KEY_SMS_APPLICATION);
+        }
+
+        if (!DefaultDialerPreference.isAvailable(getActivity())) {
+            removePreference(KEY_DEFAULT_DIALER);
         }
 
         if (!DefaultEmergencyPreference.isAvailable(getActivity())) {
