f57bad7d5b0f9044231fc52351ed27e14013f491
John Spurlock <jspurlock@google.com>
2015-04-30 09:26:15 -0400

    Settings: Do not disturb automatic rule updates.
     - Remove FAB for adding rules, move to last preference.
     - Add first-class event condition type and config sub-page.
     - Always show radio buttons when adding rules.
     - Add new data model for event rules.
     - Add stub condition provider for event rules (always false for now).
     - Add rule-type icons to rule preference rows.
    
    Bug: 20064962
    Change-Id: Id5acde371eb2e7d22b1f195459897614db5ba80a

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -101,6 +101,7 @@ import com.android.settings.notification.NotificationAccessSettings;
 import com.android.settings.notification.NotificationSettings;
 import com.android.settings.notification.NotificationStation;
 import com.android.settings.notification.OtherSoundSettings;
+import com.android.settings.notification.ZenModeEventRuleSettings;
 import com.android.settings.notification.ZenModeExternalRuleSettings;
 import com.android.settings.notification.ZenModePrioritySettings;
 import com.android.settings.notification.ZenModeSettings;
@@ -341,6 +342,7 @@ public class SettingsActivity extends Activity
             WifiCallingSettings.class.getName(),
             ZenModePrioritySettings.class.getName(),
             ZenModeScheduleRuleSettings.class.getName(),
+            ZenModeEventRuleSettings.class.getName(),
             ZenModeExternalRuleSettings.class.getName(),
             ProcessStatsUi.class.getName(),
     };
