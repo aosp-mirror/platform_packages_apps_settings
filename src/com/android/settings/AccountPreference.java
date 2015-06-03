cb8ccbb3b5907eb6deb9fbc74fb3b3de141028fb
Jason Monk <jmonk@google.com>
2015-06-03 10:53:03 -0400

    Fix some dark theme issues
    Bug: 21117509
    Bug: 21077345
    Bug: 21480587
    Change-Id: I61bcd6631288b0e51204ac8bf134c44b2800e0fe

--- src/com/android/settings/AccountPreference.java
+++ src/com/android/settings/AccountPreference.java
@@ -114,16 +114,14 @@ public class AccountPreference extends Preference {
         int res;
         switch (status) {
             case SYNC_ENABLED:
-                res = R.drawable.ic_sync_green_holo;
+            case SYNC_IN_PROGRESS:
+                res = R.drawable.ic_settings_sync;
                 break;
             case SYNC_DISABLED:
                 res = R.drawable.ic_sync_grey_holo;
                 break;
             case SYNC_ERROR:
                 res = R.drawable.ic_sync_red_holo;
-                break;
-            case SYNC_IN_PROGRESS:
-                res = R.drawable.ic_sync_green_holo;
                 break;
             default:
                 res = R.drawable.ic_sync_red_holo;
