59c7926d4a0bd65aa9d274ff0660fe2a2ab677e2
Guang Zhu <guangzhu@google.com>
2015-07-23 10:47:40 -0700

    Prevent monkey from changing adoptable storage settings
    Bug: 22423188
    Change-Id: I36c798574c933b0339fad46af81b87a93ccea07b

--- src/com/android/settings/deviceinfo/PublicVolumeSettings.java
+++ src/com/android/settings/deviceinfo/PublicVolumeSettings.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.deviceinfo;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -81,7 +82,8 @@ public class PublicVolumeSettings extends SettingsPreferenceFragment {
 
         final Context context = getActivity();
 
-        mIsPermittedToAdopt = UserManager.get(context).isAdminUser();
+        mIsPermittedToAdopt = UserManager.get(context).isAdminUser()
+                && !ActivityManager.isUserAMonkey();
 
         mStorageManager = context.getSystemService(StorageManager.class);
 
