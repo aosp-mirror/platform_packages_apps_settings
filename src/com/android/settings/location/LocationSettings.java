def100b32fb8bad0340951fb6ca0654710bd4797
qinglian.x.zhang <qinglian.x.zhang@sonymobile.com>
2015-11-27 13:33:48 +0800

    Fix no options menu on location settings screen
    Since a help URL for location settings is not provided,
    LocationSettings fragment doesn't show options menu by default.
    This change is to show options menu even if the help URL is
    not set.
    
    Change-Id: I1f37a1bfe155db73a639a8fa214159c044385377

--- src/com/android/settings/location/LocationSettings.java
+++ src/com/android/settings/location/LocationSettings.java
@@ -120,6 +120,8 @@ public class LocationSettings extends LocationSettingsBase
         mSwitchBar = activity.getSwitchBar();
         mSwitch = mSwitchBar.getSwitch();
         mSwitchBar.show();
+
+        setHasOptionsMenu(true);
     }
 
     @Override
