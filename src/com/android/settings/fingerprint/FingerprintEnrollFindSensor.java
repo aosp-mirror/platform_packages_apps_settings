03c3705ae90352eb9045c6735519e43a1747a893
Jim Miller <jaggies@google.com>
2016-01-07 18:38:47 -0800

    Auto-advance when fingerprint touch is detected.
    Because there's no way to listen to the fingerprint
    sensor without enrolling or authenticating, we start a short
    enrollment session to detect first press of a finger to
    advance to the next stage.
    
    Change-Id: If242ade8729f34464171cda54deab0922ad85b1d

--- src/com/android/settings/fingerprint/FingerprintEnrollFindSensor.java
+++ src/com/android/settings/fingerprint/FingerprintEnrollFindSensor.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.ChooseLockSettingsHelper;
 import com.android.settings.R;
+import com.android.settings.fingerprint.FingerprintEnrollSidecar.Listener;
 
 /**
  * Activity explaining the fingerprint sensor location for fingerprint enrollment.
@@ -35,16 +36,23 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
 
     private FingerprintFindSensorAnimation mAnimation;
     private boolean mLaunchedConfirmLock;
+    private FingerprintEnrollSidecar mSidecar;
+    private boolean mNextClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fingerprint_enroll_find_sensor);
         setHeaderText(R.string.security_settings_fingerprint_enroll_find_sensor_title);
-        mLaunchedConfirmLock = savedInstanceState != null && savedInstanceState.getBoolean(
-                EXTRA_KEY_LAUNCHED_CONFIRM);
+        if (savedInstanceState != null) {
+            mLaunchedConfirmLock = savedInstanceState.getBoolean(EXTRA_KEY_LAUNCHED_CONFIRM);
+            mToken = savedInstanceState.getByteArray(
+                    ChooseLockSettingsHelper.EXTRA_KEY_CHALLENGE_TOKEN);
+        }
         if (mToken == null && !mLaunchedConfirmLock) {
             launchConfirmLock();
+        } else if (mToken != null) {
+            startLookingForFingerprint(); // already confirmed, so start looking for fingerprint
         }
         mAnimation = (FingerprintFindSensorAnimation) findViewById(
                 R.id.fingerprint_sensor_location_animation);
@@ -54,6 +62,37 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
     protected void onStart() {
         super.onStart();
         mAnimation.startAnimation();
+    }
+
+    private void startLookingForFingerprint() {
+        mSidecar = (FingerprintEnrollSidecar) getFragmentManager().findFragmentByTag(
+                FingerprintEnrollEnrolling.TAG_SIDECAR);
+        if (mSidecar == null) {
+            mSidecar = new FingerprintEnrollSidecar();
+            getFragmentManager().beginTransaction()
+                    .add(mSidecar, FingerprintEnrollEnrolling.TAG_SIDECAR).commit();
+        }
+        mSidecar.setListener(new Listener() {
+            @Override
+            public void onEnrollmentProgressChange(int steps, int remaining) {
+                mNextClicked = true;
+                if (!mSidecar.cancelEnrollment()) {
+                    proceedToEnrolling();
+                }
+            }
+
+            @Override
+            public void onEnrollmentHelp(CharSequence helpString) {
+            }
+
+            @Override
+            public void onEnrollmentError(int errMsgId, CharSequence errString) {
+                if (mNextClicked && errMsgId == FingerprintManager.FINGERPRINT_ERROR_CANCELED) {
+                    mNextClicked = false;
+                    proceedToEnrolling();
+                }
+            }
+        });
     }
 
     @Override
@@ -66,10 +105,20 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean(EXTRA_KEY_LAUNCHED_CONFIRM, mLaunchedConfirmLock);
+        outState.putByteArray(ChooseLockSettingsHelper.EXTRA_KEY_CHALLENGE_TOKEN, mToken);
     }
 
     @Override
     protected void onNextButtonClick() {
+        mNextClicked = true;
+        if (mSidecar == null || (mSidecar != null && !mSidecar.cancelEnrollment())) {
+            proceedToEnrolling();
+        }
+    }
+
+    private void proceedToEnrolling() {
+        getFragmentManager().beginTransaction().remove(mSidecar).commit();
+        mSidecar = null;
         startActivityForResult(getEnrollingIntent(), ENROLLING);
     }
 
@@ -79,6 +128,8 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
             if (resultCode == RESULT_OK) {
                 mToken = data.getByteArrayExtra(ChooseLockSettingsHelper.EXTRA_KEY_CHALLENGE_TOKEN);
                 overridePendingTransition(R.anim.suw_slide_next_in, R.anim.suw_slide_next_out);
+                getIntent().putExtra(ChooseLockSettingsHelper.EXTRA_KEY_CHALLENGE_TOKEN, mToken);
+                startLookingForFingerprint();
             } else {
                 finish();
             }
@@ -99,6 +150,9 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
                         com.android.internal.R.integer.config_fingerprintMaxTemplatesPerUser);
                 if (enrolled >= max) {
                     finish();
+                } else {
+                    // We came back from enrolling but it wasn't completed, start again.
+                    startLookingForFingerprint();
                 }
             }
         } else {
