8b85fbfe51753506f6b4eb65892648d49059a8d6
Andres Morales <anmorales@google.com>
2016-02-09 11:10:10 -0800

    fix issue when cancelling OEM unlocking change
    Bug: 26928752
    Change-Id: Ibafe9df564f1035cbd6782d8982bc3b424ee3cb4

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1702,10 +1702,18 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     }
 
     private void confirmEnableOemUnlock() {
-        DialogInterface.OnClickListener onConfirmListener = new DialogInterface.OnClickListener() {
+        DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Utils.setOemUnlockEnabled(getActivity(), true);
+                if (which == DialogInterface.BUTTON_POSITIVE) {
+                    Utils.setOemUnlockEnabled(getActivity(), true);
+                }
+            }
+        };
+
+        DialogInterface.OnDismissListener onDismissListener = new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
                 updateAllOptions();
             }
         };
@@ -1713,8 +1721,9 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.confirm_enable_oem_unlock_title)
                 .setMessage(R.string.confirm_enable_oem_unlock_text)
-                .setPositiveButton(R.string.enable_text, onConfirmListener)
+                .setPositiveButton(R.string.enable_text, onClickListener)
                 .setNegativeButton(android.R.string.cancel, null)
+                .setOnDismissListener(onDismissListener)
                 .create()
                 .show();
     }
