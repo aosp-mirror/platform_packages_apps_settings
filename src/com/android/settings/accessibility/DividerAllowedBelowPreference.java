6a2a53e94d3d807fb7bac12f4923b1e52c660f43
Anna Galusza <agalusza@google.com>
2016-03-17 12:55:14 -0700

    Add divider below summary text on A11y SUW hub. Bug: 27066012
    Change-Id: Ic545a50ec2a0b5b7a40152ea818bd15673c629d2

---
+++ src/com/android/settings/accessibility/DividerAllowedBelowPreference.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.accessibility;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+/*
+ * Preference that always has a divider below. Used for SUW Accessibility Settings Summary text.
+ */
+public class DividerAllowedBelowPreference extends Preference {
+    public DividerAllowedBelowPreference(Context context) {
+        super(context);
+    }
+
+    public DividerAllowedBelowPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DividerAllowedBelowPreference(Context context, AttributeSet attrs, int defStyleAttrs) {
+        super(context, attrs, defStyleAttrs);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        holder.setDividerAllowedBelow(true);
+    }
+}
+
