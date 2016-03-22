b28b2cc68f0f61388383d1dd75630c3b86597a8e
Abodunrinwa Toki <toki@google.com>
2016-03-22 16:29:38 +0000

    InputSettings: Physical keyboard settings activity.
    This CL adds functionality to trigger the "Physical keyboard"
    settings screen via an intent.
    
    Bug: 26943892
    Change-Id: Ib4a0be38f94f83672501d5312079d132b2ab717d

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -43,6 +43,7 @@ public class Settings extends SettingsActivity {
     public static class InputMethodAndLanguageSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AvailableVirtualKeyboardActivity extends SettingsActivity { /* empty */ }
     public static class KeyboardLayoutPickerActivity extends SettingsActivity { /* empty */ }
+    public static class PhysicalKeyboardActivity extends SettingsActivity { /* empty */ }
     public static class InputMethodAndSubtypeEnablerActivity extends SettingsActivity { /* empty */ }
     public static class SpellCheckersSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LocalePickerActivity extends SettingsActivity { /* empty */ }
