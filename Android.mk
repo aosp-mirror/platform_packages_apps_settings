4bffbdb928a5cad83387f18203b36115b47401dc
Fan Zhang <zhfan@google.com>
2016-04-11 16:25:04 +0000

    Revert "Add suggestion carousel view and dependencies to settings"
    This reverts commit 0050ac0bb759d15539c7551ea6149e521ad0cc08.
    
    Change-Id: Ic61137bae0fa1a6de5ba4970e8a3a3b4fe0469a3

--- Android.mk
+++ Android.mk
@@ -9,7 +9,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-preference \
     android-support-v7-appcompat \
     android-support-v14-preference \
-    android-support-v7-cardview \
     jsr305
 
 LOCAL_MODULE_TAGS := optional
@@ -22,8 +21,7 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res \
-    frameworks/support/v7/cardview/res
+    frameworks/support/v7/recyclerview/res
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
@@ -32,7 +30,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview:android.support.v7.cardview
+    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
