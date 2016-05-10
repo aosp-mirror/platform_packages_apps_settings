e4eb75e39b5457501d0d1330263fe0bcc0d4d44d
Jason Monk <jmonk@google.com>
2016-05-10 13:03:35 -0400

    Make sure TileAdapter always has a SuggestionParser
    Since it expects to have one
    
    Change-Id: Ic0dccd2271c011cf7dc877e341af5a8bfea90c37
    Fixes: 28689801

--- src/com/android/settings/dashboard/DashboardSummary.java
+++ src/com/android/settings/dashboard/DashboardSummary.java
@@ -178,7 +178,7 @@ public class DashboardSummary extends InstrumentedFragment
         mDashboard.setHasFixedSize(true);
         mDashboard.setListener(this);
         mDashboard.addItemDecoration(new DashboardDecorator(getContext()));
-        mAdapter = new DashboardAdapter(getContext());
+        mAdapter = new DashboardAdapter(getContext(), mSuggestionParser);
         mAdapter.setConditions(mConditionManager.getConditions());
         mDashboard.setAdapter(mAdapter);
         mSummaryLoader.setAdapter(mAdapter);
@@ -230,7 +230,7 @@ public class DashboardSummary extends InstrumentedFragment
 
         @Override
         protected void onPostExecute(List<Tile> tiles) {
-            mAdapter.setSuggestions(tiles, mSuggestionParser);
+            mAdapter.setSuggestions(tiles);
         }
     }
 }
