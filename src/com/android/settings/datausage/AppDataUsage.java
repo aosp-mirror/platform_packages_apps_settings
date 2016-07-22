2a84bbcddbd4098c3424deb827f9332311715401
Fan Zhang <zhfan@google.com>
2016-07-22 10:03:52 -0700

    Don't allow changing data usage setting if uid is not app.
    Bug: 30262890
    
    Changing non-app UID's (eg system, firewalld) data usage will just crash.
    
    Change-Id: I1c3f2504aae75f514f962fd54f4dcf240b3bcd81

--- src/com/android/settings/datausage/AppDataUsage.java
+++ src/com/android/settings/datausage/AppDataUsage.java
@@ -149,7 +149,7 @@ public class AppDataUsage extends DataUsageBase implements Preference.OnPreferen
                 } catch (PackageManager.NameNotFoundException e) {
                 }
             }
-            if (mAppItem.key == Process.SYSTEM_UID) {
+            if (!UserHandle.isApp(mAppItem.key)) {
                 removePreference(KEY_UNRESTRICTED_DATA);
                 removePreference(KEY_RESTRICT_BACKGROUND);
             } else {
