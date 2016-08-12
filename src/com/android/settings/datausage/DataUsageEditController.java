25437d7c61fe7ba34693781f9560d10a07273f50
Matthew Fritze <mfritze@google.com>
2016-08-12 09:37:43 -0700

    Data Usage summary creates a dialog to change data warning level
    Clicking the Data usage summary preference will open the same dialog
    that billing cycle does to modify data warning level. The Dialog
    was abstracted to be used outside of the Billing Cycle settings
    fragment.
    
    Test: Robo tests in next cherry-pick.
    
    Change-Id: I70398d4656b6da4233b2bfa0da6ffa7402ead69b
    Fixes: 26932938
    (cherry picked from commit 9fad289ca95d2ce0c44d01e0668bdaffba4404d9)

---
+++ src/com/android/settings/datausage/DataUsageEditController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.android.settings.datausage;
+
+import android.net.NetworkTemplate;
+import com.android.settingslib.NetworkPolicyEditor;
+
+/**
+ * Used to create a dialog that modifies the Cellular data warning amount.
+ */
+public interface DataUsageEditController {
+    /**
+     * @return NetworkPolicyEditor to update the values of the data warning and usage limits.
+     */
+    NetworkPolicyEditor getNetworkPolicyEditor();
+
+    /**
+     * @return NetworkTemplate to get the currently set values of the data warning and usage limits.
+     */
+    NetworkTemplate getNetworkTemplate();
+
+    /**
+     * Callback to update the UI and values changed by the Dialog.
+     */
+    void updateDataUsage();
+}
