d6b1c666c56e15c6806d891002c6abd8ed81aa2d
Udam Saini <udam@google.com>
2016-11-01 11:20:14 -0700

    Can launch into the lock screen with setup wizard style
    The trampoline currently always uses the settings style for set action
    new password. When called during setup wizard, it should launch the
    setup wizard style.
    
    Test: Added robotests for launching activity and copying setup wizard
    extras. Also manually tested with an application when device is
    provisioned and not provisioned.
    
    bug:32575389
    Change-Id: I5763eb87b63a46b05cd200bb73b15bdc24c8bd3b

---
+++ tests/robotests/src/com/android/settings/SetupWizardUtilsTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings;
+
+import android.content.Intent;
+
+import com.android.settings.SettingsRobolectricTestRunner;
+import com.android.settings.TestConfig;
+import com.android.setupwizardlib.util.WizardManagerHelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(SettingsRobolectricTestRunner.class)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+public class SetupWizardUtilsTest {
+
+    @Test
+    public void testCopySetupExtras() throws Throwable {
+        Intent fromIntent = new Intent();
+        final String theme = "TEST_THEME";
+        fromIntent.putExtra(WizardManagerHelper.EXTRA_THEME, theme);
+        fromIntent.putExtra(WizardManagerHelper.EXTRA_USE_IMMERSIVE_MODE, true);
+        Intent toIntent = new Intent();
+        SetupWizardUtils.copySetupExtras(fromIntent, toIntent);
+
+        assertThat(theme).isEqualTo(toIntent.getStringExtra(WizardManagerHelper.EXTRA_THEME));
+        assertThat(toIntent.getBooleanExtra(WizardManagerHelper.EXTRA_USE_IMMERSIVE_MODE, false))
+                .isTrue();
+    }
+}
