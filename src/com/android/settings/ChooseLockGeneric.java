79b4ebffebb48449d5d2126795f6c729eea25ec4
Rubin Xu <rubinxu@google.com>
2017-01-04 17:31:53 +0000

    Deprecate old LockPatternUtils.clearLock()
    New clearLock() requires current password to be supplied
    
    Test: (depends on escrow token CL), add escrow token, clear lockscreen;
          verify escrow token is still usable.
    Bug: 33126414
    Change-Id: Ib248ee2ba8fa2743b038091253fd41401cfa3579

--- src/com/android/settings/ChooseLockGeneric.java
+++ src/com/android/settings/ChooseLockGeneric.java
@@ -675,7 +675,7 @@ public class ChooseLockGeneric extends SettingsActivity {
 
             if (quality == DevicePolicyManager.PASSWORD_QUALITY_UNSPECIFIED) {
                 mLockPatternUtils.setSeparateProfileChallengeEnabled(mUserId, true, mUserPassword);
-                mChooseLockSettingsHelper.utils().clearLock(mUserId);
+                mChooseLockSettingsHelper.utils().clearLock(mUserPassword, mUserId);
                 mChooseLockSettingsHelper.utils().setLockScreenDisabled(disabled, mUserId);
                 getActivity().setResult(Activity.RESULT_OK);
                 removeAllFingerprintForUserAndFinish(mUserId);
