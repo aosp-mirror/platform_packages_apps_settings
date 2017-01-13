73dd0ae30e2779cb845d1ac6e785d619e1e1e310
Steven Moreland <smoreland@google.com>
2017-01-13 11:31:41 -0800

    Development Options: toggle binderized hals
    This allows for quick performance testing and also for a better
    dogfooding experience.
    
    Bug: 34256441
    Test: Verification with getprop.
    Change-Id: I81c5e0b1044d4569a6fe55a4b30f7e21c4465fc2

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -222,6 +222,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private static final String KEY_CONVERT_FBE = "convert_to_file_encryption";
 
     private static final String OTA_DISABLE_AUTOMATIC_UPDATE_KEY = "ota_disable_automatic_update";
+    private static final String ENABLE_HAL_BINDERIZATION_KEY = "enable_hal_binderization";
+    private static final String ENABLE_HAL_BINDERIZATION_PROPERTY = "persist.hal.binderization";
 
     private static final int RESULT_DEBUG_APP = 1000;
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
@@ -284,6 +286,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private ListPreference mBluetoothSelectA2dpLdacPlaybackQuality;
 
     private SwitchPreference mOtaDisableAutomaticUpdate;
+    private SwitchPreference mEnableHalBinderization;
     private SwitchPreference mWifiAllowScansWithTraffic;
     private SwitchPreference mStrictMode;
     private SwitchPreference mPointerLocation;
@@ -522,6 +525,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
         mOtaDisableAutomaticUpdate = findAndInitSwitchPref(OTA_DISABLE_AUTOMATIC_UPDATE_KEY);
 
+        mEnableHalBinderization = findAndInitSwitchPref(ENABLE_HAL_BINDERIZATION_KEY);
+
         mColorModePreference = (ColorModePreference) findPreference(KEY_COLOR_MODE);
         mColorModePreference.updateCurrentAndSupported();
         if (mColorModePreference.getColorModeCount() < 2) {
@@ -751,6 +756,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         updateShowAllANRsOptions();
         updateVerifyAppsOverUsbOptions();
         updateOtaDisableAutomaticUpdateOptions();
+        updateEnableHalBinderizationOptions();
         updateBugreportOptions();
         updateForceRtlOptions();
         updateLogdSizeValues();
@@ -1038,6 +1044,17 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         Settings.Global.putInt(getActivity().getContentResolver(),
                 Settings.Global.OTA_DISABLE_AUTOMATIC_UPDATE,
                 mOtaDisableAutomaticUpdate.isChecked() ? 0 : 1);
+    }
+
+    private void updateEnableHalBinderizationOptions() {
+        updateSwitchPreference(mEnableHalBinderization,
+                SystemProperties.getBoolean(ENABLE_HAL_BINDERIZATION_PROPERTY, false));
+    }
+
+    private void writeEnableHalBinderizationOptions() {
+        SystemProperties.set(ENABLE_HAL_BINDERIZATION_PROPERTY,
+                             mEnableHalBinderization.isChecked() ? "true" : "false");
+        pokeSystemProperties();
     }
 
     private boolean enableVerifierSetting() {
@@ -2398,6 +2415,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             writeVerifyAppsOverUsbOptions();
         } else if (preference == mOtaDisableAutomaticUpdate) {
             writeOtaDisableAutomaticUpdateOptions();
+        } else if (preference == mEnableHalBinderization) {
+            writeEnableHalBinderizationOptions();
         } else if (preference == mStrictMode) {
             writeStrictModeVisualOptions();
         } else if (preference == mPointerLocation) {
