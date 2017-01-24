bb6859ca751e5813685ef8a71bebe22381ce575b
Christine Franks <christyfranks@google.com>
2017-01-23 16:44:02 -0800

    DO NOT MERGE - Allow FR in carrier demo mode
    Bug: 34341567
    Test: manual - in carrier demo mode, go to Settings > Backup and
    Reset > Factory data reset > Reset phone > Erase everything and
    this entire path is viewable.
    
    Change-Id: I373a2e94f205f6e6e4dd9a54dcf08532a64e5187

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1134,7 +1134,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             }
         }
         setTileEnabled(new ComponentName(packageName,
-                BackupSettingsActivity.class.getName()), hasBackupActivity, isAdmin, pm);
+                BackupSettingsActivity.class.getName()), hasBackupActivity,
+                isAdmin || Utils.isCarrierDemoUser(this), pm);
 
     }
 
