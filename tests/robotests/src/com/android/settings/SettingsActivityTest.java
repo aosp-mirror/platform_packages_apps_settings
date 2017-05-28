048f6fb8b32ebf71f1fb9da06fce57f2c3194309
Andrew Sapperstein <asapperstein@google.com>
2017-05-28 12:20:10 -0700

    Initial search bar implementation.
    Replaces the default Toolbar in SettingsActivity with one that looks
    like a search bar. It uses a Toolbar inside a CardView with some custom
    styling.
    
    Since the search bar is a floating element, the new toolbar lives in the
    content frame of the dashboard. A FrameLayout is used to provide the
    layering that is desired.
    
    Since the search bar is on top, an additional spacer view is added to
    the list of items in the dashboard. Its color changes based on what
    the first view is so that it always matches.
    
    Adds android-support-v7-cardview as a dependency (and reorders the
    other deps to be in alphabetical order).
    
    Remaining work (in future CLs):
    - remove search menu option?
    - clean up initial window
    - remove the line between the header and the first condition
           when there's a condition
    
    Bug: 37477506
    Test: make RunSettingsRoboTests
    Change-Id: Id7477b90fbaf30eb5cac1ee244c847bddb95b3fd

--- tests/robotests/src/com/android/settings/SettingsActivityTest.java
+++ tests/robotests/src/com/android/settings/SettingsActivityTest.java
@@ -30,12 +30,14 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.android.settings.search.SearchActivity;
 import com.android.settings.testutils.FakeFeatureFactory;
 
 import org.junit.Before;
@@ -46,6 +48,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowApplication;
 import org.robolectric.util.ReflectionHelpers;
 
 @RunWith(SettingsRobolectricTestRunner.class)
@@ -139,5 +142,16 @@ public class SettingsActivityTest {
         mActivity.onRestoreInstanceState(bundle);
 
         assertThat(mActivity.mDisplaySearch).isTrue();
+    }
+
+    @Test
+    public void testOnClick() {
+        doReturn("com.android.settings").when(mActivity).getPackageName();
+
+        mActivity.onClick(null);
+
+        Intent intent = ShadowApplication.getInstance().getNextStartedActivity();
+        assertThat(intent.getComponent()).isEqualTo(
+                new ComponentName("com.android.settings", SearchActivity.class.getName()));
     }
 }
