e76550a66e92e072aa1afa20a2bed7e8a6bd7924
Doris Ling <dling@google.com>
2017-06-21 14:43:48 -0700

    Add account sync summary.
    - Check for the number of sync adapter that is enabled for the account and
    update the summary text for the account sync preference accordingly.
    - Add divider above the Remove Account button.
    
    Merged-In: Ic333f62cce9aed0a72771324976ebe3d8e586287
    Change-Id: I003d6dd0a070bf123f178b8d9ba7b6657466b905
    Fix: 62862167
    Test: make RunSettingsRoboTests

--- tests/robotests/src/com/android/settings/accounts/AccountSyncPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/accounts/AccountSyncPreferenceControllerTest.java
@@ -16,11 +16,17 @@
 package com.android.settings.accounts;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.mock;
+
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AuthenticatorDescription;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SyncAdapterType;
 import android.os.UserHandle;
 import android.support.v7.preference.Preference;
 
@@ -28,29 +34,58 @@ import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.testutils.SettingsRobolectricTestRunner;
 import com.android.settings.TestConfig;
+import com.android.settings.testutils.shadow.ShadowAccountManager;
+import com.android.settings.testutils.shadow.ShadowContentResolver;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowApplication;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION,
+        shadows = {ShadowAccountManager.class, ShadowContentResolver.class})
 public class AccountSyncPreferenceControllerTest {
+
+    @Mock(answer = RETURNS_DEEP_STUBS)
+    private AccountManager mAccountManager;
+
+    private Context mContext;
+    private AccountSyncPreferenceController mController;
+    private Preference mPreference;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        ShadowApplication application = ShadowApplication.getInstance();
+        application.setSystemService(Context.ACCOUNT_SERVICE, mAccountManager);
+        mContext = application.getApplicationContext();
+
+        when(mAccountManager.getAuthenticatorTypesAsUser(anyInt())).thenReturn(
+                new AuthenticatorDescription[0]);
+        when(mAccountManager.getAccountsAsUser(anyInt())).thenReturn(new Account[0]);
+
+        mPreference = new Preference(mContext);
+        mPreference.setKey("account_sync");
+
+        mController = new AccountSyncPreferenceController(mContext);
+        mController.init(new Account("acct1", "type1"), new UserHandle(3));
+    }
+
+    @After
+    public void tearDown() {
+        ShadowContentResolver.reset();
+    }
 
     @Test
     public void handlePreferenceTreeClick_shouldStartFragment() {
-        final ShadowApplication application = ShadowApplication.getInstance();
-        final Context context = application.getApplicationContext();
-        final Preference preference = new Preference(context);
-        preference.setKey("account_sync");
+        mController.handlePreferenceTreeClick(mPreference);
 
-        final AccountSyncPreferenceController controller =
-                new AccountSyncPreferenceController(context);
-        controller.init(new Account("acct1", "type1"), mock(UserHandle.class));
-        controller.handlePreferenceTreeClick(preference);
-
-        final Intent nextActivity = application.getNextStartedActivity();
+        final Intent nextActivity = ShadowApplication.getInstance().getNextStartedActivity();
 
         assertThat(nextActivity.getStringExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT))
                 .isEqualTo(AccountSyncSettings.class.getName());
@@ -58,4 +93,93 @@ public class AccountSyncPreferenceControllerTest {
                 .isEqualTo(R.string.account_sync_title);
     }
 
+    @Test
+    public void updateSummary_adapterInvisible_shouldNotCount() {
+        SyncAdapterType syncAdapterType = new SyncAdapterType("authority" /* authority */,
+                "type1" /* accountType */, false /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType[] syncAdapters = {syncAdapterType};
+        ShadowContentResolver.setSyncAdapterTypes(syncAdapters);
+
+        mController.updateSummary(mPreference);
+
+        assertThat(mPreference.getSummary())
+                .isEqualTo(mContext.getString(R.string.account_sync_summary_all_off));
+    }
+
+    @Test
+    public void updateSummary_notSameAccountType_shouldNotCount() {
+        SyncAdapterType syncAdapterType = new SyncAdapterType("authority" /* authority */,
+                "type5" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType[] syncAdapters = {syncAdapterType};
+        ShadowContentResolver.setSyncAdapterTypes(syncAdapters);
+
+        mController.updateSummary(mPreference);
+
+        assertThat(mPreference.getSummary())
+                .isEqualTo(mContext.getString(R.string.account_sync_summary_all_off));
+    }
+
+    @Test
+    public void updateSummary_notSyncable_shouldNotCount() {
+        SyncAdapterType syncAdapterType = new SyncAdapterType("authority" /* authority */,
+                "type1" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType[] syncAdapters = {syncAdapterType};
+        ShadowContentResolver.setSyncAdapterTypes(syncAdapters);
+        ShadowContentResolver.setSyncable("authority", 0);
+
+        mController.updateSummary(mPreference);
+
+        assertThat(mPreference.getSummary())
+                .isEqualTo(mContext.getString(R.string.account_sync_summary_all_off));
+    }
+
+    @Test
+    public void updateSummary_syncDisabled_shouldNotCount() {
+        SyncAdapterType syncAdapterType = new SyncAdapterType("authority" /* authority */,
+                "type1" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType[] syncAdapters = {syncAdapterType};
+        ShadowContentResolver.setSyncAdapterTypes(syncAdapters);
+        ShadowContentResolver.setSyncAutomatically("authority", false);
+        ShadowContentResolver.setMasterSyncAutomatically(3, true);
+
+        mController.updateSummary(mPreference);
+
+        assertThat(mPreference.getSummary())
+                .isEqualTo(mContext.getString(R.string.account_sync_summary_all_off));
+    }
+
+    @Test
+    public void updateSummary_syncEnabled_shouldCount() {
+        SyncAdapterType syncAdapterType = new SyncAdapterType("authority" /* authority */,
+                "type1" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType[] syncAdapters = {syncAdapterType};
+        ShadowContentResolver.setSyncAdapterTypes(syncAdapters);
+
+        mController.updateSummary(mPreference);
+
+        assertThat(mPreference.getSummary())
+                .isEqualTo(mContext.getString(R.string.account_sync_summary_all_on));
+    }
+
+    @Test
+    public void updateSummary_multipleSyncAdapters_shouldSetSummary() {
+        SyncAdapterType syncAdapterType1 = new SyncAdapterType("authority1" /* authority */,
+                "type1" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType syncAdapterType2 = new SyncAdapterType("authority2" /* authority */,
+                "type1" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType syncAdapterType3 = new SyncAdapterType("authority3" /* authority */,
+                "type1" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType syncAdapterType4 = new SyncAdapterType("authority4" /* authority */,
+                "type1" /* accountType */, true /* userVisible */, true /* supportsUploading */);
+        SyncAdapterType[] syncAdapters =
+                {syncAdapterType1, syncAdapterType2, syncAdapterType3, syncAdapterType4};
+        ShadowContentResolver.setSyncAdapterTypes(syncAdapters);
+
+        ShadowContentResolver.setSyncAutomatically("authority4", false);
+
+        mController.updateSummary(mPreference);
+
+        assertThat(mPreference.getSummary())
+                .isEqualTo(mContext.getString(R.string.account_sync_summary_some_on, 3, 4));
+    }
 }
