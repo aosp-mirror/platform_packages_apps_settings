bc393be97791938e7b30b1e7243cbda8f06d892b
Rajeev Kumar <rajekumar@google.com>
2017-07-06 17:19:50 -0700

    Some import cleanups.
    1. Get rid of unused import.
    2. Fix import order.
    
    Change-Id: I278949d95242682fd277adc2c71bc892e58a8533

--- src/com/android/settings/datausage/DataUsagePreference.java
+++ src/com/android/settings/datausage/DataUsagePreference.java
@@ -19,14 +19,12 @@ import android.content.Intent;
 import android.net.NetworkTemplate;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
-import android.telephony.SubscriptionManager;
 import android.text.format.Formatter;
 import android.util.AttributeSet;
-
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settingslib.net.DataUsageController;
-import com.android.settings.R;
 
 public class DataUsagePreference extends Preference implements TemplatePreference {
 
