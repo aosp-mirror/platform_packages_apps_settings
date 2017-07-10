88a9e4cc199028974faebf318bc5a425ff0c04ea
jackqdyulei <jackqdyulei@google.com>
2017-07-10 15:15:33 -0700

    Remove obsolete bluetooth pages
    1. Remove BluetoothSettingsObsolete
    2. Remove DeviceListPreferenceObsoleteFragment
    3. Update master swtich, now constantly point to new page
    4. Update the tests
    
    Future cl will remove the page switch API in BluetoothFeatureProvider
    
    Bug: 63444548
    Test: Test still pass
    Change-Id: I24fb5cd03cf30044edb7201426e11e0a818f0a7f

--- src/com/android/settings/bluetooth/BluetoothMasterSwitchPreferenceController.java
+++ src/com/android/settings/bluetooth/BluetoothMasterSwitchPreferenceController.java
@@ -86,11 +86,8 @@ public class BluetoothMasterSwitchPreferenceController extends AbstractPreferenc
     @Override
     public boolean handlePreferenceTreeClick(Preference preference) {
         if (KEY_TOGGLE_BLUETOOTH.equals(preference.getKey())) {
-            final String fragmentClass = mBluetoothFeatureProvider.isPairingPageEnabled() ?
-                    BluetoothSettings.class.getName() :
-                    BluetoothSettingsObsolete.class.getName();
-            mActivity.startPreferencePanelAsUser(mFragment, fragmentClass, null, R.string.bluetooth,
-                    null, new UserHandle(UserHandle.myUserId()));
+            mActivity.startPreferencePanelAsUser(mFragment, BluetoothSettings.class.getName(), null,
+                    R.string.bluetooth, null, new UserHandle(UserHandle.myUserId()));
             return true;
         }
         return super.handlePreferenceTreeClick(preference);
