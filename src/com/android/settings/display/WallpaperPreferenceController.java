4427cd7e22b83d6c2db8ed7c134e6f0bd837daf0
Doris Ling <dling@google.com>
2017-07-25 10:38:22 -0700

    Disable Wallpaper setting when Wallpaper is disabled.
    Check whether the wallpaper picker is enabled in
    WallpaperPreferenceController.isAvailable() instead of always returning
    true.
    
    Bug: 63939450
    Test: make RunSettingsRoboTests
    Change-Id: Ie3a4a68b728ccab1a7aa50c0018a5153907c49b4

--- src/com/android/settings/display/WallpaperPreferenceController.java
+++ src/com/android/settings/display/WallpaperPreferenceController.java
@@ -13,10 +13,17 @@
  */
 package com.android.settings.display;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import android.support.v7.preference.Preference;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedPreference;
@@ -24,18 +31,38 @@ import com.android.settingslib.core.AbstractPreferenceController;
 
 import static android.os.UserManager.DISALLOW_SET_WALLPAPER;
 
+import java.util.List;
+
 public class WallpaperPreferenceController extends AbstractPreferenceController implements
         PreferenceControllerMixin {
 
+    private static final String TAG = "WallpaperPrefController";
+
     public static final String KEY_WALLPAPER = "wallpaper";
+
+    private final String mWallpaperPackage;
+    private final String mWallpaperClass;
 
     public WallpaperPreferenceController(Context context) {
         super(context);
+        mWallpaperPackage = mContext.getString(R.string.config_wallpaper_picker_package);
+        mWallpaperClass = mContext.getString(R.string.config_wallpaper_picker_class);
     }
 
     @Override
     public boolean isAvailable() {
-        return true;
+        if (TextUtils.isEmpty(mWallpaperPackage) || TextUtils.isEmpty(mWallpaperClass)) {
+            Log.e(TAG, "No Wallpaper picker specified!");
+            return false;
+        }
+        final ComponentName componentName =
+                new ComponentName(mWallpaperPackage, mWallpaperClass);
+        final PackageManager pm = mContext.getPackageManager();
+        final Intent intent = new Intent();
+        intent.setComponent(componentName);
+        final List<ResolveInfo> resolveInfos =
+                pm.queryIntentActivities(intent, 0 /* flags */);
+        return resolveInfos != null && resolveInfos.size() != 0;
     }
 
     @Override
