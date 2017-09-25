c215b3c4e96ee23f2ab449158c1e887021aa8c70
jeffreyhuang <jeffreyhuang@google.com>
2017-09-25 10:19:51 -0700

    Introduce CameraLaserSensorPreferenceControllerV2
     - Create new CameraLaserSensorPreferenceControllerV2
     - Copy logic from CameraLaserSensorPreferenceController with slight
     modifications for dashboard fragment compatibility
     - Deprecate CameraLaserSensorPreferenceController
    
    Bug: 34203528
    Test: make RunSettingsRoboTests -j40
    Change-Id: I8854d3e891afab607025a427a376e33e9b61dcc3

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -199,7 +199,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         // logger buffer sizes
         // store logger data persistently on device
         // telephony monitor
-        // camera laser sensor
+        controllers.add(new CameraLaserSensorPreferenceControllerV2(context));
         // camera HAL HDR+
         // feature flags
         // wireless display certification
