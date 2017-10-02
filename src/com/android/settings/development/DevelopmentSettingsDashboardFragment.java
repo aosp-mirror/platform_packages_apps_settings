a19a24930b6257b82676f959ad7f9745b24f2628
jeffreyhuang <jeffreyhuang@google.com>
2017-10-02 13:03:34 -0700

    Introduce ResizableActivityPreferenceController
     - Create new ResizableActivityPreferenceController
     - Create controller inside the DashboardFragment
     - Port logic from DevelopmentSettings into the controller
    
    Bug: 34203528
    Test: make RunSettingsRoboTests -j40
    Change-Id: I401db1fe3a051ee70ab9afe2bf8c377b3b0b4e2f

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -311,7 +311,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         // show notification channel warnings
         // inactive apps
         controllers.add(new AllowAppsOnExternalPreferenceController(context));
-        // force activities to be resizable
+        controllers.add(new ResizableActivityPreferenceController(context));
         // reset shortcutmanager rate-limiting
         return controllers;
     }
