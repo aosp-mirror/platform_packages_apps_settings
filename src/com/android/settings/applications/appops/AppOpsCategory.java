f68e6ec2ae7b76946fd49c6b79f9716f603f01db
Fan Zhang <zhfan@google.com>
2017-10-28 14:11:44 -0700

    Move app ops classes to their own package
    Bug: 68426851
    Test: robotests
    Change-Id: I076b7adf59d4bf66792eb0aebaa3dc305d6d0c4b

--- src/com/android/settings/applications/appops/AppOpsCategory.java
+++ src/com/android/settings/applications/appops/AppOpsCategory.java
@@ -1,20 +1,20 @@
-/**
+/*
  * Copyright (C) 2013 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package com.android.settings.applications;
+package com.android.settings.applications.appops;
 
 import android.app.AppOpsManager;
 import android.app.ListFragment;
@@ -39,7 +39,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 import com.android.settings.R;
-import com.android.settings.applications.AppOpsState.AppOpEntry;
+import com.android.settings.applications.appops.AppOpsState.AppOpEntry;
 
 import java.util.List;
 
