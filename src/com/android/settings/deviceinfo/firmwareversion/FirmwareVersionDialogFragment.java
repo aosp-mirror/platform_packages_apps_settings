3dec170661928370860e1aa198e3aa0f27a8f19d
jeffreyhuang <jeffreyhuang@google.com>
2017-11-02 11:01:18 -0700

    Create a dialog for firmware version
     - create xml layout files for dialog
     - create some new strings
     - create new FirmwareVersionPreferenceControllerV2
     - create a new dialog fragment for firmware version
    
    Bug: 36458278
    Test: make RunSettingsRoboTests -j40
    Change-Id: Icc567ff6aa1eedc1ab02c20e0d22529457c33466

---
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDialogFragment.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
+
+public class FirmwareVersionDialogFragment extends InstrumentedDialogFragment {
+
+    private static final String TAG = "firmwareVersionDialog";
+
+    public static void show(Fragment host) {
+        final FragmentManager manager = host.getChildFragmentManager();
+        if (manager.findFragmentByTag(TAG) == null) {
+            final FirmwareVersionDialogFragment dialog = new FirmwareVersionDialogFragment();
+            dialog.show(manager, TAG);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DIALOG_FIRMWARE_VERSION;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.firmware_title)
+                .setPositiveButton(android.R.string.ok, null /* listener */);
+
+        final View view = LayoutInflater.from(getActivity()).inflate(
+                R.layout.dialog_firmware_version, null /* parent */);
+
+        return builder.setView(view).create();
+    }
+}
