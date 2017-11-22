65304af2159e5881a4d869423732ffe828d5de3d
jeffreyhuang <jeffreyhuang@google.com>
2017-11-22 14:14:10 -0800

    Fix AutoSyncDataPreferenceControllerTest
     - Prevent this test from hanging due to fragment deep mock
    
    Test: make RunSettingsRoboTests -j40
    Change-Id: I043303c87eb89508fd96e151a17dc70a7b7fa3bb

--- tests/robotests/src/com/android/settings/accounts/AutoSyncDataPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/accounts/AutoSyncDataPreferenceControllerTest.java
@@ -44,14 +44,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public class AutoSyncDataPreferenceControllerTest {
 
     @Mock(answer = RETURNS_DEEP_STUBS)
     private PreferenceScreen mScreen;
     @Mock(answer = RETURNS_DEEP_STUBS)
     private UserManager mUserManager;
-    @Mock(answer = RETURNS_DEEP_STUBS)
+    @Mock
     private Fragment mFragment;
 
     private Preference mPreference;
