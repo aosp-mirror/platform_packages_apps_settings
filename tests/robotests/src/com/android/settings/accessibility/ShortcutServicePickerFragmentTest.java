cafc84d2d0d7ce45b5affc114398f73e2e755d9b
jeffreyhuang <jeffreyhuang@google.com>
2017-11-22 11:21:29 -0800

    Move accessibility package to sdk 26
    Test: make RunSettingsRoboTests -j40
    Change-Id: I6fa70ec4da82e29934a21cd5780175aa510b5d62

--- tests/robotests/src/com/android/settings/accessibility/ShortcutServicePickerFragmentTest.java
+++ tests/robotests/src/com/android/settings/accessibility/ShortcutServicePickerFragmentTest.java
@@ -41,7 +41,7 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public class ShortcutServicePickerFragmentTest {
 
     private static final String TEST_SERVICE_KEY_1 = "abc/123";
