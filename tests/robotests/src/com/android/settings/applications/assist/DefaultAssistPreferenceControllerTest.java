32c3f96c231f50990b369d9aeb65802ef552d386
jeffreyhuang <jeffreyhuang@google.com>
2017-11-27 10:21:47 -0800

    Move assist package to sdk 26
    Test: make RunSettingsRoboTests -j40
    Change-Id: Id37a5b2e445481bc586f12a4397e7082f28ec075

--- tests/robotests/src/com/android/settings/applications/assist/DefaultAssistPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/applications/assist/DefaultAssistPreferenceControllerTest.java
@@ -56,7 +56,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public class DefaultAssistPreferenceControllerTest {
 
     private static final String TEST_KEY = "test_pref_key";
