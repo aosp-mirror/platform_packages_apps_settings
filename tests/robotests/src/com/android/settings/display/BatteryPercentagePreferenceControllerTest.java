adc9d7fb4eb64ac7778a3999a893708e04af7607
jeffreyhuang <jeffreyhuang@google.com>
2017-11-28 13:41:30 -0800

    Move display test package to sdk 26
    Test: make RunSettingsRoboTests -j40
    Change-Id: I88978ab800e845eda6c937bf890016d53928d4fd

--- tests/robotests/src/com/android/settings/display/BatteryPercentagePreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/display/BatteryPercentagePreferenceControllerTest.java
@@ -31,7 +31,7 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public class BatteryPercentagePreferenceControllerTest {
     @Mock private Context mContext;
     private BatteryPercentagePreferenceController mController;
--- tests/robotests/src/com/android/settings/display/ShowOperatorNamePreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/display/ShowOperatorNamePreferenceControllerTest.java
@@ -36,7 +36,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public class ShowOperatorNamePreferenceControllerTest {
 
     private static final String KEY_SHOW_OPERATOR_NAME = "show_operator_name";
