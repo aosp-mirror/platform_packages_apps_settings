1ae1e171993bfc521636b9a664d1aee5c0e5abe9
Fan Zhang <zhfan@google.com>
2017-11-28 16:52:53 -0800

    Fix indexing in cast and zen mode settings
    - Fragment should either not implement serach provider or return a valid
      xml. Search provider with 0 resource is invalid.
    
    Change-Id: Ie87c739bf72c926cecf48d271c6c2d72459787c4
    Fixes: 69864274
    Test: robotests

--- src/com/android/settings/notification/ZenModeAutomationSettings.java
+++ src/com/android/settings/notification/ZenModeAutomationSettings.java
@@ -18,6 +18,7 @@ package com.android.settings.notification;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.provider.SearchIndexableResource;
 import android.service.notification.ConditionProviderService;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -78,17 +79,30 @@ public class ZenModeAutomationSettings extends ZenModeSettingsBase {
      */
     public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
-        @Override
-        public List<String> getNonIndexableKeys(Context context) {
-            final List<String> keys = super.getNonIndexableKeys(context);
-            keys.add(KEY_ADD_RULE);
-            keys.add(KEY_AUTOMATIC_RULES);
-            return keys;
-        }
 
-        @Override
-        public List<AbstractPreferenceController> getPreferenceControllers(Context context) {
-            return buildPreferenceControllers(context, null, null);
-        }
-    };
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.zen_mode_automation_settings;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    keys.add(KEY_ADD_RULE);
+                    keys.add(KEY_AUTOMATIC_RULES);
+                    return keys;
+                }
+
+                @Override
+                public List<AbstractPreferenceController> getPreferenceControllers(
+                        Context context) {
+                    return buildPreferenceControllers(context, null, null);
+                }
+            };
 }
