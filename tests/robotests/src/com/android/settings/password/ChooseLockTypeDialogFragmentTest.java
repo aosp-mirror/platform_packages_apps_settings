a442405a0e2f82ff4c8993f577c82eda196547e4
jeffreyhuang <jeffreyhuang@google.com>
2017-11-30 15:43:42 -0800

    Move password test package to sdk 26
    Test: make RunSettingsRoboTests -j40
    Change-Id: I37c5df89dc4236a7d225c4802bf4195f2fd943b7

--- tests/robotests/src/com/android/settings/password/ChooseLockTypeDialogFragmentTest.java
+++ tests/robotests/src/com/android/settings/password/ChooseLockTypeDialogFragmentTest.java
@@ -49,7 +49,7 @@ import org.robolectric.util.FragmentTestUtil;
 @RunWith(SettingsRobolectricTestRunner.class)
 @Config(
         manifest = TestConfig.MANIFEST_PATH,
-        sdk = TestConfig.SDK_VERSION,
+        sdk = TestConfig.SDK_VERSION_O,
         shadows = {
                 ShadowEventLogWriter.class,
                 ShadowUserManager.class,
--- tests/robotests/src/com/android/settings/password/SetNewPasswordControllerTest.java
+++ tests/robotests/src/com/android/settings/password/SetNewPasswordControllerTest.java
@@ -57,7 +57,7 @@ import org.robolectric.annotation.Config;
  * Tests for {@link SetNewPasswordController}.
  */
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public final class SetNewPasswordControllerTest {
     private static final int CURRENT_USER_ID = 101;
     private static final long FINGERPRINT_CHALLENGE = -9876512313131L;
