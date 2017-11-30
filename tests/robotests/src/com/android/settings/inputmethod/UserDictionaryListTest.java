153d14b0f35cbabdca86671b0f1850bb7fba6086
jeffreyhuang <jeffreyhuang@google.com>
2017-11-30 12:28:14 -0800

    move inputmethod test package to sdk 26
    Test: make RunSettingsRoboTests -j40
    Change-Id: I4d0435d8833bf1ff767688109e239c6729fdead9

--- tests/robotests/src/com/android/settings/inputmethod/UserDictionaryListTest.java
+++ tests/robotests/src/com/android/settings/inputmethod/UserDictionaryListTest.java
@@ -37,7 +37,7 @@ import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowContentResolver;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public class UserDictionaryListTest {
 
     private FakeProvider mContentProvider;
--- tests/robotests/src/com/android/settings/inputmethod/VirtualKeyboardPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/inputmethod/VirtualKeyboardPreferenceControllerTest.java
@@ -49,7 +49,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SettingsRobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
+@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION_O)
 public class VirtualKeyboardPreferenceControllerTest {
 
     @Mock
