e4ca92af6b367f162320fcdb899606bf7798ccb8
Ben Lin <linben@google.com>
2017-12-12 16:44:53 -0800

    Introduce config_show_wallpaper_attribution.
    This introduces a boolean flag in which when set to true, Wallpaper
    Attribution will be shown in the Legal page. When set to false, it
    will be hidden. Wallpaper has always been removed from search results,
    so that has not changed.
    
    Bug: 62378616
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=LegalSettings passes.
    Change-Id: Ia6f3e7d1ef471eecf79f1b46616fa4ba27d35748

--- tests/robotests/src/com/android/settings/LegalSettingsTest.java
+++ tests/robotests/src/com/android/settings/LegalSettingsTest.java
@@ -15,22 +15,48 @@
  */
 package com.android.settings;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+
 import android.content.Context;
 
 import com.android.settings.testutils.SettingsRobolectricTestRunner;
 import com.android.settings.testutils.XmlTestUtils;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import java.util.List;
 
-import static com.google.common.truth.Truth.assertThat;
-
 @RunWith(SettingsRobolectricTestRunner.class)
 @Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
 public class LegalSettingsTest {
+
+    private Context mContext;
+    private LegalSettings mFragment;
+    private boolean mWallpaperRemoved;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mContext = spy(RuntimeEnvironment.application);
+        mFragment = new LegalSettings() {
+            @Override
+            protected boolean removePreference(String key) {
+                if (LegalSettings.KEY_WALLPAPER_ATTRIBUTIONS.equals(key)) {
+                    mWallpaperRemoved = true;
+
+                    return true;
+                }
+                return false;
+            }
+        };
+    }
 
     @Test
     public void testNonIndexableKeys_existInXmlLayout() {
@@ -42,5 +68,20 @@ public class LegalSettingsTest {
                 R.xml.about_legal);
 
         assertThat(keys).containsAllIn(niks);
+    }
+
+    @Test
+    public void testWallpaperAttributions_byDefault_shouldBeShown() {
+        mFragment.checkWallpaperAttributionAvailability(mContext);
+
+        assertThat(mWallpaperRemoved).isEqualTo(false);
+    }
+
+    @Test
+    @Config(qualifiers = "mcc999")
+    public void testWallpaperAttributions_ifDisabled_shouldNotBeShown() {
+        mFragment.checkWallpaperAttributionAvailability(mContext);
+
+        assertThat(mWallpaperRemoved).isEqualTo(true);
     }
 }
