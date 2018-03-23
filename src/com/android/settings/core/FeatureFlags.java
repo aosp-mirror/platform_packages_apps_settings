63bdfa8998791eb08fb70c21ea37d23c910e5057
ryanywlin <ryanywlin@google.com>
2018-03-23 16:57:51 +0800

    Add audio switch UI in sound settings
    - Build two controller to control list preferences.
    
    - MediaOutputPreferenceController which allows switching
    the media output between current device and connected
    BT device supporting A2DP. It also controls disabling
    media output switching during a call or cast mode.
    
    - HandsFreeProfilePreferenceController which allows
    switching between HFP-connected BT devices while in
    on-call state.
    
    - Add test cases for controllers.
    
    Bug: 74130772
    Test: make RunSettingsRoboTests ROBOTEST_FILTER="MediaOutputPreferenceControllerTest" -j56
    Test: make RunSettingsRoboTests ROBOTEST_FILTER="HandsFreeProfileOutputPreferenceControllerTest" -j56
    Test: make RunSettingsRoboTests ROBOTEST_FILTER="AudioOutputSwitchPreferenceControllerTest" -j56
    
    Change-Id: I37f5418442ce77e72cdff07f071ea519ab1047f3

--- src/com/android/settings/core/FeatureFlags.java
+++ src/com/android/settings/core/FeatureFlags.java
@@ -25,4 +25,5 @@ public class FeatureFlags {
     public static final String ABOUT_PHONE_V2 = "settings_about_phone_v2";
     public static final String BLUETOOTH_WHILE_DRIVING = "settings_bluetooth_while_driving";
     public static final String DATA_USAGE_SETTINGS_V2 = "settings_data_usage_v2";
+    public static final String AUDIO_SWITCHER_SETTINGS = "settings_audio_switcher";
 }
