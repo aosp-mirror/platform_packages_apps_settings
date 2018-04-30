6761acdd332794176e7b962a6ad13b2cbc7c833b
Beverly Tai <beverlyt@google.com>
2018-04-30 20:01:40 +0000

    Revert "Get rid of charging sounds toggle"
    Bug: 77912907
    This reverts commit fbe721b1b39d48ef8665267774b54f5f04b20d58.
    
    Reason for revert: negative dogfood feedback
    
    Change-Id: I8485a379eafee0a4fcd599e9b1c47aee7dd3c6ad

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -205,6 +205,8 @@ public class SoundSettings extends DashboardFragment {
                 new DialPadTonePreferenceController(context, fragment, lifecycle);
         final ScreenLockSoundPreferenceController screenLockSoundPreferenceController =
                 new ScreenLockSoundPreferenceController(context, fragment, lifecycle);
+        final ChargingSoundPreferenceController chargingSoundPreferenceController =
+                new ChargingSoundPreferenceController(context, fragment, lifecycle);
         final DockingSoundPreferenceController dockingSoundPreferenceController =
                 new DockingSoundPreferenceController(context, fragment, lifecycle);
         final TouchSoundPreferenceController touchSoundPreferenceController =
@@ -220,6 +222,7 @@ public class SoundSettings extends DashboardFragment {
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(screenLockSoundPreferenceController);
+        controllers.add(chargingSoundPreferenceController);
         controllers.add(dockingSoundPreferenceController);
         controllers.add(touchSoundPreferenceController);
         controllers.add(vibrateOnTouchPreferenceController);
@@ -230,6 +233,7 @@ public class SoundSettings extends DashboardFragment {
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,
                         screenLockSoundPreferenceController,
+                        chargingSoundPreferenceController,
                         dockingSoundPreferenceController,
                         touchSoundPreferenceController,
                         vibrateOnTouchPreferenceController,
