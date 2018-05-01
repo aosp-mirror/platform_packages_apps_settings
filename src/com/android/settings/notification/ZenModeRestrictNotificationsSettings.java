ded64625f4ed5c400181344c20c4dc030abf4c15
Julia Reynolds <juliacr@google.com>
2018-05-01 10:07:59 -0400

    Disable 'remove' option while setting is selected
    Test: robotests
    Bug: 78916709
    Change-Id: Id1dfc3b61a80c2e0677472ae84dc7425116825f0

--- src/com/android/settings/notification/ZenModeRestrictNotificationsSettings.java
+++ src/com/android/settings/notification/ZenModeRestrictNotificationsSettings.java
@@ -65,6 +65,7 @@ public class ZenModeRestrictNotificationsSettings extends ZenModeSettingsBase im
             ZenModeVisEffectsCustomPreferenceController custom =
                     use(ZenModeVisEffectsCustomPreferenceController.class);
             custom.setShownByMenu(mShowMenuSelected);
+            custom.displayPreference(getPreferenceScreen());
 
             if (mShowMenuSelected) {
                 custom.select();
@@ -80,13 +81,19 @@ public class ZenModeRestrictNotificationsSettings extends ZenModeSettingsBase im
 
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
-        if (mShowMenuSelected && !use(ZenModeVisEffectsCustomPreferenceController.class)
-                .areCustomOptionsSelected()) {
+        if (mShowMenuSelected) {
             menu.findItem(APP_MENU_SHOW_CUSTOM)
                     .setTitle(R.string.zen_mode_restrict_notifications_disable_custom);
         } else {
             menu.findItem(APP_MENU_SHOW_CUSTOM)
                     .setTitle(R.string.zen_mode_restrict_notifications_enable_custom);
+        }
+
+        if (mShowMenuSelected && use(ZenModeVisEffectsCustomPreferenceController.class)
+                .areCustomOptionsSelected()) {
+            menu.findItem(APP_MENU_SHOW_CUSTOM).setEnabled(false);
+        } else {
+            menu.findItem(APP_MENU_SHOW_CUSTOM).setEnabled(true);
         }
     }
 
