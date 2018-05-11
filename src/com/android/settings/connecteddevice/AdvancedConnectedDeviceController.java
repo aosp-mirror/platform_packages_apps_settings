fd7c9650ed55fc5043f1215af0382ec1304eb2d1
hughchen <hughchen@google.com>
2018-05-11 20:11:56 +0800

    Add controller to show the summary
    * Add AdvancedConnectedDeviceController that used to show which component is available
    * Add getConnectedDevices Summary Resource Id() to decide which string should be shown.
      Here have  4 cases to shown the string.
      case 1: driving mode available and NFC is availalbe, show "Bluetooth, driving mode, NFC"
      case 2: driving mode available and NFC is not availalbe, show "Bluetooth, driving mode"
      case 3: driving mode not available and NFC is availalbe, show "Bluetooth, NFC"
      case 4: driving mode not available and NFC not availalbe, show "Bluetooth"
    * Add test to verify the summary string is correct in each condition
    * Add test to verify getAvailabilityStatus() is AVAILABLE.
    
    Bug: 79299421
    Test: make -j50 RunSettingsRoboTests ROBOTEST_FILTER=AdvancedConnectedDeviceControllerTest
    Change-Id: I1048355bbd344db3ab645dd1537b4259eff57f38
    Merged-In: I1048355bbd344db3ab645dd1537b4259eff57f38

---
+++ src/com/android/settings/connecteddevice/AdvancedConnectedDeviceController.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.settings.connecteddevice;
+
+import android.content.Context;
+import android.support.annotation.VisibleForTesting;
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settings.nfc.NfcPreferenceController;
+
+/**
+ * Controller that used to show which component is available
+ */
+public class AdvancedConnectedDeviceController extends BasePreferenceController {
+
+    public AdvancedConnectedDeviceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return mContext.getText(getConnectedDevicesSummaryResourceId(mContext));
+    }
+
+    /**
+     * Get Connected Devices summary that depend on {@link NfcPreferenceController} or
+     * diving mode are available
+     */
+    public static int getConnectedDevicesSummaryResourceId(Context context) {
+        final NfcPreferenceController nfcPreferenceController =
+                new NfcPreferenceController(context);
+        final boolean isDrivingModeAvailable = false;
+
+        return getConnectedDevicesSummaryResourceId(nfcPreferenceController,
+                isDrivingModeAvailable);
+    }
+
+    @VisibleForTesting
+    static int getConnectedDevicesSummaryResourceId(NfcPreferenceController
+            nfcPreferenceController, boolean isDrivingModeAvailable) {
+        final int resId;
+
+        if (nfcPreferenceController.isAvailable()) {
+            if (isDrivingModeAvailable) {
+                // NFC available, driving mode available
+                resId = R.string.connected_devices_dashboard_summary;
+            } else {
+                // NFC available, driving mode not available
+                resId = R.string.connected_devices_dashboard_no_driving_mode_summary;
+            }
+        } else {
+            if (isDrivingModeAvailable) {
+                // NFC not available, driving mode available
+                resId = R.string.connected_devices_dashboard_no_nfc_summary;
+            } else {
+                // NFC not available, driving mode not available
+                resId = R.string.connected_devices_dashboard_no_driving_mode_no_nfc_summary;
+            }
+        }
+
+        return resId;
+    }
+}
