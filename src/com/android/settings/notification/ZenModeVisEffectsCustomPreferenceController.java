55d6aebcc5ab11a6b1dcdefb868cfe5aa8e216ac
Beverly <beverlyt@google.com>
2018-05-14 14:12:38 -0400

    Always show custom zen vis eff option
    Cherry-picked from pi-dev ag/4044915
    + dnd string edits
    
    Change-Id: I2fcbe6c527b40610f997efacb08049c07b3a6dd7
    Fixes: 79538038
    Bug: 78447976
    Test: ZenModeViseffectsCustomPreferenceControllerTes

--- src/com/android/settings/notification/ZenModeVisEffectsCustomPreferenceController.java
+++ src/com/android/settings/notification/ZenModeVisEffectsCustomPreferenceController.java
@@ -30,7 +30,6 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 public class ZenModeVisEffectsCustomPreferenceController
         extends AbstractZenModePreferenceController {
 
-    protected boolean mShowMenuSelected;
     protected static final int INTERRUPTIVE_EFFECTS = Policy.SUPPRESSED_EFFECT_AMBIENT
             | Policy.SUPPRESSED_EFFECT_PEEK
             | Policy.SUPPRESSED_EFFECT_LIGHTS
@@ -43,11 +42,7 @@ public class ZenModeVisEffectsCustomPreferenceController
 
     @Override
     public boolean isAvailable() {
-        if (mShowMenuSelected) {
-            return true;
-        }
-
-        return areCustomOptionsSelected();
+        return true;
     }
 
     @Override
@@ -65,10 +60,6 @@ public class ZenModeVisEffectsCustomPreferenceController
         pref.setOnRadioButtonClickListener(p -> {
             launchCustomSettings();
         });
-    }
-
-    protected void setShownByMenu(boolean shown) {
-        mShowMenuSelected = shown;
     }
 
     protected boolean areCustomOptionsSelected() {
