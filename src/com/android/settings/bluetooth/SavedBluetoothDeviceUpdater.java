f43a1f185db25408dd4a0f169731557da9265694
timhypeng <timhypeng@google.com>
2018-08-14 18:45:18 +0800

    Remove test-purpose constructor from SavedBluetoothDeviceUpdater
    - replace mock object with ShadowBluetoothAdapter
    
    Bug: 111848213
    Test: make -j50 RunSettingsRoboTests
    Change-Id: Iff69ed511b23846078925609bb603ae414956ea3

--- src/com/android/settings/bluetooth/SavedBluetoothDeviceUpdater.java
+++ src/com/android/settings/bluetooth/SavedBluetoothDeviceUpdater.java
@@ -16,16 +16,13 @@
 package com.android.settings.bluetooth;
 
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.util.Log;
 
 import com.android.settings.connecteddevice.DevicePreferenceCallback;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settingslib.bluetooth.CachedBluetoothDevice;
-import com.android.settingslib.bluetooth.LocalBluetoothManager;
 
-import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
 /**
@@ -39,13 +36,6 @@ public class SavedBluetoothDeviceUpdater extends BluetoothDeviceUpdater
     public SavedBluetoothDeviceUpdater(Context context, DashboardFragment fragment,
             DevicePreferenceCallback devicePreferenceCallback) {
         super(context, fragment, devicePreferenceCallback);
-    }
-
-    @VisibleForTesting
-    SavedBluetoothDeviceUpdater(DashboardFragment fragment,
-            DevicePreferenceCallback devicePreferenceCallback,
-            LocalBluetoothManager localBluetoothManager) {
-        super(fragment, devicePreferenceCallback, localBluetoothManager);
     }
 
     @Override
