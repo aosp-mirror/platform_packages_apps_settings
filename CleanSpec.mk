9a7786b3e11f626c08c99691dd0cb718de4267da
Anton Hansson <hansson@google.com>
2018-12-10 18:14:48 +0000

    Move Settings app to /product
    Since this app is frequently customized by OEMs, it doesn't
    fit in the mainline /system image right now. Move it to
    /product.
    
    Bug: 110072687
    Test: make
    Change-Id: Iee5940125af3f2a8543ecb3a68d7fba737acd77d
    Merged-In: Iee5940125af3f2a8543ecb3a68d7fba737acd77d
    (cherry picked from commit f109282c8ea955bfc72a025c7068930c67c38681)

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -46,6 +46,7 @@
 
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Settings_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Settings_intermediates)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/Settings)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
