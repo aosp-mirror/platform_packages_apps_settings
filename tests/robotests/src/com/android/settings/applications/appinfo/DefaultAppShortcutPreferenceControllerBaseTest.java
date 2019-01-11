ac5a639aeba2a8f565d05316342a56bda76ca50e
Hai Zhang <zhanghai@google.com>
2019-01-11 15:29:03 -0800

    Use role for App info default SMS shortcut.
    This change makes the default SMS shortcut in App info launch the new
    default app UI based on roles, since we've migrated default SMS
    mechanism.
    
    Bug: 110557011
    Test: atest DefaultAppShortcutPreferenceControllerBaseTest && atest DefaultSmsShortcutPreferenceControllerTest
    Change-Id: I074e41433fe912309082f1e9c54bf74200297dd1

--- tests/robotests/src/com/android/settings/applications/appinfo/DefaultAppShortcutPreferenceControllerBaseTest.java
+++ tests/robotests/src/com/android/settings/applications/appinfo/DefaultAppShortcutPreferenceControllerBaseTest.java
@@ -18,11 +18,13 @@ package com.android.settings.applications.appinfo;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.robolectric.Shadows.shadowOf;
 
 import android.app.Activity;
+import android.app.role.RoleManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.UserManager;
@@ -42,108 +44,173 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowApplication;
 import org.robolectric.shadows.ShadowUserManager;
+
+import java.util.Collections;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(shadows = ShadowUserManager.class)
 public class DefaultAppShortcutPreferenceControllerBaseTest {
 
-    private ShadowUserManager mShadowUserManager;
+    private static final String TEST_PREFERENCE_KEY = "TestKey";
+    private static final String TEST_ROLE_NAME = "TestRole";
+    private static final String TEST_PACKAGE_NAME = "TestPackage";
+
     @Mock
-    private AppInfoDashboardFragment mFragment;
+    private RoleManager mRoleManager;
     @Mock
     private Preference mPreference;
 
     private Activity mActivity;
-    private TestPreferenceController mController;
+    private ShadowUserManager mShadowUserManager;
+
+    private TestRolePreferenceController mController;
+    private TestLegacyPreferenceController mLegacyController;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        ShadowApplication.getInstance().setSystemService(Context.ROLE_SERVICE, mRoleManager);
         mActivity = Robolectric.setupActivity(Activity.class);
         mShadowUserManager = shadowOf(mActivity.getSystemService(UserManager.class));
-        mController = new TestPreferenceController(mActivity, mFragment);
-        final String key = mController.getPreferenceKey();
-        when(mPreference.getKey()).thenReturn(key);
+        mController = new TestRolePreferenceController(mActivity);
+        when(mPreference.getKey()).thenReturn(mController.getPreferenceKey());
+        mLegacyController = new TestLegacyPreferenceController(mActivity);
     }
 
     @Test
-    public void getAvailabilityStatus_managedProfile_shouldReturnDisabled() {
+    public void getAvailabilityStatus_isManagedProfile_shouldReturnDisabled() {
         mShadowUserManager.setManagedProfile(true);
 
-        assertThat(mController.getAvailabilityStatus())
-                .isEqualTo(DefaultAppShortcutPreferenceControllerBase.DISABLED_FOR_USER);
+        assertThat(mController.getAvailabilityStatus()).isEqualTo(
+                DefaultAppShortcutPreferenceControllerBase.DISABLED_FOR_USER);
     }
 
     @Test
-    public void getAvailabilityStatus_hasAppCapability_shouldReturnAvailable() {
-        mController.capable = true;
+    public void getAvailabilityStatus_roleIsAvailable_shouldReturnAvailable() {
         mShadowUserManager.setManagedProfile(false);
+        when(mRoleManager.isRoleAvailable(eq(TEST_ROLE_NAME))).thenReturn(true);
 
-        assertThat(mController.getAvailabilityStatus())
-                .isEqualTo(DefaultAppShortcutPreferenceControllerBase.AVAILABLE);
+        assertThat(mController.getAvailabilityStatus()).isEqualTo(
+                DefaultAppShortcutPreferenceControllerBase.AVAILABLE);
     }
 
     @Test
-    public void getAvailabilityStatus_noAppCapability_shouldReturnDisabled() {
-        mController.capable = false;
+    public void getAvailabilityStatus_roleNotAvailable_shouldReturnDisabled() {
         mShadowUserManager.setManagedProfile(false);
+        when(mRoleManager.isRoleAvailable(eq(TEST_ROLE_NAME))).thenReturn(false);
 
         assertThat(mController.getAvailabilityStatus()).isEqualTo(
                 DefaultAppShortcutPreferenceControllerBase.UNSUPPORTED_ON_DEVICE);
     }
 
     @Test
-    public void updateState_isDefaultApp_shouldSetSummaryToYes() {
-        mController.isDefault = true;
+    public void updateState_isRoleHolder_shouldSetSummaryToYes() {
+        when(mRoleManager.getRoleHolders(eq(TEST_ROLE_NAME))).thenReturn(Collections.singletonList(
+                TEST_PACKAGE_NAME));
+        final CharSequence yesText = mActivity.getText(R.string.yes);
 
         mController.updateState(mPreference);
-        String yesString = mActivity.getString(R.string.yes);
-        verify(mPreference).setSummary(yesString);
+        verify(mPreference).setSummary(yesText);
+    }
+
+    @Test
+    public void updateState_notRoleHoler_shouldSetSummaryToNo() {
+        when(mRoleManager.getRoleHolders(eq(TEST_ROLE_NAME))).thenReturn(Collections.emptyList());
+        final CharSequence noText = mActivity.getText(R.string.no);
+
+        mController.updateState(mPreference);
+        verify(mPreference).setSummary(noText);
+    }
+
+    @Test
+    public void handlePreferenceTreeClick_shouldStartManageDefaultAppIntent() {
+        final ShadowActivity shadowActivity = shadowOf(mActivity);
+
+        mController.handlePreferenceTreeClick(mPreference);
+        final Intent intent = shadowActivity.getNextStartedActivity();
+        assertThat(intent).isNotNull();
+        assertThat(intent.getAction()).isEqualTo(Intent.ACTION_MANAGE_DEFAULT_APP);
+        assertThat(intent.getStringExtra(Intent.EXTRA_ROLE_NAME)).isEqualTo(TEST_ROLE_NAME);
+    }
+
+    private class TestRolePreferenceController extends DefaultAppShortcutPreferenceControllerBase {
+
+        private TestRolePreferenceController(Context context) {
+            super(context, TEST_PREFERENCE_KEY, TEST_ROLE_NAME, TEST_PACKAGE_NAME);
+        }
+    }
+
+    // TODO: STOPSHIP(b/110557011): Remove following tests once we have all default apps migrated.
+
+    @Test
+    public void getAvailabilityStatus_hasAppCapability_shouldReturnAvailable() {
+        mShadowUserManager.setManagedProfile(false);
+        mLegacyController.mHasAppCapability = true;
+
+        assertThat(mLegacyController.getAvailabilityStatus()).isEqualTo(
+                DefaultAppShortcutPreferenceControllerBase.AVAILABLE);
+    }
+
+    @Test
+    public void getAvailabilityStatus_noAppCapability_shouldReturnDisabled() {
+        mShadowUserManager.setManagedProfile(false);
+        mLegacyController.mHasAppCapability = false;
+
+        assertThat(mLegacyController.getAvailabilityStatus()).isEqualTo(
+                DefaultAppShortcutPreferenceControllerBase.UNSUPPORTED_ON_DEVICE);
+    }
+
+    @Test
+    public void updateState_isDefaultApp_shouldSetSummaryToYes() {
+        mLegacyController.mIsDefaultApp = true;
+        final CharSequence yesText = mActivity.getText(R.string.yes);
+
+        mLegacyController.updateState(mPreference);
+        verify(mPreference).setSummary(yesText);
     }
 
     @Test
     public void updateState_notDefaultApp_shouldSetSummaryToNo() {
-        mController.isDefault = false;
+        mLegacyController.mIsDefaultApp = false;
+        final CharSequence noText = mActivity.getText(R.string.no);
 
-        mController.updateState(mPreference);
-
-        String noString = mActivity.getString(R.string.no);
-        verify(mPreference).setSummary(noString);
+        mLegacyController.updateState(mPreference);
+        verify(mPreference).setSummary(noText);
     }
 
     @Test
     public void handlePreferenceTreeClick_shouldStartDefaultAppSettings() {
         final ShadowActivity shadowActivity = shadowOf(mActivity);
 
-        mController.handlePreferenceTreeClick(mPreference);
-
-        final Intent nextIntent = shadowActivity.getNextStartedActivity();
-        assertThat(nextIntent).isNotNull();
-        assertThat(nextIntent.getStringExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT)).isEqualTo(
+        mLegacyController.handlePreferenceTreeClick(mPreference);
+        final Intent intent = shadowActivity.getNextStartedActivity();
+        assertThat(intent).isNotNull();
+        assertThat(intent.getStringExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT)).isEqualTo(
                 DefaultAppSettings.class.getName());
-        assertThat(
-                nextIntent.getBundleExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_ARGUMENTS).getString(
-                        SettingsActivity.EXTRA_FRAGMENT_ARG_KEY)).isEqualTo("TestKey");
+        assertThat(intent.getBundleExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_ARGUMENTS).getString(
+                SettingsActivity.EXTRA_FRAGMENT_ARG_KEY)).isEqualTo(TEST_PREFERENCE_KEY);
     }
 
-    private class TestPreferenceController extends DefaultAppShortcutPreferenceControllerBase {
+    private class TestLegacyPreferenceController
+            extends DefaultAppShortcutPreferenceControllerBase {
 
-        private boolean isDefault;
-        private boolean capable;
+        private boolean mIsDefaultApp;
+        private boolean mHasAppCapability;
 
-        private TestPreferenceController(Context context, AppInfoDashboardFragment parent) {
-            super(context, "TestKey", "TestPackage");
+        private TestLegacyPreferenceController(Context context) {
+            super(context, TEST_PREFERENCE_KEY, TEST_PACKAGE_NAME);
         }
 
         @Override
         protected boolean hasAppCapability() {
-            return capable;
+            return mHasAppCapability;
         }
 
         @Override
         protected boolean isDefaultApp() {
-            return isDefault;
+            return mIsDefaultApp;
         }
     }
 }
