34042bbf1d62aa19094bdb00f12ab46b4fcdd848
Rich Cannings <richc@google.com>
2019-02-19 13:15:30 -0800

    DO NOT MERGE Refactor passwords/pins/patterns to byte[]
    Relating to packages/apps/Settings
    
    Bug: 120484642
    Test: manual - test setting and unlocking passwords/pins/patterns.
          automated - atest packages/apps/Settings/tests/robotests/src/com/android/settings/password/
    
    Change-Id: Idec8338d141c185bef67ade12035fdb2fa9d17ea
    (cherry picked from commit b27c4308a2ab839cff4f4751451440f6dc752e8e)

--- src/com/android/settings/password/ConfirmLockPattern.java
+++ src/com/android/settings/password/ConfirmLockPattern.java
@@ -462,7 +462,7 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
                                 mLockPatternUtils, pattern, challenge, localUserId,
                                 onVerifyCallback)
                         : LockPatternChecker.verifyTiedProfileChallenge(
-                                mLockPatternUtils, LockPatternUtils.patternToString(pattern),
+                                mLockPatternUtils, LockPatternUtils.patternToByteArray(pattern),
                                 true, challenge, localUserId, onVerifyCallback);
             }
 
@@ -487,7 +487,7 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
                                     intent.putExtra(ChooseLockSettingsHelper.EXTRA_KEY_TYPE,
                                                     StorageManager.CRYPT_TYPE_PATTERN);
                                     intent.putExtra(ChooseLockSettingsHelper.EXTRA_KEY_PASSWORD,
-                                                    LockPatternUtils.patternToString(pattern));
+                                                    LockPatternUtils.patternToByteArray(pattern));
                                 }
                                 mCredentialCheckResultTracker.setResult(matched, intent, timeoutMs,
                                         localEffectiveUserId);
