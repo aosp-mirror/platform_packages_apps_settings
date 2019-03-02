c70f3ebc29c89b5529a283583df6c2e59365ba74
Andrii Kulian <akulian@google.com>
2019-03-02 01:44:55 +0000

    Revert "Revert "Revert "Add LOCAL_RESOURCE_DIR in Settings makefile"""
    This reverts commit e7fe45f4cad79a9f51d3f4b8c1b7917dd485bbf2.
    
    Reason for revert: broke git_pi-dev-plus-aosp build
    
    Change-Id: I390b0006bb780ce81520ea851f474e0188f0495e

--- Android.mk
+++ Android.mk
@@ -18,8 +18,6 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
