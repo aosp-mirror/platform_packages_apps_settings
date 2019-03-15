ecea9e96d97d4f96ef57dd98f9e722cd25cb720e
Lifu Tang <lifu@google.com>
2019-03-15 11:40:44 -0700

    Display empty message if no recent location access
    Bug: 120873123
    Test: build, flash, and test manually
    Change-Id: Ibd7a47cb814fe16ecb1c017865ab28862e17ddcc

--- src/com/android/settings/location/RecentLocationAccessPreferenceController.java
+++ src/com/android/settings/location/RecentLocationAccessPreferenceController.java
@@ -74,6 +74,7 @@ public class RecentLocationAccessPreferenceController extends AbstractPreference
         mController = AppEntitiesHeaderController.newInstance(mContext, view)
                 .setHeaderTitleRes(R.string.location_category_recent_location_access)
                 .setHeaderDetailsRes(R.string.location_recent_location_access_view_details)
+                .setHeaderEmptyRes(R.string.location_no_recent_accesses)
                 .setHeaderDetailsClickListener((View v) -> {
                     final Intent intent = new Intent(Intent.ACTION_REVIEW_PERMISSION_USAGE);
                     intent.putExtra(Intent.EXTRA_PERMISSION_NAME,
@@ -106,8 +107,6 @@ public class RecentLocationAccessPreferenceController extends AbstractPreference
             for (; i < MAXIMUM_APP_COUNT; i++) {
                 mController.removeAppEntity(i);
             }
-        } else {
-            // If there's no item to display, add a "No recent apps" item.
         }
         mController.apply();
     }
