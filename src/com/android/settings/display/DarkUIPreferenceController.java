ee5f79edbd3a1c05780de3e1bbfe0ccba0f605f4
Salvador Martinez <dehboxturtle@google.com>
2019-05-08 14:30:25 -0700

    Fix dark theme and battery saver interaction
    In settings we weren't properly taking into account battery saver
    enabled dark theme. This CL makes it so that when battery saver
    on we disable the toggle until battery saver is off since
    you can't change dark theme in battery saver. Additionally it
    adds a message indicating why the toggle was disabled so the
    user doesn't get confused.
    
    Test: robotests
    Bug: 131913864
    Change-Id: I855e429a666098c837650852d1e1355477afa13d

--- src/com/android/settings/display/DarkUIPreferenceController.java
+++ src/com/android/settings/display/DarkUIPreferenceController.java
@@ -17,32 +17,70 @@
 package com.android.settings.display;
 
 import android.app.UiModeManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.PowerManager;
 import android.provider.Settings;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 
+import com.android.settings.R;
 import com.android.settings.core.TogglePreferenceController;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;
 
-public class DarkUIPreferenceController extends TogglePreferenceController {
+public class DarkUIPreferenceController extends TogglePreferenceController implements
+        LifecycleObserver, OnStart, OnStop {
 
     public static final String DARK_MODE_PREFS = "dark_mode_prefs";
     public static final String PREF_DARK_MODE_DIALOG_SEEN = "dark_mode_dialog_seen";
     public static final int DIALOG_SEEN = 1;
+
+    @VisibleForTesting
+    SwitchPreference mPreference;
+
     private UiModeManager mUiModeManager;
+    private PowerManager mPowerManager;
     private Context mContext;
+
     private Fragment mFragment;
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateEnabledStateIfNeeded();
+        }
+    };
 
     public DarkUIPreferenceController(Context context, String key) {
         super(context, key);
         mContext = context;
         mUiModeManager = context.getSystemService(UiModeManager.class);
+        mPowerManager = context.getSystemService(PowerManager.class);
     }
 
     @Override
     public boolean isChecked() {
         return mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        updateEnabledStateIfNeeded();
     }
 
     @Override
@@ -68,12 +106,46 @@ public class DarkUIPreferenceController extends TogglePreferenceController {
     }
 
     @VisibleForTesting
+    void updateEnabledStateIfNeeded() {
+        if (mPreference == null) {
+            return;
+        }
+        boolean isBatterySaver = isPowerSaveMode();
+        mPreference.setEnabled(!isBatterySaver);
+        if (isBatterySaver) {
+            int stringId = mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES
+                    ? R.string.dark_ui_mode_disabled_summary_dark_theme_on
+                    : R.string.dark_ui_mode_disabled_summary_dark_theme_off;
+            mPreference.setSummary(mContext.getString(stringId));
+        } else {
+            mPreference.setSummary(null);
+        }
+    }
+
+    @VisibleForTesting
+    boolean isPowerSaveMode() {
+        return mPowerManager.isPowerSaveMode();
+    }
+
+
+    @VisibleForTesting
     void setUiModeManager(UiModeManager uiModeManager) {
         mUiModeManager = uiModeManager;
     }
 
     public void setParentFragment(Fragment fragment) {
         mFragment = fragment;
+    }
+
+    @Override
+    public void onStart() {
+        mContext.registerReceiver(mReceiver,
+                new IntentFilter(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED));
+    }
+
+    @Override
+    public void onStop() {
+        mContext.unregisterReceiver(mReceiver);
     }
 
     @Override
