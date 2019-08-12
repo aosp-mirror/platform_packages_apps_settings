fe9a794eaf7b05dedd4fa1e0eced865e4d519d04
Arc Wang <arcwang@google.com>
2019-08-12 20:11:13 +0800

    [Wi-Fi] Fix multi SIM devices switch to mobile data automatically settings missing problem
    Context#getResources loading is done based on the last SIM to
    come up, we may get wrong customization settings.
    
    This fix check all active subscriptions and shows the preference
    when there is one SIM applies.
    
    Bug: 138956509
    Test: CellularFallbackPreferenceControllerTest
          manual test:
              1. Insert feature supported SIM and check UI.
              2. Remove feature supported SIM and check UI.
              3. Turn airplane mode on and check 1. and 2..
    
    Change-Id: I72b6db415429181395a02f163889bb1b9c0f070f

--- tests/robotests/src/com/android/settings/wifi/CellularFallbackPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/wifi/CellularFallbackPreferenceControllerTest.java
@@ -18,36 +18,55 @@ package com.android.settings.wifi;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowApplication;
 
 @RunWith(RobolectricTestRunner.class)
 public class CellularFallbackPreferenceControllerTest {
     private static final String KEY_CELLULAR_FALLBACK = "wifi_cellular_data_fallback";
 
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private Context mContext;
+    @Mock
+    private SubscriptionManager mSubscriptionManager;
 
     private CellularFallbackPreferenceController mController;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        mController = new CellularFallbackPreferenceController(mContext, KEY_CELLULAR_FALLBACK);
+        ShadowApplication shadowApplication = ShadowApplication.getInstance();
+        shadowApplication.setSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE,
+                mSubscriptionManager);
+        mController = spy(new CellularFallbackPreferenceController(RuntimeEnvironment.application,
+                KEY_CELLULAR_FALLBACK));
     }
 
     @Test
     public void isAvailable_avoidBadWifiConfigIsFalse_shouldReturnTrue() {
-        when(mContext.getResources().getInteger(
+        setupMockIcc();
+        final Resources resources = mock(Resources.class);
+
+        doReturn(resources).when(mController).getResourcesForSubId(anyInt());
+        when(resources.getInteger(
                 com.android.internal.R.integer.config_networkAvoidBadWifi))
                 .thenReturn(0);
 
@@ -56,10 +75,24 @@ public class CellularFallbackPreferenceControllerTest {
 
     @Test
     public void isAvailable_avoidBadWifiConfigIsTrue_shouldReturnFalse() {
-        when(mContext.getResources().getInteger(
+        setupMockIcc();
+        final Resources resources = mock(Resources.class);
+
+        doReturn(resources).when(mController).getResourcesForSubId(anyInt());
+        when(resources.getInteger(
                 com.android.internal.R.integer.config_networkAvoidBadWifi))
                 .thenReturn(1);
 
         assertThat(mController.isAvailable()).isFalse();
+    }
+
+    private void setupMockIcc() {
+        final List<SubscriptionInfo> subscriptionInfoList = new ArrayList<>();
+        final SubscriptionInfo info1 = mock(SubscriptionInfo.class);
+        final SubscriptionInfo info2 = mock(SubscriptionInfo.class);
+        subscriptionInfoList.add(info1);
+        subscriptionInfoList.add(info2);
+        when(mSubscriptionManager.getActiveSubscriptionInfoList())
+                .thenReturn(subscriptionInfoList);
     }
 }
