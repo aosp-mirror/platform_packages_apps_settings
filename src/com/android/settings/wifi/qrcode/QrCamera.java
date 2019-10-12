b056ea70f013d4f117af019874e89e40326e4782
LuK1337 <priv.luk@gmail.com>
2019-10-12 11:59:09 +0200

    Settings: Fix QrCamera crash on devices without flash
    Change-Id: Ife34b5fb87a9a4f509b665a3433e0b2ba857c885

--- src/com/android/settings/wifi/qrcode/QrCamera.java
+++ src/com/android/settings/wifi/qrcode/QrCamera.java
@@ -178,7 +178,9 @@ public class QrCamera extends Handler {
         Size pictureSize = getBestPictureSize(mParameters);
         mParameters.setPreviewSize(pictureSize.getWidth(), pictureSize.getHeight());
 
-        if (mParameters.getSupportedFlashModes().contains(Parameters.FLASH_MODE_OFF)) {
+        final List<String> supportedFlashModes = mParameters.getSupportedFlashModes();
+        if (supportedFlashModes != null &&
+                supportedFlashModes.contains(Parameters.FLASH_MODE_OFF)) {
             mParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
         }
 
