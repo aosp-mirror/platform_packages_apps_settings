95108c4b3887aea6993027a35b99afecdf0de144
Dianne Hackborn <hackbod@google.com>
2019-10-28 16:36:15 -0700

    Reland: Rework platform version to hide codenames.
    The public platform version no longer can be a codename, it is
    always the most recently released platform.  A new build property
    and API provides either the offical version or the current codename
    as appropriate.  This will avoid breaking apps that look at the
    platform version while development is under a codename.
    
    Bug:  143175463
    Test: manual
    Exempt-From-Owner-Approval: Previously approved, no new changes.
    Change-Id: Ifb6f6236aab2c40fd51abb9c6587eafcc8983857

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDetailPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDetailPreferenceController.java
@@ -75,7 +75,7 @@ public class FirmwareVersionDetailPreferenceController extends BasePreferenceCon
 
     @Override
     public CharSequence getSummary() {
-        return Build.VERSION.RELEASE;
+        return Build.VERSION.RELEASE_OR_CODENAME;
     }
 
     @Override
