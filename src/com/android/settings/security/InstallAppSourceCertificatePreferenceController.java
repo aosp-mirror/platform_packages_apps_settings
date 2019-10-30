c8a1960cf4aaab46f26dc3f872710a9aa64666d3
Victor Hsieh <victorhsieh@google.com>
2019-10-30 15:35:19 -0700

    Support installation of the new app source certificate
    The new certificate can be installed from Settings ("Install a
    certificate > App Source certificate").  The installation flow includes
    a warning with user authorization to proceed, then a prompt for reboot
    (now or later).
    
    Installed certificate can be managed in "User credentials".  The name is
    currently a hash of hex numbers.
    
    Upon deletion, there will also be a promot for reboot (now or later).
    
    Test: Only see the new setting entry if feature is enabled
    Test: Install from Settings, see the expected file name in
          /data/misc/keysetore/user_0.  Reboot also works.
    Test: Able to see the certificate in Settings after installed
    Test: Able to delete the certificate, which triggers confirmation dialog
          to reboot.  Reboot works.
    Test: add certificate, see dialog, "not now" / tapping elsewhere does
          nothing
    Test: atest RestrictedEncryptionPreferenceControllerTest
    Bug: 112038744
    
    Change-Id: I7a4494ea0f243730df2212076588074d8774ae23

---
+++ src/com/android/settings/security/InstallAppSourceCertificatePreferenceController.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.internal.annotations.VisibleForTesting;
+import com.android.settings.core.BasePreferenceController;
+
+class InstallAppSourceCertificatePreferenceController extends
+        BasePreferenceController {
+
+    private static final String APK_VERITY_PROPERTY = "ro.apk_verity.mode";
+    private static final int APK_VERITY_MODE_ENABLED = 2;
+
+    InstallAppSourceCertificatePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return isApkVerityEnabled() ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @VisibleForTesting
+    static boolean isApkVerityEnabled() {
+        // TODO(victorhsieh): replace this with a new API in PackageManager once it is landed.
+        return SystemProperties.getInt(APK_VERITY_PROPERTY, 0) == APK_VERITY_MODE_ENABLED;
+    }
+}
