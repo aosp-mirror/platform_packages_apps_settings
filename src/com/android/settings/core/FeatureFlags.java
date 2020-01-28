0141e6f9436b433a32fad753a388f5c9652449a0
Zhen Zhang <zzhen@google.com>
2020-01-28 15:05:05 -0800

    Add a feature flag for the new tether settings UI
    The flag is mirrored from FeatureFlagUtils in framework/base
    Bug: 148182953
    Bug: 148220934
    Test: Build successfully.
    Change-Id: Ifada88cb585b3f618b7e2f59f1ceff007ae8eacd

--- src/com/android/settings/core/FeatureFlags.java
+++ src/com/android/settings/core/FeatureFlags.java
@@ -28,4 +28,5 @@ public class FeatureFlags {
             "settings_wifi_details_datausage_header";
     public static final String CONTROLLER_ENHANCEMENT = "settings_controller_loading_enhancement";
     public static final String CONDITIONAL_CARDS = "settings_conditionals";
+    public static final String TETHER_ALL_IN_ONE = "settings_tether_all_in_one";
 }
