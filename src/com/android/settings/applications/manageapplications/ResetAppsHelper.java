exthmui-aosp/android_packages_apps_Settings

Add whitelist in ResetAppsHelper
Add "com.android.systemui" into the whitelist and skip the clear data.

Fixes: 137037930
Test: manual test
Change-Id: Ie2f527309f9159babe79636a69ff13fe3ceb64d9

Former-commit-id: b54d2f90f2674e73e397b3ac4578a39739b4d94b

src/com/android/settings/applications/manageapplications/ResetAppsHelper.java
@@ -37,6 +37,7 @@ import androidx.appcompat.app.AlertDialog;
 
 import com.android.settings.R;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ResetAppsHelper implements DialogInterface.OnClickListener,
@@ -109,10 +110,17 @@ public class ResetAppsHelper implements DialogInterface.OnClickListener,
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                List<ApplicationInfo> apps = mPm.getInstalledApplications(
+                final List<ApplicationInfo> apps = mPm.getInstalledApplications(
                         PackageManager.GET_DISABLED_COMPONENTS);
+                final List<String> whiteList = Arrays.asList(
+                        mContext.getResources().getStringArray(
+                                R.array.config_reset_app_package_name));
+
                 for (int i = 0; i < apps.size(); i++) {
                     ApplicationInfo app = apps.get(i);
+                    if (whiteList.contains(app.packageName)) {
+                        continue;
+                    }
                     try {
                         mNm.clearData(app.packageName, app.uid, false);
                     } catch (android.os.RemoteException ex) {

aosp-mirror/platform_packages_apps_settings

- Add "com.android.systemui" and "com.android.vending" into the whitelist.
- Add "com.android.providers.downloads" and "android" into the whitelist.
- Do not data clear in the whitelist.
Fixes: 147124412
Merged-In: Ie2f527309f9159babe79636a69ff13fe3ceb64d9
(cherry picked from commit b54d2f90f2674e73e397b3ac4578a39739b4d94b)

src/com/android/settings/applications/manageapplications/ResetAppsHelper.java
@@ -37,6 +37,7 @@ import androidx.appcompat.app.AlertDialog;
 
 import com.android.settings.R;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ResetAppsHelper implements DialogInterface.OnClickListener,
@@ -109,10 +110,17 @@ public class ResetAppsHelper implements DialogInterface.OnClickListener,
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                List<ApplicationInfo> apps = mPm.getInstalledApplications(
+                final List<ApplicationInfo> apps = mPm.getInstalledApplications(
                         PackageManager.GET_DISABLED_COMPONENTS);
+                final List<String> whiteList = Arrays.asList(
+                        mContext.getResources().getStringArray(
+                                R.array.config_skip_reset_apps_package_name));
+
                 for (int i = 0; i < apps.size(); i++) {
                     ApplicationInfo app = apps.get(i);
+                    if (whiteList.contains(app.packageName)) {
+                        continue;
+                    }
                     try {
                         mNm.clearData(app.packageName, app.uid, false);
                     } catch (android.os.RemoteException ex) {