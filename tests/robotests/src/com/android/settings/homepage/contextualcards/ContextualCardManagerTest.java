36d1c17c71022882b32ca2b890d754a2649e73ce
Morten Krogh-Jespersen <mkroghj@google.com>
2020-02-04 13:10:59 +0000

    Revert "[Settings] Remove dependency of TelephonyIntents"
    This reverts commit 6a6b416c3db0ed4381f0c3e9177b6145fd69f872.
    
    Reason for revert: b/148838390
    
    Change-Id: I9be2c370c6923e9d7293e4eff5306bc54d49ade2

--- tests/robotests/src/com/android/settings/homepage/contextualcards/ContextualCardManagerTest.java
+++ tests/robotests/src/com/android/settings/homepage/contextualcards/ContextualCardManagerTest.java
@@ -33,18 +33,13 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.robolectric.Shadows.shadowOf;
 
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.telephony.SubscriptionManager;
-import android.telephony.TelephonyManager;
 import android.util.ArrayMap;
 import android.util.FeatureFlagUtils;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.android.settings.core.FeatureFlags;
 import com.android.settings.homepage.contextualcards.conditional.ConditionContextualCardController;
@@ -63,9 +58,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.shadows.ShadowSubscriptionManager;
-import org.robolectric.shadows.ShadowTelephonyManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -73,9 +67,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(RobolectricTestRunner.class)
 public class ContextualCardManagerTest {
-    private static final int SUB_ID = 2;
 
     private static final String TEST_SLICE_URI = "context://test/test";
     private static final String TEST_SLICE_NAME = "test_name";
@@ -86,8 +79,6 @@ public class ContextualCardManagerTest {
     Lifecycle mLifecycle;
 
     private Context mContext;
-    private ShadowSubscriptionManager mShadowSubscriptionManager;
-    private ShadowTelephonyManager mShadowTelephonyManager;
     private ContextualCardManager mManager;
 
     @Before
@@ -95,16 +86,6 @@ public class ContextualCardManagerTest {
         MockitoAnnotations.initMocks(this);
         mContext = RuntimeEnvironment.application;
         FeatureFlagUtils.setEnabled(mContext, FeatureFlags.CONDITIONAL_CARDS, true);
-
-        mShadowSubscriptionManager = shadowOf(
-                mContext.getSystemService(SubscriptionManager.class));
-        mShadowSubscriptionManager.setDefaultDataSubscriptionId(SUB_ID);
-
-        final TelephonyManager telephonyManager =
-                mContext.getSystemService(TelephonyManager.class);
-        mShadowTelephonyManager = shadowOf(telephonyManager);
-        mShadowTelephonyManager.setTelephonyManagerForSubscriptionId(SUB_ID, telephonyManager);
-
         mManager = new ContextualCardManager(mContext, mLifecycle, null /* bundle */);
     }
 
