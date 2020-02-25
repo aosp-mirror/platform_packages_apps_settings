fc8115d91bc3ded8918c7a3c6cb6fcf2fc9e5657
Rahul Sabnis <rahulsabnis@google.com>
2020-02-25 22:28:00 +0000

    Revert "Revert "Pass in active device to all BluetoothA2dp APIs ..."
    Revert submission 10303287-revert-10253996-bt-a2dp-no-null-FQRXACWPIA
    
    Reason for revert: Fixing breakage
    Reverted Changes:
    I4d9f2f819:Revert "Make sure calls to BluetoothA2dp APIs pass...
    I771ca0d57:Revert "Need to now pass in active device instead ...
    I76529c7a1:Revert "Pass in active device to all BluetoothA2dp...
    I297bda68d:Revert "Require user pass in a non-null BluetoothD...
    I525327959:Revert "Pass in active device to all BluetoothA2dp...
    I1d8660b11:Revert "Pass in active device to all BluetoothA2dp...
    
    Bug: 147287141
    Test: robotests
    Change-Id: I5aecfa4b5a8e371b914573ddd080acb98078bfca

--- tests/robotests/src/com/android/settings/development/bluetooth/BluetoothChannelModeDialogPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/development/bluetooth/BluetoothChannelModeDialogPreferenceControllerTest.java
@@ -23,8 +23,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothCodecConfig;
 import android.bluetooth.BluetoothCodecStatus;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 
 import androidx.lifecycle.LifecycleOwner;
@@ -47,6 +49,8 @@ import java.util.List;
 @RunWith(RobolectricTestRunner.class)
 public class BluetoothChannelModeDialogPreferenceControllerTest {
 
+    private static final String DEVICE_ADDRESS = "00:11:22:33:44:55";
+
     @Mock
     private BluetoothA2dp mBluetoothA2dp;
     @Mock
@@ -58,6 +62,7 @@ public class BluetoothChannelModeDialogPreferenceControllerTest {
     private BluetoothCodecStatus mCodecStatus;
     private BluetoothCodecConfig mCodecConfigAAC;
     private BluetoothCodecConfig mCodecConfigSBC;
+    private BluetoothDevice mActiveDevice;
     private Context mContext;
     private LifecycleOwner mLifecycleOwner;
     private Lifecycle mLifecycle;
@@ -69,6 +74,7 @@ public class BluetoothChannelModeDialogPreferenceControllerTest {
         mLifecycleOwner = () -> mLifecycle;
         mLifecycle = new Lifecycle(mLifecycleOwner);
         mBluetoothA2dpConfigStore = spy(new BluetoothA2dpConfigStore());
+        mActiveDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(DEVICE_ADDRESS);
         mController = new BluetoothChannelModeDialogPreferenceController(mContext, mLifecycle,
                 mBluetoothA2dpConfigStore);
         mPreference = new BluetoothChannelModeDialogPreference(mContext);
@@ -86,13 +92,14 @@ public class BluetoothChannelModeDialogPreferenceControllerTest {
                 BluetoothCodecConfig.BITS_PER_SAMPLE_NONE,
                 BluetoothCodecConfig.CHANNEL_MODE_MONO | BluetoothCodecConfig.CHANNEL_MODE_STEREO,
                 0, 0, 0, 0);
+        when(mBluetoothA2dp.getActiveDevice()).thenReturn(mActiveDevice);
     }
 
     @Test
     public void writeConfigurationValues_selectDefault_setHighest() {
         BluetoothCodecConfig[] mCodecConfigs = {mCodecConfigAAC, mCodecConfigSBC};
         mCodecStatus = new BluetoothCodecStatus(mCodecConfigSBC, null, mCodecConfigs);
-        when(mBluetoothA2dp.getCodecStatus(null)).thenReturn(mCodecStatus);
+        when(mBluetoothA2dp.getCodecStatus(mActiveDevice)).thenReturn(mCodecStatus);
         mController.onBluetoothServiceConnected(mBluetoothA2dp);
 
         mController.writeConfigurationValues(0);
@@ -118,7 +125,7 @@ public class BluetoothChannelModeDialogPreferenceControllerTest {
     public void getSelectableIndex_verifyList() {
         BluetoothCodecConfig[] mCodecConfigs = {mCodecConfigAAC, mCodecConfigSBC};
         mCodecStatus = new BluetoothCodecStatus(mCodecConfigSBC, null, mCodecConfigs);
-        when(mBluetoothA2dp.getCodecStatus(null)).thenReturn(mCodecStatus);
+        when(mBluetoothA2dp.getCodecStatus(mActiveDevice)).thenReturn(mCodecStatus);
         mController.onBluetoothServiceConnected(mBluetoothA2dp);
         List<Integer> indexList = new ArrayList<>();
         indexList.add(mPreference.getDefaultIndex());
