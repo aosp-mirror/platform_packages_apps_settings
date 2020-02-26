1a690debcc11e3cd2f163f96a7f058086bdafe13
Mill Chen <millchen@google.com>
2020-02-26 23:55:51 +0800

    RESTRICT AUTOMERGE Allow LockScreenPattern to be launched in the pinning screen
    If work profile lock is enabled and work app is pinned, users will get a
    black/white screen on the phone. That's because Settings is prevented
    from other apps launch any pages of Settings in the pinning mode.
    
    In order to launch some pages of Settings from other apps, we add a
    condition to the preventive mechanism and allow the activity inherited
    from SettingsBaseActivity to override the condition to have the activity
    to be launched from other apps in the pinning mode.
    
    Bug: 137015265
    Bug: 135604684
    Test: manual test
    Change-Id: I8070de79a83350d1658efcb19e983669dad0e673
    Merged-In: I8070de79a83350d1658efcb19e983669dad0e673
    (cherry picked from commit 3f4d3b4ac257451ccc1676f65321dedb2cb1870e)

--- src/com/android/settings/ConfirmDeviceCredentialBaseActivity.java
+++ src/com/android/settings/ConfirmDeviceCredentialBaseActivity.java
@@ -129,6 +129,11 @@ public abstract class ConfirmDeviceCredentialBaseActivity extends SettingsActivi
         }
     }
 
+    @Override
+    public boolean isLaunchableInTaskModePinned() {
+        return true;
+    }
+
     public void prepareEnterAnimation() {
         getFragment().prepareEnterAnimation();
     }
