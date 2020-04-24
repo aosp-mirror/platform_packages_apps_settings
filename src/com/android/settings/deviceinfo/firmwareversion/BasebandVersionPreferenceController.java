a03a4afb9affc087a9dada4d3f60e7f289ff2429
Bruno Martins <bgcngm@gmail.com>
2020-04-24 22:22:47 +0100

    BasebandVersionPreferenceController: Trim baseband if needed
    Some Qualcomm MSIM devices report the baseband version twice.
    Handle it gracefully, similarly to CAF.
    
    Change-Id: I93bd071f31fed120c20185c4e0ec40edb7ca342b

--- src/com/android/settings/deviceinfo/firmwareversion/BasebandVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/BasebandVersionPreferenceController.java
@@ -18,6 +18,7 @@ package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
 import android.os.SystemProperties;
+import android.text.TextUtils;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -41,7 +42,13 @@ public class BasebandVersionPreferenceController extends BasePreferenceControlle
 
     @Override
     public CharSequence getSummary() {
-        return SystemProperties.get(BASEBAND_PROPERTY,
+        String baseband = SystemProperties.get(BASEBAND_PROPERTY,
                 mContext.getString(R.string.device_info_default));
+        for (String str : baseband.split(",")) {
+            if (!TextUtils.isEmpty(str)) {
+                return str;
+            }
+        }
+        return baseband;
     }
 }
