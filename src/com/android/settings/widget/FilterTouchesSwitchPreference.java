5d73464b76747d367aa57668472a821bb64fbf15
Sunny Shao <sunnyshao@google.com>
2020-07-01 16:59:27 +0800

    Enable the filterTouchesWhenObscured in Special app access
    - Enable the filterTouchesWhenObscured attribute on all toggle
      switches in all pages of the special app access
    
    Bug: 155288585
    Test: make RunSettingsRoboTests
    Merged-In: I011cfe4b7e4e624a8338332ac47a353f7f3ab661
    Merged-In: I0731057ec6e77c6a0867784c729c3f5812ef6170
    Change-Id: I0731057ec6e77c6a0867784c729c3f5812ef6170

--- src/com/android/settings/widget/FilterTouchesSwitchPreference.java
+++ src/com/android/settings/widget/FilterTouchesSwitchPreference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,22 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.android.settings.widget;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.preference.PreferenceViewHolder;
 import androidx.preference.SwitchPreference;
 
-import com.android.settings.R;
+/**
+ *  This widget with enabled filterTouchesWhenObscured attribute use to replace
+ *  the {@link SwitchPreference} in the Special access app pages for security.
+ */
+public class FilterTouchesSwitchPreference extends SwitchPreference {
 
-public class AppSwitchPreference extends SwitchPreference {
+    public FilterTouchesSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 
-    public AppSwitchPreference(Context context) {
+    public FilterTouchesSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public FilterTouchesSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FilterTouchesSwitchPreference(Context context) {
         super(context);
-        setLayoutResource(R.layout.preference_app);
     }
 
     @Override
