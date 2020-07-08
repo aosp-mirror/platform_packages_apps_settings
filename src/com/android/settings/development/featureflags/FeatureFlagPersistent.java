7c737822d6fa8e77c9b195eaa3f7bfba8574b39c
Abhijeet Kaur <abkaur@google.com>
2020-07-08 13:22:20 +0100

    Remove Settings fuse flag as the change is now stable
    Bug: 160159282
    Test: Fuse flag does not show up in Settings -> Feature flags
    Change-Id: I9ae8fcc0e10cba4b6a0887f6960cf9671b1a6b38

--- src/com/android/settings/development/featureflags/FeatureFlagPersistent.java
+++ src/com/android/settings/development/featureflags/FeatureFlagPersistent.java
@@ -36,7 +36,6 @@ public class FeatureFlagPersistent {
         PERSISTENT_FLAGS = new HashSet<>();
         PERSISTENT_FLAGS.add(FeatureFlags.HEARING_AID_SETTINGS);
         PERSISTENT_FLAGS.add(FeatureFlags.DYNAMIC_SYSTEM);
-        PERSISTENT_FLAGS.add(FeatureFlags.SETTINGS_FUSE_FLAG);
     }
 
     public static boolean isEnabled(Context context, String feature) {
