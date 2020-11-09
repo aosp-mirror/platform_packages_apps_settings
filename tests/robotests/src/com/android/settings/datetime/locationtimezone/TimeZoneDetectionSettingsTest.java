218404394fdcfe3a4676cc411c10d752e820a439
Neil Fuller <nfuller@google.com>
2020-11-09 20:44:27 +0000

    Move the Location time zone detection setting
    Move the Location time zone detection setting to Date & Time as per
    UI review / product request.
    
    This initial commit just moves the existing MVP setting behavior to a
    different screen. Still invisible by default.
    
    Enable with:
    $ adb shell setprop persist.sys.location_time_zone_detection_feature_enabled 1
    .. plus a reboot.
    
    Bug: 152746236
    Test: Manual: build / boot / toggle switch in SettingsUI / inspect output of adb shell dumpsys time_zone_detector
    Test: m -j30 RunSettingsRoboTests ROBOTEST_FILTER="com.android.settings.datetime.LocationTimeZoneDetectionPreferenceControllerTest"
    Test: m -j30 RunSettingsRoboTests ROBOTEST_FILTER="com.android.settings.datetime.locationtimezone.TimeZoneDetectionTogglePreferenceControllerTest"
    Test: m -j30 RunSettingsRoboTests ROBOTEST_FILTER="com.android.settings.datetime.locationtimezone.TimeZoneDetectionSettingsTest"
    Change-Id: I9ecfa853014a9f92086a9cb2d34e1517474ceb93

--- tests/robotests/src/com/android/settings/datetime/locationtimezone/TimeZoneDetectionSettingsTest.java
+++ tests/robotests/src/com/android/settings/datetime/locationtimezone/TimeZoneDetectionSettingsTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.location;
+package com.android.settings.datetime.locationtimezone;
 
 import static com.google.common.truth.Truth.assertThat;
 
