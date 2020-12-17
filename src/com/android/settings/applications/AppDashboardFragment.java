92fce8ede440cc2cf9df4367c55decd0c8ac79eb
Yanting Yang <yantingyang@google.com>
2020-12-17 23:26:49 +0800

    Create a new apps page for silky home
    - Change recent apps to the list view.
    - Remove Conversations, Notifications and Permission manager.
    - Remove Wireless emergency alerts and Special app access.
    - Show App info preference if there's no recent apps.
    - Show general category with recent apps.
    - Use this page as the new apps entry on homepage for silky home.
    - Make old apps page unsearchable when silky home is enabled.
    - Exported new apps page in AndroidManifest.
    
    Bug: 168166015
    Bug: 174964405
    Test: robotest & visual
    Change-Id: I50d35a9d4723612214fce45a9e129fc175bc6831

---
+++ src/com/android/settings/applications/AppDashboardFragment.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.applications;
+
+import android.app.settings.SettingsEnums;
+import android.content.Context;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.drawer.CategoryKey;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** Settings page for apps. */
+@SearchIndexable
+public class AppDashboardFragment extends DashboardFragment {
+
+    private static final String TAG = "AppDashboardFragment";
+    private AppsPreferenceController mAppsPreferenceController;
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new AppsPreferenceController(context));
+        return controllers;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.SETTINGS_APP_NOTIF_CATEGORY;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getHelpResource() {
+        return R.string.help_url_apps_and_notifications;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.apps;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mAppsPreferenceController = use(AppsPreferenceController.class);
+        mAppsPreferenceController.setFragment(this /* fragment */);
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context);
+    }
+
+    @Override
+    public String getCategoryKey() {
+        // TODO(b/174964405): Remove this function when the silky flag was deprecated.
+        // To include injection tiles, map this app fragment to the app category in the short term.
+        // When we deprecate the silky flag, we have to:
+        // 1. Remove this method.
+        // 2. Update the mapping in DashboardFragmentRegistry.PARENT_TO_CATEGORY_KEY_MAP.
+        return CategoryKey.CATEGORY_APPS;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.apps);
+}
