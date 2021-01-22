b56ea7e9e3401288bdedfcf60bcc04f237f8f4c7
Jason Chiu <chiujason@google.com>
2021-01-22 13:40:08 +0800

    Adjust homepage styles and layouts
    - Fine tune the header text size and position
    - Fix the header overlapping problem on the homepage and collapsing
      toolbar pages when configuration changes by adding android:id.
    - New AppBarScrollingViewBehavior for the homepage to avoid manually
      offsetting the IA
    - Correct the formula of counting search box height
    
    Test: robotest, manual
    Fixes: 177967923
    Fixes: 177968564
    Change-Id: Id3ed44f4296fa2a19d6890b009d3881a5c140bbd

---
+++ src/com/android/settings/widget/HomepageAppBarScrollingViewBehavior.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+/**
+ * This scrolling view behavior will set the background of the {@link AppBarLayout} as
+ * transparent and without the elevation.
+ */
+public class HomepageAppBarScrollingViewBehavior extends AppBarLayout.ScrollingViewBehavior {
+    private boolean mInitialized;
+
+    public HomepageAppBarScrollingViewBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        boolean changed = super.onDependentViewChanged(parent, child, dependency);
+        if (!mInitialized && dependency instanceof AppBarLayout) {
+            mInitialized = true;
+            AppBarLayout appBarLayout = (AppBarLayout) dependency;
+            setAppBarLayoutTransparent(appBarLayout);
+        }
+        return changed;
+    }
+
+    @VisibleForTesting
+    void setAppBarLayoutTransparent(AppBarLayout appBarLayout) {
+        appBarLayout.setBackgroundColor(Color.TRANSPARENT);
+        appBarLayout.setTargetElevation(0);
+    }
+}
