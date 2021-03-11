fd41cce94b81e51617f8d2edc0927cc8717f60f2
lesl <lesl@google.com>
2021-03-11 22:15:29 +0800

    wifi: Use new API for wifi data usage function
    For wifi, a old API: buildTemplateWifiWildcard will includes the merged
    wifi which is included to mobile usage. It should not double count
    again. Call new API: buildTemplateWifi with
    NetworkId: NetworkTemplate.WIFI_NETWORKID_ALL
    and null subscriberId to get non-merged wifi usage.
    
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=AppDataUsageTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=AppDataUsagePreferenceControllerTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=DataUsageSummaryTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=DataUsageSummaryPreferenceTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=DataUsageSummaryPreferenceControllerTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=DataUsageUtilsTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=NetworkProviderSettingsTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=SettingsDumpServiceTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=WifiSettingsTest
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=DataUsageInfoControllerTest
    Test: Manual Test, wifi data usage count correctly.
    Bug: 176396812
    Merged-In: Ia4d8fa67ea3cb75c2d35be9ab60e5c5ffa391ffb
    Change-Id: Ia4d8fa67ea3cb75c2d35be9ab60e5c5ffa391ffb

--- src/com/android/settings/datausage/DataUsageSummaryPreference.java
+++ src/com/android/settings/datausage/DataUsageSummaryPreference.java
@@ -224,7 +224,8 @@ public class DataUsageSummaryPreference extends Preference {
     static void launchWifiDataUsage(Context context) {
         final Bundle args = new Bundle(1);
         args.putParcelable(DataUsageList.EXTRA_NETWORK_TEMPLATE,
-                NetworkTemplate.buildTemplateWifiWildcard());
+                NetworkTemplate.buildTemplateWifi(NetworkTemplate.WIFI_NETWORKID_ALL,
+                null /* subscriberId */));
         args.putInt(DataUsageList.EXTRA_NETWORK_TYPE, ConnectivityManager.TYPE_WIFI);
         final SubSettingLauncher launcher = new SubSettingLauncher(context)
                 .setArguments(args)
@@ -364,7 +365,9 @@ public class DataUsageSummaryPreference extends Preference {
     @VisibleForTesting
     long getHistoricalUsageLevel() {
         final DataUsageController controller = new DataUsageController(getContext());
-        return controller.getHistoricalUsageLevel(NetworkTemplate.buildTemplateWifiWildcard());
+        return controller.getHistoricalUsageLevel(
+                NetworkTemplate.buildTemplateWifi(NetworkTemplate.WIFI_NETWORKID_ALL,
+                null /* subscriberId */));
     }
 
 }
