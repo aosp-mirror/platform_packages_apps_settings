d86b57c2078335790f14b328e14e4d4655d353b0
Hugh Chen <hughchen@google.com>
2021-03-12 10:40:20 +0800

    RESTRICT AUTOMERGE Fix bluetooth settings will broadcast to anywhere when some cases
    BluetoothPermissionActivity and DevicePickerFragment will send
    broadcast to return the result to calling apps. As this broadcast
    intent is from Settings with uid 1000, it will be sent to any
    protected BroadcastReceivers in the device. It can make an attacker
    send broadcast to protected BroadcastReceivers like factory reset intent
    (android/com.android.server.MasterClearReceiver) via
    BluetoothPermissionActivity or DevicePickerFragment.
    
    This CL will not allow to set package name and class name to avoid
    the attacker.
    
    Bug: 179386960
    Bug: 179386068
    Test: make -j42 RunSettingsRoboTests and use test apk to manually test
    to verify factory reset not started and no system UI notification.
    
    Change-Id: Id27a78091ab578077853b8fbb97a4422cff0a158
    (cherry picked from commit 8adedc62496cf8cf6ecfc6ccf23b0b248081d7d4)
    (cherry picked from commit c2adcb2d3a213e94abcdb2a88b3c19f59566788e)

--- tests/robotests/src/com/android/settings/bluetooth/DevicePickerFragmentTest.java
+++ tests/robotests/src/com/android/settings/bluetooth/DevicePickerFragmentTest.java
@@ -16,7 +16,17 @@
 
 package com.android.settings.bluetooth;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+
+import com.android.settingslib.bluetooth.CachedBluetoothDevice;
 
 import com.android.settings.TestConfig;
 import com.android.settings.testutils.SettingsRobolectricTestRunner;
@@ -25,9 +35,12 @@ import com.android.settingslib.bluetooth.LocalBluetoothAdapter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 
 @RunWith(SettingsRobolectricTestRunner.class)
 @Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
@@ -36,14 +49,17 @@ public class DevicePickerFragmentTest {
     private LocalBluetoothAdapter mLocalAdapter;
     @Mock
     private BluetoothProgressCategory mAvailableDevicesCategory;
+
     private DevicePickerFragment mFragment;
+    private Context mContext;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
         mFragment = new DevicePickerFragment();
-
+        mContext = spy(RuntimeEnvironment.application);
+        mFragment.mContext = mContext;
         mFragment.mAvailableDevicesCategory = mAvailableDevicesCategory;
     }
 
@@ -54,5 +70,19 @@ public class DevicePickerFragmentTest {
         mFragment.onScanningStateChanged(true);
 
         verify(mAvailableDevicesCategory).setProgress(true);
+    }
+
+    @Test
+    public void sendBroadcastWithPermission() {
+        final CachedBluetoothDevice cachedDevice = mock(CachedBluetoothDevice.class);
+        final BluetoothDevice bluetoothDevice = mock(BluetoothDevice.class);
+        final ArgumentCaptor<Intent> intentCaptor = ArgumentCaptor.forClass(Intent.class);
+        when(cachedDevice.getDevice()).thenReturn(bluetoothDevice);
+        mFragment.mSelectedDevice = bluetoothDevice;
+
+        mFragment.onDeviceBondStateChanged(cachedDevice, BluetoothDevice.BOND_BONDED);
+
+        verify(mContext).sendBroadcast(intentCaptor.capture(),
+                eq("android.permission.BLUETOOTH_ADMIN"));
     }
 }
