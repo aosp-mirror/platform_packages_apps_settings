340a7fb64056909a2198b9689fe55cec6ec51551
Quang Luong <qal@google.com>
2021-06-21 11:04:16 -0700

    Rename Enhanced MAC Randomization to Non-Persistent
    Enhanced MAC Randomization has been renamed to Non-Persistent MAC
    Randomization, so this change should also be reflected in Settings code.
    
    Bug: 162854429
    Test: verify developer toggle for non-persistent MAC still worked
    Change-Id: I5d647a666ac9ae1125ed96e690608e4ad414ac69

--- src/com/android/settings/development/WifiNonPersistentMacRandomizationPreferenceController.java
+++ src/com/android/settings/development/WifiNonPersistentMacRandomizationPreferenceController.java
@@ -26,30 +26,30 @@ import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.development.DeveloperOptionsPreferenceController;
 
 /**
- * Developer option controller for enhanced MAC randomization.
+ * Developer option controller for non-persistent MAC randomization.
  */
-public class WifiEnhancedMacRandomizationPreferenceController
+public class WifiNonPersistentMacRandomizationPreferenceController
         extends DeveloperOptionsPreferenceController
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
-    private static final String WIFI_ENHANCED_MAC_RANDOMIZATION_KEY =
-            "wifi_enhanced_mac_randomization";
-    private static final String ENHANCED_MAC_RANDOMIZATION_FEATURE_FLAG =
-            "enhanced_mac_randomization_force_enabled";
+    private static final String WIFI_NON_PERSISTENT_MAC_RANDOMIZATION_KEY =
+            "wifi_non_persistent_mac_randomization";
+    private static final String NON_PERSISTENT_MAC_RANDOMIZATION_FEATURE_FLAG =
+            "non_persistent_mac_randomization_force_enabled";
 
-    public WifiEnhancedMacRandomizationPreferenceController(Context context) {
+    public WifiNonPersistentMacRandomizationPreferenceController(Context context) {
         super(context);
     }
 
     @Override
     public String getPreferenceKey() {
-        return WIFI_ENHANCED_MAC_RANDOMIZATION_KEY;
+        return WIFI_NON_PERSISTENT_MAC_RANDOMIZATION_KEY;
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         int isEnabledInt = ((Boolean) newValue) ? 1 : 0;
         Settings.Global.putInt(mContext.getContentResolver(),
-                ENHANCED_MAC_RANDOMIZATION_FEATURE_FLAG, isEnabledInt);
+                NON_PERSISTENT_MAC_RANDOMIZATION_FEATURE_FLAG, isEnabledInt);
         return true;
     }
 
@@ -57,7 +57,7 @@ public class WifiEnhancedMacRandomizationPreferenceController
     public void updateState(Preference preference) {
         boolean enabled = false;
         if (Settings.Global.getInt(mContext.getContentResolver(),
-                ENHANCED_MAC_RANDOMIZATION_FEATURE_FLAG, 0) == 1) {
+                NON_PERSISTENT_MAC_RANDOMIZATION_FEATURE_FLAG, 0) == 1) {
             enabled = true;
         }
         ((SwitchPreference) mPreference).setChecked(enabled);
@@ -67,7 +67,7 @@ public class WifiEnhancedMacRandomizationPreferenceController
     protected void onDeveloperOptionsSwitchDisabled() {
         super.onDeveloperOptionsSwitchDisabled();
         Settings.Global.putInt(mContext.getContentResolver(),
-                ENHANCED_MAC_RANDOMIZATION_FEATURE_FLAG, 0);
+                NON_PERSISTENT_MAC_RANDOMIZATION_FEATURE_FLAG, 0);
         ((SwitchPreference) mPreference).setChecked(false);
     }
 }
