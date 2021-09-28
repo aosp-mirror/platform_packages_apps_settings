e893a890ca47653dc47331bd139d8c86f58b7a2b
ykhung <ykhung@google.com>
2021-09-28 17:30:04 +0800

    [Robustness] protect get battery stats from DeadSystemException
    Refine the original workaround patch in the ag/15527095 from NPE to
    return default BatteryUsageStats instance as we merged in the
    ag/15919139 (align the same solution).
    
    Bug: 195306545
    Test: make SettingsRoboTests
    Change-Id: Ic03e8296d16ecb8629155f75727e9cde48c303eb

--- src/com/android/settings/fuelgauge/batterytip/BatteryTipLoader.java
+++ src/com/android/settings/fuelgauge/batterytip/BatteryTipLoader.java
@@ -18,7 +18,6 @@ package com.android.settings.fuelgauge.batterytip;
 
 import android.content.Context;
 import android.os.BatteryUsageStats;
-import android.util.Log;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -68,16 +67,11 @@ public class BatteryTipLoader extends AsyncLoaderCompat<List<BatteryTip>> {
         final BatteryInfo batteryInfo = mBatteryUtils.getBatteryInfo(TAG);
         final Context context = getContext();
 
-        tips.add(new EarlyWarningDetector(policy, context).detect());
-        if (batteryInfo == null) {
-            Log.w(TAG, "loadInBackground() batteryInfo = null");
-            return tips;
-        }
-
         tips.add(new LowBatteryDetector(context, policy, batteryInfo).detect());
         tips.add(new HighUsageDetector(context, policy, mBatteryUsageStats, batteryInfo).detect());
         tips.add(new SmartBatteryDetector(
                 context, policy, batteryInfo, context.getContentResolver()).detect());
+        tips.add(new EarlyWarningDetector(policy, context).detect());
         tips.add(new BatteryDefenderDetector(batteryInfo).detect());
         Collections.sort(tips);
         return tips;
