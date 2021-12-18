8e1cfdf29d82668194439b2be3c0b5197ab5b9c8
palaych <palaych@arrowos.net>
2021-12-19 06:12:34 +0900

    Settings: Switch to VERSION.INCREMENTAL instead of FINGERPRINT
    Change-Id: I1f539acc3dfd83b6b5833dd7df45b1c3a6877345
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/slices/SlicesDatabaseHelper.java
+++ src/com/android/settings/slices/SlicesDatabaseHelper.java
@@ -239,6 +239,6 @@ public class SlicesDatabaseHelper extends SQLiteOpenHelper {
 
     @VisibleForTesting
     String getBuildTag() {
-        return Build.FINGERPRINT;
+        return Build.VERSION.INCREMENTAL;
     }
 }
