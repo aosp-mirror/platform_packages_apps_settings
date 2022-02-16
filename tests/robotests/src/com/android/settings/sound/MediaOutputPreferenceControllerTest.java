a95180f2288783b1828bceba396fe3a420edb674
William Escande <wescande@google.com>
2022-02-16 20:00:59 +0100

    Remove hidden Bluetooth dependencies
    Bug: 217736913
    Test: pre-submit
    Change-Id: Ib4e12e8e350ffa08b48f58e8256991d7ed4fa013

--- tests/robotests/src/com/android/settings/sound/MediaOutputPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/sound/MediaOutputPreferenceControllerTest.java
@@ -170,7 +170,7 @@ public class MediaOutputPreferenceControllerTest {
         when(mLocalBluetoothProfileManager.getA2dpProfile()).thenReturn(mA2dpProfile);
         when(mLocalBluetoothProfileManager.getHearingAidProfile()).thenReturn(mHearingAidProfile);
 
-        mBluetoothManager = new BluetoothManager(mContext);
+        mBluetoothManager = mContext.getSystemService(BluetoothManager.class);
         mBluetoothAdapter = mBluetoothManager.getAdapter();
 
         mBluetoothDevice = spy(mBluetoothAdapter.getRemoteDevice(TEST_DEVICE_ADDRESS_1));
