25b0db21ac2c9814c017837fb9b0da14aa65007a
Weng Su <wengsu@google.com>
2022-05-11 22:51:45 +0800

    Get app label by launched package
    - Activity#getCallingPackage will return null in some special cases, use the launched package instead.
    
    Bug: 194709435
    Test: manual test
    make RunSettingsRoboTests ROBOTEST_FILTER=WifiScanModeActivityTest
    
    Change-Id: I4f22bab7592dedf75dd36daf5e18dbc934bc8655

--- tests/robotests/src/com/android/settings/wifi/WifiScanModeActivityTest.java
+++ tests/robotests/src/com/android/settings/wifi/WifiScanModeActivityTest.java
@@ -16,16 +16,75 @@
 
 package com.android.settings.wifi;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import android.text.TextUtils;
+
+import com.android.settings.testutils.shadow.ShadowUtils;
+import com.android.settingslib.wifi.WifiPermissionChecker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(shadows = {ShadowUtils.class})
 public class WifiScanModeActivityTest {
+
+    static final String LAUNCHED_PACKAGE = "launched_package";
+    static final String APP_LABEL = "app_label";
+
+    @Rule
+    public final MockitoRule mMockitoRule = MockitoJUnit.rule();
+    @Mock
+    WifiPermissionChecker mWifiPermissionChecker;
+
+    WifiScanModeActivity mActivity;
+
+    @Before
+    public void setUp() {
+        mActivity = spy(Robolectric.setupActivity(WifiScanModeActivity.class));
+        mActivity.mWifiPermissionChecker = mWifiPermissionChecker;
+    }
+
+    @After
+    public void tearDown() {
+        ShadowUtils.reset();
+    }
+
     @Test
     public void launchActivity_noIntentAction_shouldNotFatalException() {
         WifiScanModeActivity wifiScanModeActivity =
                 Robolectric.setupActivity(WifiScanModeActivity.class);
+    }
+
+    @Test
+    public void refreshAppLabel_noPackageName_shouldNotFatalException() {
+        when(mWifiPermissionChecker.getLaunchedPackage()).thenReturn(null);
+
+        mActivity.refreshAppLabel();
+
+        assertThat(TextUtils.isEmpty(mActivity.mApp)).isTrue();
+    }
+
+    @Test
+    public void refreshAppLabel_hasPackageName_shouldHasAppLabel() {
+        ShadowUtils.setApplicationLabel(LAUNCHED_PACKAGE, APP_LABEL);
+        when(mWifiPermissionChecker.getLaunchedPackage()).thenReturn(LAUNCHED_PACKAGE);
+
+        mActivity.refreshAppLabel();
+
+        assertThat(mActivity.mApp).isEqualTo(APP_LABEL);
     }
 }
