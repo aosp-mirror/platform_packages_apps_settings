2b0d9cfc5d61d82624b6c9d773eaf8f8fdc7878b
Edgar Wang <edgarwang@google.com>
2022-05-19 09:20:30 +0800

    Fix talkback speak "double tap to activate" on LabeledSeekBarPreference
    - SeekBarPreference should be unselectable
    - remove unnecessary handling of Summary
    - set currect Title color when preference is enabled and unselectable
    
    Bug: 230553896
    Test: robotest
    Change-Id: Ie5d819088dc3c435005ddd00e232b2f6992b234a

--- src/com/android/settings/widget/SeekBarPreference.java
+++ src/com/android/settings/widget/SeekBarPreference.java
@@ -78,12 +78,7 @@ public class SeekBarPreference extends RestrictedPreference
                 com.android.internal.R.layout.preference_widget_seekbar);
         a.recycle();
 
-        a = context.obtainStyledAttributes(
-                attrs, com.android.internal.R.styleable.Preference, defStyleAttr, defStyleRes);
-        final boolean isSelectable = a.getBoolean(
-                com.android.settings.R.styleable.Preference_android_selectable, false);
-        setSelectable(isSelectable);
-        a.recycle();
+        setSelectable(false);
 
         setLayoutResource(layoutResId);
     }
@@ -168,11 +163,6 @@ public class SeekBarPreference extends RestrictedPreference
                 }
             }
         });
-    }
-
-    @Override
-    public CharSequence getSummary() {
-        return null;
     }
 
     @Override
