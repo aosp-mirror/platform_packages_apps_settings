1d26b6d898dc9f8b7482c5a433c30c7503a8d5e9
menghanli <menghanli@google.com>
2022-06-29 08:22:59 +0800

    Refactor CaptionPropertiesFragment to improve maintainability
    Root cause: There is a bunch of different logic of preferences in CaptionPropertiesFragment. It’s hard to implement new features and hard to maintain and hard to be testable.
    Solution: Move out different logic of CaptionPropertiesFragment into controllers to reduce the complexity of the relationship between preference and fragment.
    
    Bug: 197695932
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=CaptionPropertiesFragmentTest CaptionTogglePreferenceControllerTest
    Change-Id: I7013a7aa284b587c329c2e678dbb079e553ea94d

--- src/com/android/settings/accessibility/CaptionPropertiesFragment.java
+++ src/com/android/settings/accessibility/CaptionPropertiesFragment.java
@@ -17,63 +17,21 @@
 package com.android.settings.accessibility;
 
 import android.app.settings.SettingsEnums;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.view.accessibility.CaptioningManager;
-import android.widget.Switch;
-
-import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.widget.SettingsMainSwitchPreference;
 import com.android.settingslib.search.SearchIndexable;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /** Settings fragment containing captioning properties. */
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
-public class CaptionPropertiesFragment extends DashboardFragment
-        implements OnPreferenceChangeListener, OnMainSwitchChangeListener {
+public class CaptionPropertiesFragment extends DashboardFragment {
 
     private static final String TAG = "CaptionPropertiesFragment";
-    private static final String PREF_SWITCH = "captioning_preference_switch";
-    private static final String PREF_TEXT = "captioning_caption_appearance";
-    private static final String PREF_MORE = "captioning_more_options";
-
-    private CaptioningManager mCaptioningManager;
-
-    private SettingsMainSwitchPreference mSwitch;
-    private Preference mTextAppearance;
-    private Preference mMoreOptions;
-
-    private final List<Preference> mPreferenceList = new ArrayList<>();
 
     @Override
     public int getMetricsCategory() {
         return SettingsEnums.ACCESSIBILITY_CAPTION_PROPERTIES;
-    }
-
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        super.onCreatePreferences(savedInstanceState, rootKey);
-
-        mCaptioningManager = (CaptioningManager) getSystemService(Context.CAPTIONING_SERVICE);
-
-        initializeAllPreferences();
-        installUpdateListeners();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mSwitch.setChecked(mCaptioningManager.isEnabled());
     }
 
     @Override
@@ -86,32 +44,6 @@ public class CaptionPropertiesFragment extends DashboardFragment
         return TAG;
     }
 
-    private void initializeAllPreferences() {
-        mSwitch = (SettingsMainSwitchPreference) findPreference(PREF_SWITCH);
-        mTextAppearance = (Preference) findPreference(PREF_TEXT);
-        mMoreOptions = (Preference) findPreference(PREF_MORE);
-
-        mPreferenceList.add(mTextAppearance);
-        mPreferenceList.add(mMoreOptions);
-    }
-
-    private void installUpdateListeners() {
-        mSwitch.setOnPreferenceChangeListener(this);
-        mSwitch.addOnSwitchChangeListener(this);
-
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
-        final ContentResolver cr = getActivity().getContentResolver();
-        if (mSwitch == preference) {
-            Settings.Secure.putInt(
-                    cr, Settings.Secure.ACCESSIBILITY_CAPTIONING_ENABLED, (boolean) value ? 1 : 0);
-        }
-
-        return true;
-    }
-
     @Override
     public int getHelpResource() {
         return R.string.help_url_caption;
@@ -119,11 +51,4 @@ public class CaptionPropertiesFragment extends DashboardFragment
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(R.xml.captioning_settings);
-
-    @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
-        final ContentResolver cr = getActivity().getContentResolver();
-        Settings.Secure.putInt(
-                cr, Settings.Secure.ACCESSIBILITY_CAPTIONING_ENABLED, isChecked ? 1 : 0);
-    }
 }
