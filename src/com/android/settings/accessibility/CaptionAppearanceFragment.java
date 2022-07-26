0e7f366ffa69da4f77cedf878a4b369f022ae9d4
menghanli <menghanli@google.com>
2022-07-26 10:52:25 +0800

    Refactor CaptionAppearanceFragment to improve maintainability (5/n)
    Root cause: There is a bunch of different logic of preferences in CaptionAppearanceFragment. It’s hard to implement new features and hard to maintain and hard to be testable.
    Solution: Move out custom perference visiblity logic of CaptionAppearanceFragment into controllers to reduce the complexity of the relationship between preference and fragment.
    
    Bug: 197695932
    Test: make RunSettingsRoboTests ROBOTEST_FILTER=com.android.settings.accessibility
    Change-Id: I53d4ca3809031587f8d6853b427774fd322a71d8

--- src/com/android/settings/accessibility/CaptionAppearanceFragment.java
+++ src/com/android/settings/accessibility/CaptionAppearanceFragment.java
@@ -17,68 +17,21 @@
 package com.android.settings.accessibility;
 
 import android.app.settings.SettingsEnums;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.provider.Settings;
-import android.view.accessibility.CaptioningManager;
-import android.view.accessibility.CaptioningManager.CaptionStyle;
-
-import androidx.annotation.VisibleForTesting;
-import androidx.preference.PreferenceCategory;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import java.util.Arrays;
-import java.util.List;
-
 /** Settings fragment containing font style of captioning properties. */
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class CaptionAppearanceFragment extends DashboardFragment {
 
     private static final String TAG = "CaptionAppearanceFragment";
-    @VisibleForTesting
-    static final String PREF_CUSTOM = "custom";
-    @VisibleForTesting
-    static final List<String> CAPTIONING_FEATURE_KEYS = Arrays.asList(
-            Settings.Secure.ACCESSIBILITY_CAPTIONING_PRESET
-    );
-
-    private final Handler mHandler = new Handler(Looper.getMainLooper());
-    @VisibleForTesting
-    AccessibilitySettingsContentObserver mSettingsContentObserver;
-    private CaptioningManager mCaptioningManager;
-    private PreferenceCategory mCustom;
 
     @Override
     public int getMetricsCategory() {
         return SettingsEnums.ACCESSIBILITY_CAPTION_APPEARANCE;
-    }
-
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        super.onCreatePreferences(savedInstanceState, rootKey);
-        mCaptioningManager = getContext().getSystemService(CaptioningManager.class);
-        mSettingsContentObserver = new AccessibilitySettingsContentObserver(mHandler);
-        mSettingsContentObserver.registerKeysToObserverCallback(CAPTIONING_FEATURE_KEYS,
-                key -> refreshShowingCustom());
-        mCustom = findPreference(PREF_CUSTOM);
-        refreshShowingCustom();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mSettingsContentObserver.register(getContext().getContentResolver());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        getContext().getContentResolver().unregisterContentObserver(mSettingsContentObserver);
     }
 
     @Override
@@ -94,12 +47,6 @@ public class CaptionAppearanceFragment extends DashboardFragment {
     @Override
     public int getHelpResource() {
         return R.string.help_url_caption;
-    }
-
-    private void refreshShowingCustom() {
-        final boolean isCustomPreset =
-                mCaptioningManager.getRawUserStyle() == CaptionStyle.PRESET_CUSTOM;
-        mCustom.setVisible(isCustomPreset);
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
