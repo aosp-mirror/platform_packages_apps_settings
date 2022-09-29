9ef3cf257299c3dcd2b2b3305739790aaf65b0f5
Julia Reynolds <juliacr@google.com>
2022-09-29 14:56:30 -0400

    Add link to more app settings
    Test: MoreSettingsPreferenceControllerTest
    Test: test app that can handle the settings intent
    Bug: 236932102
    Change-Id: Iae778c93057e47aa0f664d3faab21640b2cd5193

--- src/com/android/settings/applications/specialaccess/notificationaccess/NotificationAccessDetails.java
+++ src/com/android/settings/applications/specialaccess/notificationaccess/NotificationAccessDetails.java
@@ -124,6 +124,9 @@ public class NotificationAccessDetails extends DashboardFragment {
                 .setCn(mComponentName)
                 .setUserId(mUserId)
                 .setTargetSdk(listenerTargetSdk);
+        use(MoreSettingsPreferenceController.class)
+                .setPackage(mComponentName.getPackageName())
+                .setPackageManager(mPm);
         final int finalListenerTargetSdk = listenerTargetSdk;
         getPreferenceControllers().forEach(controllers -> {
             controllers.forEach(controller -> {
