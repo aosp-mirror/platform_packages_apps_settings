2df165a58aa8d485e4545f633e99a5988597a6bf
Vania Januar <vanjan@google.com>
2022-10-20 14:44:59 +0100

    Add a stylus controller to Bluetooth Device Details.
    Feature is currently flagged behing SETTINGS_SHOW_STYLUS_PREFERENCES.
    
    Bug: 251201006
    Test: SettingsRoboTests StylusDevicesControllerTest
    DD: go/stylus-connected-devices-doc
    Change-Id: I438b7fe5ca1c94f9dfb506c8918d0e6cb005ca33

--- tests/robotests/src/com/android/settings/bluetooth/BluetoothDeviceDetailsFragmentTest.java
+++ tests/robotests/src/com/android/settings/bluetooth/BluetoothDeviceDetailsFragmentTest.java
@@ -25,16 +25,21 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
 import android.os.UserManager;
+import android.util.FeatureFlagUtils;
+import android.view.InputDevice;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceScreen;
@@ -51,6 +56,7 @@ import org.mockito.Answers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
@@ -68,6 +74,7 @@ public class BluetoothDeviceDetailsFragmentTest {
     private RoboMenu mMenu;
     private MenuInflater mInflater;
     private FragmentTransaction mFragmentTransaction;
+    private FragmentActivity mActivity;
 
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private CachedBluetoothDevice mCachedDevice;
@@ -77,29 +84,18 @@ public class BluetoothDeviceDetailsFragmentTest {
     private PreferenceScreen mPreferenceScreen;
     @Mock
     private UserManager mUserManager;
+    @Mock
+    private InputManager mInputManager;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         mContext = spy(RuntimeEnvironment.application);
+        doReturn(mInputManager).when(mContext).getSystemService(InputManager.class);
+        removeInputDeviceWithMatchingBluetoothAddress();
         FakeFeatureFactory.setupForTest();
 
-        mFragment = spy(BluetoothDeviceDetailsFragment.newInstance(TEST_ADDRESS));
-        doReturn(mLocalManager).when(mFragment).getLocalBluetoothManager(any());
-        doReturn(mCachedDevice).when(mFragment).getCachedDevice(any());
-        doReturn(mPreferenceScreen).when(mFragment).getPreferenceScreen();
-        doReturn(mUserManager).when(mFragment).getUserManager();
-
-        FragmentManager fragmentManager = mock(FragmentManager.class);
-        when(mFragment.getFragmentManager()).thenReturn(fragmentManager);
-        mFragmentTransaction = mock(FragmentTransaction.class);
-        when(fragmentManager.beginTransaction()).thenReturn(mFragmentTransaction);
-
-        when(mCachedDevice.getAddress()).thenReturn(TEST_ADDRESS);
-        when(mCachedDevice.getIdentityAddress()).thenReturn(TEST_ADDRESS);
-        Bundle args = new Bundle();
-        args.putString(BluetoothDeviceDetailsFragment.KEY_DEVICE_ADDRESS, TEST_ADDRESS);
-        mFragment.setArguments(args);
+        mFragment = setupFragment();
         mFragment.onAttach(mContext);
 
         mMenu = new RoboMenu(mContext);
@@ -111,6 +107,43 @@ public class BluetoothDeviceDetailsFragmentTest {
         assertThat(mFragment.mDeviceAddress).isEqualTo(TEST_ADDRESS);
         assertThat(mFragment.mManager).isEqualTo(mLocalManager);
         assertThat(mFragment.mCachedDevice).isEqualTo(mCachedDevice);
+        assertThat(mFragment.mInputDevice).isEqualTo(null);
+    }
+
+    @Test
+    public void verifyOnAttachResult_flagEnabledAndInputDeviceSet_returnsInputDevice() {
+        FeatureFlagUtils.setEnabled(mContext, FeatureFlagUtils.SETTINGS_SHOW_STYLUS_PREFERENCES,
+                true);
+        InputDevice inputDevice = createInputDeviceWithMatchingBluetoothAddress();
+        BluetoothDeviceDetailsFragment fragment = setupFragment();
+        FragmentActivity activity = mock(FragmentActivity.class);
+        doReturn(inputDevice).when(fragment).getInputDevice(any());
+        doReturn(activity).when(fragment).getActivity();
+
+        fragment.onAttach(mContext);
+
+        assertThat(fragment.mDeviceAddress).isEqualTo(TEST_ADDRESS);
+        assertThat(fragment.mManager).isEqualTo(mLocalManager);
+        assertThat(fragment.mCachedDevice).isEqualTo(mCachedDevice);
+        assertThat(fragment.mInputDevice).isEqualTo(inputDevice);
+    }
+
+    @Test
+    public void verifyOnAttachResult_flagDisabled_returnsNullInputDevice() {
+        FeatureFlagUtils.setEnabled(mContext, FeatureFlagUtils.SETTINGS_SHOW_STYLUS_PREFERENCES,
+                false);
+        InputDevice inputDevice = createInputDeviceWithMatchingBluetoothAddress();
+        BluetoothDeviceDetailsFragment fragment = setupFragment();
+        FragmentActivity activity = mock(FragmentActivity.class);
+        doReturn(inputDevice).when(fragment).getInputDevice(any());
+        doReturn(activity).when(fragment).getActivity();
+
+        fragment.onAttach(mContext);
+
+        assertThat(fragment.mDeviceAddress).isEqualTo(TEST_ADDRESS);
+        assertThat(fragment.mManager).isEqualTo(mLocalManager);
+        assertThat(fragment.mCachedDevice).isEqualTo(mCachedDevice);
+        assertThat(fragment.mInputDevice).isEqualTo(null);
     }
 
     @Test
@@ -123,6 +156,31 @@ public class BluetoothDeviceDetailsFragmentTest {
     }
 
     @Test
+    public void getTitle_inputDeviceTitle() {
+        FeatureFlagUtils.setEnabled(mContext, FeatureFlagUtils.SETTINGS_SHOW_STYLUS_PREFERENCES,
+                true);
+        doReturn(mock(InputDevice.class)).when(mFragment).getInputDevice(mContext);
+        mFragment.onAttach(mContext);
+
+        mFragment.setTitleForInputDevice();
+
+        assertThat(mActivity.getTitle().toString()).isEqualTo(
+                mContext.getString(R.string.stylus_device_details_title));
+    }
+
+    @Test
+    public void getTitle_inputDeviceNull_doesNotSetTitle() {
+        FeatureFlagUtils.setEnabled(mContext, FeatureFlagUtils.SETTINGS_SHOW_STYLUS_PREFERENCES,
+                true);
+        doReturn(null).when(mFragment).getInputDevice(mContext);
+        mFragment.onAttach(mContext);
+
+        mFragment.setTitleForInputDevice();
+
+        verify(mActivity, times(0)).setTitle(any());
+    }
+
+    @Test
     public void editMenu_clicked_showDialog() {
         mFragment.onCreateOptionsMenu(mMenu, mInflater);
         final MenuItem item = mMenu.getItem(0);
@@ -131,7 +189,7 @@ public class BluetoothDeviceDetailsFragmentTest {
         mFragment.onOptionsItemSelected(item);
 
         assertThat(item.getItemId())
-            .isEqualTo(BluetoothDeviceDetailsFragment.EDIT_DEVICE_NAME_ITEM_ID);
+                .isEqualTo(BluetoothDeviceDetailsFragment.EDIT_DEVICE_NAME_ITEM_ID);
         verify(mFragmentTransaction).add(captor.capture(), eq(RemoteDeviceNameDialogFragment.TAG));
         RemoteDeviceNameDialogFragment dialog = (RemoteDeviceNameDialogFragment) captor.getValue();
         assertThat(dialog).isNotNull();
@@ -144,5 +202,45 @@ public class BluetoothDeviceDetailsFragmentTest {
         mFragment.finishFragmentIfNecessary();
 
         verify(mFragment).finish();
+    }
+
+    private InputDevice createInputDeviceWithMatchingBluetoothAddress() {
+        doReturn(new int[]{0}).when(mInputManager).getInputDeviceIds();
+        InputDevice device = mock(InputDevice.class);
+        doReturn(TEST_ADDRESS).when(mInputManager).getInputDeviceBluetoothAddress(0);
+        doReturn(device).when(mInputManager).getInputDevice(0);
+        return device;
+    }
+
+    private InputDevice removeInputDeviceWithMatchingBluetoothAddress() {
+        doReturn(new int[]{0}).when(mInputManager).getInputDeviceIds();
+        doReturn(null).when(mInputManager).getInputDeviceBluetoothAddress(0);
+        return null;
+    }
+
+    private BluetoothDeviceDetailsFragment setupFragment() {
+        BluetoothDeviceDetailsFragment fragment = spy(
+                BluetoothDeviceDetailsFragment.newInstance(TEST_ADDRESS));
+        doReturn(mLocalManager).when(fragment).getLocalBluetoothManager(any());
+        doReturn(mCachedDevice).when(fragment).getCachedDevice(any());
+        doReturn(mPreferenceScreen).when(fragment).getPreferenceScreen();
+        doReturn(mUserManager).when(fragment).getUserManager();
+
+        mActivity = spy(Robolectric.setupActivity(FragmentActivity.class));
+        doReturn(mActivity).when(fragment).getActivity();
+        doReturn(mContext).when(fragment).getContext();
+
+        FragmentManager fragmentManager = mock(FragmentManager.class);
+        doReturn(fragmentManager).when(fragment).getFragmentManager();
+        mFragmentTransaction = mock(FragmentTransaction.class);
+        doReturn(mFragmentTransaction).when(fragmentManager).beginTransaction();
+
+        doReturn(TEST_ADDRESS).when(mCachedDevice).getAddress();
+        doReturn(TEST_ADDRESS).when(mCachedDevice).getIdentityAddress();
+        Bundle args = new Bundle();
+        args.putString(BluetoothDeviceDetailsFragment.KEY_DEVICE_ADDRESS, TEST_ADDRESS);
+        fragment.setArguments(args);
+
+        return fragment;
     }
 }
