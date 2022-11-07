e5e232428dec8e5221c6ccf18dd338811a091a72
Adam Bookatz <bookatz@google.com>
2022-11-07 10:51:01 -0800

    Introduce concept of DockUser
    Dreams and docking often refers to "user zero" or "system user".
    But more generally, the special user for dreams or switching-to-on-docking
    need not be user 0, and in fact won't be in certain modes. So this needs updating.
    
    Test: the files mentioned in the topic
    Bug: 257333623
    Change-Id: If068aa79a770bed4abf3f7ca1859a95cd1eb4ecd
    Merged-In: If068aa79a770bed4abf3f7ca1859a95cd1eb4ecd

--- src/com/android/settings/users/UserSettings.java
+++ src/com/android/settings/users/UserSettings.java
@@ -125,7 +125,7 @@ public class UserSettings extends SettingsPreferenceFragment
     private static final String KEY_ADD_SUPERVISED_USER = "supervised_user_add";
     private static final String KEY_ADD_USER_WHEN_LOCKED = "user_settings_add_users_when_locked";
     private static final String KEY_MULTIUSER_TOP_INTRO = "multiuser_top_intro";
-    private static final String KEY_TIMEOUT_TO_USER_ZERO = "timeout_to_user_zero_preference";
+    private static final String KEY_TIMEOUT_TO_DOCK_USER = "timeout_to_dock_user_preference";
     private static final String KEY_GUEST_CATEGORY = "guest_category";
     private static final String KEY_GUEST_RESET = "guest_reset";
     private static final String KEY_GUEST_EXIT = "guest_exit";
@@ -213,7 +213,7 @@ public class UserSettings extends SettingsPreferenceFragment
     private AddUserWhenLockedPreferenceController mAddUserWhenLockedPreferenceController;
     private RemoveGuestOnExitPreferenceController mRemoveGuestOnExitPreferenceController;
     private MultiUserTopIntroPreferenceController mMultiUserTopIntroPreferenceController;
-    private TimeoutToUserZeroPreferenceController mTimeoutToUserZeroPreferenceController;
+    private TimeoutToDockUserPreferenceController mTimeoutToDockUserPreferenceController;
     private UserCreatingDialog mUserCreatingDialog;
     private final AtomicBoolean mGuestCreationScheduled = new AtomicBoolean();
     private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
@@ -306,14 +306,14 @@ public class UserSettings extends SettingsPreferenceFragment
         mMultiUserTopIntroPreferenceController = new MultiUserTopIntroPreferenceController(activity,
                 KEY_MULTIUSER_TOP_INTRO);
 
-        mTimeoutToUserZeroPreferenceController = new TimeoutToUserZeroPreferenceController(activity,
-                KEY_TIMEOUT_TO_USER_ZERO);
+        mTimeoutToDockUserPreferenceController = new TimeoutToDockUserPreferenceController(
+                activity, KEY_TIMEOUT_TO_DOCK_USER);
 
         final PreferenceScreen screen = getPreferenceScreen();
         mAddUserWhenLockedPreferenceController.displayPreference(screen);
         mRemoveGuestOnExitPreferenceController.displayPreference(screen);
         mMultiUserTopIntroPreferenceController.displayPreference(screen);
-        mTimeoutToUserZeroPreferenceController.displayPreference(screen);
+        mTimeoutToDockUserPreferenceController.displayPreference(screen);
 
         screen.findPreference(mAddUserWhenLockedPreferenceController.getPreferenceKey())
                 .setOnPreferenceChangeListener(mAddUserWhenLockedPreferenceController);
@@ -387,8 +387,8 @@ public class UserSettings extends SettingsPreferenceFragment
 
         mAddUserWhenLockedPreferenceController.updateState(screen.findPreference(
                 mAddUserWhenLockedPreferenceController.getPreferenceKey()));
-        mTimeoutToUserZeroPreferenceController.updateState(screen.findPreference(
-                mTimeoutToUserZeroPreferenceController.getPreferenceKey()));
+        mTimeoutToDockUserPreferenceController.updateState(screen.findPreference(
+                mTimeoutToDockUserPreferenceController.getPreferenceKey()));
         mRemoveGuestOnExitPreferenceController.updateState(screen.findPreference(
                 mRemoveGuestOnExitPreferenceController.getPreferenceKey()));
         if (mShouldUpdateUserList) {
