e2e753e2fc0391ccb7018774c5054f22adb7500a
Zoey Chen <zoeychen@google.com>
2022-11-10 19:53:33 +0000

    [Settings] Remove the unexisted subinfo from map
    Bug: 258250247
    Test: manual
    Change-Id: Ied5be71d6ccb251750cf9026f672aeaede860f35

--- src/com/android/settings/network/NetworkProviderSimListController.java
+++ src/com/android/settings/network/NetworkProviderSimListController.java
@@ -80,6 +80,7 @@ public class NetworkProviderSimListController extends AbstractPreferenceControll
 
     @OnLifecycleEvent(ON_PAUSE)
     public void onPause() {
+        mMobileNetworkRepository.removeRegister();
     }
 
     @Override
