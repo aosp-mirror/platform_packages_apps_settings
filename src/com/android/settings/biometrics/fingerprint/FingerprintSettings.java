c9f58d684eb0536b433c109e11fcbba31d3d61eb
lbill <lbill@google.com>
2022-11-15 12:46:30 +0000

    Fix NPE of mDismissListener in Rename Dialog
    Bug: 241122882
    Test: Enroll 1 fingerprint, and rename in landscape mode, check no
    more NPE when focus on inputfield and IME shown
    
    Change-Id: I24fe9aab11cabd67015989cfaaf6a8228ea72b59

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -1014,7 +1014,9 @@ public class FingerprintSettings extends SubSettings {
             @Override
             public void onCancel(DialogInterface dialog) {
                 super.onCancel(dialog);
-                mDismissListener.onDismiss(dialog);
+                if (mDismissListener != null) {
+                    mDismissListener.onDismiss(dialog);
+                }
             }
 
             @Override
@@ -1052,7 +1054,9 @@ public class FingerprintSettings extends SubSettings {
                                             parent.renameFingerPrint(mFp.getBiometricId(),
                                                     newName);
                                         }
-                                        mDismissListener.onDismiss(dialog);
+                                        if (mDismissListener != null) {
+                                            mDismissListener.onDismiss(dialog);
+                                        }
                                         dialog.dismiss();
                                     }
                                 })
