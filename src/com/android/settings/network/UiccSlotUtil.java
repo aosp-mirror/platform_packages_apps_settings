fe5978cde17e57ea085b2448f8ac96eba6d7ea68
SongFerng Wang <songferngwang@google.com>
2023-01-13 04:18:34 +0000

    Revert "[MEP] psim->esim, don't skip carrierConfigChanged with subId -1"
    This reverts commit 078ecbd765abca3a2a78c5af16b80796b82d3a90.
    
    Reason for revert: The in-market device have error by some unknown reasons at b/264371574. To revert it, then the settings wait the 25s, it can protect the system has enough time for enabling esim.
    
    Change-Id: Ic17380f549c764306851ad5cba6360f68df9b9b5

--- src/com/android/settings/network/UiccSlotUtil.java
+++ src/com/android/settings/network/UiccSlotUtil.java
@@ -141,8 +141,7 @@ public class UiccSlotUtil {
                         inactiveRemovableSlot,
                         /*removable sim's port Id*/ TelephonyManager.DEFAULT_PORT_INDEX,
                         excludedLogicalSlotIndex),
-                context,
-                /*isWaitingForValidSubId=*/ true);
+                context);
     }
 
     /**
@@ -180,8 +179,7 @@ public class UiccSlotUtil {
         performSwitchToSlot(telMgr,
                 prepareUiccSlotMappings(uiccSlotMappings, /*slot is not psim*/ false,
                         physicalSlotId, port, excludedLogicalSlotIndex),
-                context,
-                /*isWaitingForValidSubId=*/ false);
+                context);
     }
 
     /**
@@ -233,8 +231,7 @@ public class UiccSlotUtil {
     }
 
     private static void performSwitchToSlot(TelephonyManager telMgr,
-            Collection<UiccSlotMapping> uiccSlotMappings, Context context,
-            boolean isWaitingForValidSubId)
+            Collection<UiccSlotMapping> uiccSlotMappings, Context context)
             throws UiccSlotsException {
         CarrierConfigChangedReceiver receiver = null;
         long waitingTimeMillis =
@@ -244,7 +241,7 @@ public class UiccSlotUtil {
                         DEFAULT_WAIT_AFTER_SWITCH_TIMEOUT_MILLIS);
         try {
             CountDownLatch latch = new CountDownLatch(1);
-            receiver = new CarrierConfigChangedReceiver(latch, isWaitingForValidSubId);
+            receiver = new CarrierConfigChangedReceiver(latch);
             receiver.registerOn(context);
             telMgr.setSimSlotMapping(uiccSlotMappings);
             latch.await(waitingTimeMillis, TimeUnit.MILLISECONDS);
