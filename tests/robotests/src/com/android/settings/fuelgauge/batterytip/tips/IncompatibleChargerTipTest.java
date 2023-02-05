c6d34d6c583ccec729dd01b6c0e82e0df2233b6a
ykhung <ykhung@google.com>
2023-02-05 20:17:00 +0800

    Add incompatible charging battery tip UI and card skeleton
    Bug: 263193978
    Bug: 246960554
    Test: make -j64 RunSettingsRoboTests ROBOTEST_FILTER="com.android.settings.fuelgauge.batterytip"
    Change-Id: I8180022bd39f66c88463a2552c6050669bb0357c

---
+++ tests/robotests/src/com/android/settings/fuelgauge/batterytip/tips/IncompatibleChargerTipTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.fuelgauge.batterytip.tips;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.app.settings.SettingsEnums;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.testutils.FakeFeatureFactory;
+import com.android.settings.widget.CardPreference;
+import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowLog;
+
+@RunWith(RobolectricTestRunner.class)
+public final class IncompatibleChargerTipTest {
+
+    private Context mContext;
+    private FakeFeatureFactory mFeatureFactory;
+    private IncompatibleChargerTip mIncompatibleChargerTip;
+    private MetricsFeatureProvider mMetricsFeatureProvider;
+
+    @Mock private BatteryTip mBatteryTip;
+    @Mock private Preference mPreference;
+    @Mock private CardPreference mCardPreference;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mFeatureFactory = FakeFeatureFactory.setupForTest();
+        mMetricsFeatureProvider = mFeatureFactory.metricsFeatureProvider;
+        mContext = RuntimeEnvironment.application;
+        mIncompatibleChargerTip = new IncompatibleChargerTip(BatteryTip.StateType.NEW);
+
+        when(mPreference.getContext()).thenReturn(mContext);
+        when(mCardPreference.getContext()).thenReturn(mContext);
+    }
+
+    @Test
+    public void getTitle_showTitle() {
+        assertThat(mIncompatibleChargerTip.getTitle(mContext)).isEqualTo(
+                mContext.getString(R.string.battery_tip_incompatible_charging_title));
+    }
+
+    @Test
+    public void getSummary_showSummary() {
+        assertThat(mIncompatibleChargerTip.getSummary(mContext)).isEqualTo(
+                mContext.getString(R.string.battery_tip_incompatible_charging_message));
+    }
+
+    @Test
+    public void getIcon_showIcon() {
+        assertThat(mIncompatibleChargerTip.getIconId())
+                .isEqualTo(R.drawable.ic_battery_charging);
+    }
+
+    @Test
+    public void testLog_logMetric() {
+        mIncompatibleChargerTip.updateState(mBatteryTip);
+        mIncompatibleChargerTip.log(mContext, mMetricsFeatureProvider);
+
+        verify(mMetricsFeatureProvider).action(mContext,
+                SettingsEnums.ACTION_INCOMPATIBLE_CHARGING_TIP, mBatteryTip.mState);
+    }
+
+    @Test
+    public void updatePreference_castFail_logErrorMessage() {
+        mIncompatibleChargerTip.updatePreference(mPreference);
+        assertThat(getLastErrorLog()).isEqualTo("cast Preference to CardPreference failed");
+    }
+
+    @Test
+    public void updatePreference_shouldSetSecondaryButtonText() {
+        String expected = mContext.getString(R.string.learn_more);
+
+        mIncompatibleChargerTip.updatePreference(mCardPreference);
+
+        verify(mCardPreference).setSecondaryButtonText(expected);
+    }
+
+    @Test
+    public void updatePreference_shouldSetSecondaryButtonVisible() {
+        mIncompatibleChargerTip.updatePreference(mCardPreference);
+        verify(mCardPreference).setSecondaryButtonVisible(true);
+    }
+
+    private String getLastErrorLog() {
+        return ShadowLog.getLogsForTag(IncompatibleChargerTip.class.getSimpleName()).stream()
+                .filter(log -> log.type == Log.ERROR)
+                .reduce((first, second) -> second)
+                .orElse(createErrorLog("No Error Log"))
+                .msg;
+    }
+
+    private ShadowLog.LogItem createErrorLog(String msg) {
+        return new ShadowLog.LogItem(Log.ERROR, "tag", msg, null);
+    }
+}
