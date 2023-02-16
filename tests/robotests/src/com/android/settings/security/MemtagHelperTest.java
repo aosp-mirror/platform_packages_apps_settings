9d99ab422aee489a67a940d07c839766385194a7
Florian Mayer <fmayer@google.com>
2023-02-15 16:22:11 -0800

    Disable MTE developer option if device config overrides it
    On the backend, the device config would already override the setting by
    the UI.
    
    (cherry picked from commit 65116faa29df0537531f817973e599b9a0eeee99)
    
    Bug: 269523554
    Merged-In: Idd249e060a275b58a399349f202fc68b42964eb0
    Change-Id: Idd249e060a275b58a399349f202fc68b42964eb0

--- tests/robotests/src/com/android/settings/security/MemtagHelperTest.java
+++ tests/robotests/src/com/android/settings/security/MemtagHelperTest.java
@@ -176,4 +176,16 @@ public class MemtagHelperTest {
         ShadowSystemProperties.override(mMemtagProperty, "memtag");
         assertThat(MemtagHelper.getSummary()).isEqualTo(R.string.memtag_force_on);
     }
+
+    @Test
+    public void isForcedOn_forceOnOverride_isTrue() {
+        ShadowSystemProperties.override(mDeviceConfigOverride, "force_on");
+        assertThat(MemtagHelper.isForcedOn()).isTrue();
+    }
+
+    @Test
+    public void isForcedOff_forceOffOverride_isTrue() {
+        ShadowSystemProperties.override(mDeviceConfigOverride, "force_off");
+        assertThat(MemtagHelper.isForcedOff()).isTrue();
+    }
 }
