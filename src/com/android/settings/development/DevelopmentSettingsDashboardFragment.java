f9616e0e5ade9347155bf1ae23da948ad621db3b
Tiger <tigerhuang@google.com>
2023-02-21 23:15:45 +0800

    Add an developer option to enable config_navBarDefaultTransparent
    This enables the user to make navigation bar color transparent by
    default. It will only affect apps which don't specify the navigation
    bar color.
    
    Fix: 232195501
    Test: atest TransparentNavigationBarPreferenceControllerTest
    Change-Id: I91581558b92abeaa3501b9749f1a1ab43db8e771

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -616,6 +616,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new RtlLayoutPreferenceController(context));
         controllers.add(new WindowAnimationScalePreferenceController(context));
         controllers.add(new EmulateDisplayCutoutPreferenceController(context));
+        controllers.add(new TransparentNavigationBarPreferenceController(context));
         controllers.add(new TransitionAnimationScalePreferenceController(context));
         controllers.add(new AnimatorDurationScalePreferenceController(context));
         controllers.add(new SecondaryDisplayPreferenceController(context));
