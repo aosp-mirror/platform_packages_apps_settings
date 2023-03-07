11ac41d9b6eddc4733e782984c312fd482a616f3
Julia Reynolds <juliacr@google.com>
2023-03-07 15:44:29 -0500

    Don't show NLSes with excessively long component names
    Test: install test app with long CN
    Test: ServiceListingTest
    Bug: 260570119
    Change-Id: I3ffd02f6cf6bf282e7fc264fd070ed3add4d8571
    Merged-In: I3ffd02f6cf6bf282e7fc264fd070ed3add4d8571

--- src/com/android/settings/notification/NotificationAccessSettings.java
+++ src/com/android/settings/notification/NotificationAccessSettings.java
@@ -57,6 +57,9 @@ import java.util.List;
 @SearchIndexable
 public class NotificationAccessSettings extends EmptyTextSettings {
     private static final String TAG = "NotifAccessSettings";
+
+    private static final int MAX_CN_LENGTH = 500;
+
     private static final ManagedServiceSettings.Config CONFIG =
             new ManagedServiceSettings.Config.Builder()
                     .setTag(TAG)
@@ -91,6 +94,12 @@ public class NotificationAccessSettings extends EmptyTextSettings {
                 .setNoun(CONFIG.noun)
                 .setSetting(CONFIG.setting)
                 .setTag(CONFIG.tag)
+                .setValidator(info -> {
+                    if (info.getComponentName().flattenToString().length() > MAX_CN_LENGTH) {
+                        return false;
+                    }
+                    return true;
+                })
                 .build();
         mServiceListing.addCallback(this::updateList);
         setPreferenceScreen(getPreferenceManager().createPreferenceScreen(mContext));
