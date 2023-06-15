0b51456ac1c96320549f2a53797537c7e7f4716a
Matías Hernández <matiashe@google.com>
2023-06-15 18:37:52 +0200

    Settings: don't try to allow NLSes with too-long component names
    * NotificationAccessConfirmationActivity (triggered through CompanionDeviceManager) -> Don't show the dialog, bail out early similarly to other invalid inputs.
    * NotificationAccessSettings (from Special App Access) -> No changes, but use the canonical constant now.
    * NotificationAccessDetails -> Disable the toggle, unless the NLS was previously approved (in which case it can still be removed).
    
    Fixes: 260570119
    Fixes: 286043036
    Test: atest + manually
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:f1af8c529295c60e516b2cc1d1ac812b8d6047ac)
    Merged-In: Ifc048311746c027e3683cdcf65f1079d04cf7c56
    Change-Id: Ifc048311746c027e3683cdcf65f1079d04cf7c56

--- src/com/android/settings/notification/NotificationAccessSettings.java
+++ src/com/android/settings/notification/NotificationAccessSettings.java
@@ -58,8 +58,6 @@ import java.util.List;
 public class NotificationAccessSettings extends EmptyTextSettings {
     private static final String TAG = "NotifAccessSettings";
 
-    private static final int MAX_CN_LENGTH = 500;
-
     private static final ManagedServiceSettings.Config CONFIG =
             new ManagedServiceSettings.Config.Builder()
                     .setTag(TAG)
@@ -135,7 +133,8 @@ public class NotificationAccessSettings extends EmptyTextSettings {
         for (ServiceInfo service : services) {
             final ComponentName cn = new ComponentName(service.packageName, service.name);
             boolean isAllowed = mNm.isNotificationListenerAccessGranted(cn);
-            if (!isAllowed && cn.flattenToString().length() > MAX_CN_LENGTH) {
+            if (!isAllowed && cn.flattenToString().length()
+                    > NotificationManager.MAX_SERVICE_COMPONENT_NAME_LENGTH) {
                 continue;
             }
 
