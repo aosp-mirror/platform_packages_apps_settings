667341c343f76e498e8a7bcc670acc60bda93742
Wu Ahan <ahanwu@google.com>
2023-09-11 04:54:42 +0000

    Reland sfps enroll improvement feature with feature provider
    Prior cl, ag/24720067, was reverted due to platinum failed,
    this cl fixes the fails (NPE on UDFPS).
    
    Bug: 288155127
    Bug: 305132251
    Test: Manually check on Lynx and Felix
    Test: atest BiometricsE2eTests:FingerprintEnrollSuccessTest
    Test: abtd for platinum, see b/305048300#comment14
    Change-Id: I90f4ea14853edf03abd8ffe0b7874894eb2d4f81

--- tests/unit/src/com/android/settings/testutils/FakeFeatureFactory.java
+++ tests/unit/src/com/android/settings/testutils/FakeFeatureFactory.java
@@ -24,6 +24,7 @@ import com.android.settings.accessibility.AccessibilitySearchFeatureProvider;
 import com.android.settings.accounts.AccountFeatureProvider;
 import com.android.settings.applications.ApplicationFeatureProvider;
 import com.android.settings.biometrics.face.FaceFeatureProvider;
+import com.android.settings.biometrics.fingerprint.FingerprintFeatureProvider;
 import com.android.settings.biometrics2.factory.BiometricsRepositoryProvider;
 import com.android.settings.bluetooth.BluetoothFeatureProvider;
 import com.android.settings.connecteddevice.fastpair.FastPairFeatureProvider;
@@ -79,6 +80,7 @@ public class FakeFeatureFactory extends FeatureFactory {
     public final AccountFeatureProvider mAccountFeatureProvider;
     public final BluetoothFeatureProvider mBluetoothFeatureProvider;
     public final FaceFeatureProvider mFaceFeatureProvider;
+    public final FingerprintFeatureProvider mFingerprintFeatureProvider;
     public final BiometricsRepositoryProvider mBiometricsRepositoryProvider;
 
     public PanelFeatureProvider panelFeatureProvider;
@@ -131,6 +133,7 @@ public class FakeFeatureFactory extends FeatureFactory {
         panelFeatureProvider = mock(PanelFeatureProvider.class);
         mBluetoothFeatureProvider = mock(BluetoothFeatureProvider.class);
         mFaceFeatureProvider = mock(FaceFeatureProvider.class);
+        mFingerprintFeatureProvider = mock(FingerprintFeatureProvider.class);
         mBiometricsRepositoryProvider = mock(BiometricsRepositoryProvider.class);
         wifiTrackerLibProvider = mock(WifiTrackerLibProvider.class);
         securitySettingsFeatureProvider = mock(SecuritySettingsFeatureProvider.class);
@@ -253,6 +256,11 @@ public class FakeFeatureFactory extends FeatureFactory {
     @Override
     public FaceFeatureProvider getFaceFeatureProvider() {
         return mFaceFeatureProvider;
+    }
+
+    @Override
+    public FingerprintFeatureProvider getFingerprintFeatureProvider() {
+        return mFingerprintFeatureProvider;
     }
 
     @Override
