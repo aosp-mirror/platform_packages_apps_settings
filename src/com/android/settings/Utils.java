775280aa2769ba89bdbad4d9b42bc24206ff617d
marcusge <marcusge@google.com>
2023-09-14 18:16:26 +0000

    Update Settings homepage color role mappings
    doc: go/settings-color-roles-remapping-result
    Test: local raven device
    Bug: 299907492
    
    Change-Id: I8ba90ec61970fa2aa56f9d5d4029c22c74940120

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1277,7 +1277,8 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     @ColorInt
     public static int getHomepageIconColor(Context context) {
-        return getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
+        return getColorAttrDefaultColor(
+                context, com.android.internal.R.attr.materialColorOnSurface);
     }
 
     /**
