ea1685ac28cd39161e08c3247c2e077a07e6215a
Weng Su <wengsu@google.com>
2024-01-26 17:59:40 +0800

    [RESTRICT AUTOMERGE] Restrict WifiDialogActivity
    - Don't show WifiDialogActivity if user has DISALLOW_ADD_WIFI_CONFIG
    
    Bug: 299931761
    Flag: None
    Test: manual test with TestDPC
    atest -c SettingsRoboTests:WifiDialogActivityTest
    
    (cherry picked from commit 51fa3d798ad0397122bbb2143bc24efe1a705be9)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:aabb15de74d4d24a81ed1f5b7789fe0a8407b256)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:991116ede19426e27ff80a4403996171e5c708f8)
    Merged-In: Icbb8f45922ded163208976be9c2816060dcf09f1
    Change-Id: Icbb8f45922ded163208976be9c2816060dcf09f1

--- src/com/android/settings/wifi/WifiDialogActivity.java
+++ src/com/android/settings/wifi/WifiDialogActivity.java
@@ -17,6 +17,7 @@
 package com.android.settings.wifi;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.os.UserManager.DISALLOW_ADD_WIFI_CONFIG;
 import static android.os.UserManager.DISALLOW_CONFIG_WIFI;
 
 import android.app.KeyguardManager;
@@ -122,7 +123,7 @@ public class WifiDialogActivity extends ObservableActivity implements WifiDialog
         }
 
         super.onCreate(savedInstanceState);
-        if (!isConfigWifiAllowed()) {
+        if (!isConfigWifiAllowed() || !isAddWifiConfigAllowed()) {
             finish();
             return;
         }
@@ -391,6 +392,16 @@ public class WifiDialogActivity extends ObservableActivity implements WifiDialog
                     "The user is not allowed to configure Wi-Fi.");
         }
         return isConfigWifiAllowed;
+    }
+
+    @VisibleForTesting
+    boolean isAddWifiConfigAllowed() {
+        UserManager userManager = getSystemService(UserManager.class);
+        if (userManager != null && userManager.hasUserRestriction(DISALLOW_ADD_WIFI_CONFIG)) {
+            Log.e(TAG, "The user is not allowed to add Wi-Fi configuration.");
+            return false;
+        }
+        return true;
     }
 
     private boolean hasWifiManager() {
