c6eba747db60b044999e29bbf96643eb41799c3c
SongFerngWang <songferngwang@google.com>
2024-01-31 18:58:54 +0800

    [Sim UI enhancement] remove the "Tap to show info"
    Bug: 318310357
    Bug: 298898436
    Bug: 298891941
    
    Test: atest PhoneNumberPreferenceControllerTest
    atest ImeiInfoPreferenceControllerTest
    atest SimEidPreferenceControllerTest
    
    Change-Id: Iadd82173b55b8f66409c1ff056dd4c323d6dcc35

--- src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceController.kt
+++ src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceController.kt
@@ -92,6 +92,7 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
             }
             preference.title = title
             preference.dialogTitle = title
+            preference.summary = eid
             updateDialog()
         }
     }
@@ -130,9 +131,6 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
 
         val qrCodeView = dialog.requireViewById<ImageView>(R.id.esim_id_qrcode)
         qrCodeView.setImageBitmap(getEidQrCode(eid))
-
-        // After "Tap to show", eid is displayed on preference.
-        preference.summary = textView.text
     }
 
     override fun handlePreferenceTreeClick(preference: Preference): Boolean {
