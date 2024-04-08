844d2f4c67c0b258c70cf59a7c9eabe3b512024f
Song Chun Fan <schfan@google.com>
2024-04-08 18:16:11 +0000

    [Settings/Archive] remove all checks for the archiving system property
    No longer needed
    
    BUG: 331165939
    Test: builds
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:6ae958abd26ed77ecd4d3fb1720bb912553da2ea)
    Merged-In: I24bc9a3d7503da1606d29eb3dfa963b0e6bf9d3e
    Change-Id: I24bc9a3d7503da1606d29eb3dfa963b0e6bf9d3e

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -183,5 +183,4 @@ private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
 }
 
 fun isArchivingEnabled(featureFlags: PmFeatureFlags) =
-        featureFlags.archiving() || SystemProperties.getBoolean("pm.archiving.enabled", false)
-                || Flags.appArchiving()
+        featureFlags.archiving() || Flags.appArchiving()
