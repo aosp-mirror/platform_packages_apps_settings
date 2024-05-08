f0e6f12f10b28b9e3a51cded42849604d04de5ad
Ang Li <ihcinihsdk@google.com>
2024-05-08 20:30:58 +0000

    Adapt to the new API of `ShadowCaptioningManager`.
    Test: Presubmit.
    Change-Id: If90894a2438ff5e0aec8e727036f7a656f4c712a

--- tests/robotests/src/com/android/settings/accessibility/CaptioningWindowOpacityControllerTest.java
+++ tests/robotests/src/com/android/settings/accessibility/CaptioningWindowOpacityControllerTest.java
@@ -102,7 +102,8 @@ public class CaptioningWindowOpacityControllerTest {
 
     @Test
     public void onValueChanged_shouldSetCaptionEnabled() {
-        mShadowCaptioningManager.setEnabled(false);
+        Settings.Secure.putInt(
+            mContext.getContentResolver(), Settings.Secure.ACCESSIBILITY_CAPTIONING_ENABLED, OFF);
         mController.displayPreference(mScreen);
 
         mController.onValueChanged(mPreference, 0x80FFFFFF);
