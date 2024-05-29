82bd2c75f7a658a9ed034bc3697720279c079da0
Edgar Wang <edgarwang@google.com>
2024-05-29 11:46:46 +0000

    Fix NPE in date time settings
    - the callback is null when slice call setChecked() of PreferenceController
    
    Bug: 343109816
    Bug: 334924107
    Test: robotest & manual
    Flag: EXEMPT bug fix
    Change-Id: Ia4d60455b6f14a46d00ae285017bc96ad4855679

--- src/com/android/settings/datetime/AutoTimePreferenceController.java
+++ src/com/android/settings/datetime/AutoTimePreferenceController.java
@@ -39,6 +39,11 @@ public class AutoTimePreferenceController extends TogglePreferenceController {
     public AutoTimePreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
         mTimeManager = context.getSystemService(TimeManager.class);
+        // This is a no-op implementation of UpdateTimeAndDateCallback to avoid a NPE when
+        // setTimeAndDateCallback() isn't called, e.g. for slices and other cases where the
+        // controller is instantiated outside of the context of the real Date & Time settings
+        // screen.
+        mCallback  = (c) -> {};
     }
 
     public void setDateAndTimeCallback(UpdateTimeAndDateCallback callback) {
