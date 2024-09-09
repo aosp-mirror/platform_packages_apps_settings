994bff003380014fbc06f1fe410ae20428b5e1ba
Pechetty Sravani (xWF) <pechetty@google.com>
2024-09-09 04:06:57 +0000

    Revert "Customize Fingerprint enroll activities"
    Revert submission 29139483-494-trampoline
    
    Reason for revert: DroidMonitor created revert due to b/365453068.
    
    Reverted changes: /q/submissionid:29139483-494-trampoline
    
    Change-Id: Ie2ab22694e7128e10eb11bf29340023e2bfd1343

--- src/com/android/settings/biometrics/fingerprint/FingerprintFeatureProvider.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintFeatureProvider.java
@@ -33,6 +33,7 @@ public interface FingerprintFeatureProvider {
      */
     SfpsEnrollmentFeature getSfpsEnrollmentFeature();
 
+
     /**
      * Gets calibrator for udfps pre-enroll
      * @param appContext application context
@@ -51,13 +52,4 @@ public interface FingerprintFeatureProvider {
      * @return the feature implementation
      */
     SfpsRestToUnlockFeature getSfpsRestToUnlockFeature(@NonNull Context context);
-
-    /**
-     * Gets the provider for current fingerprint enrollment activity classes
-     * @return the provider
-     */
-    @NonNull
-    default FingerprintEnrollActivityClassProvider getEnrollActivityClassProvider() {
-        return FingerprintEnrollActivityClassProvider.getInstance();
-    }
 }
