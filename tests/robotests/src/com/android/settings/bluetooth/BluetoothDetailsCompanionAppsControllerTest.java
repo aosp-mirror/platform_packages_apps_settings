66d518d66676ba1d39c31fde9b8d7f1fd1e449b9
Evan Chen <evanxinchen@google.com>
2024-10-03 23:05:17 +0000

    Add deviceId in AssociaitonInfo
    Test: cts
    Bug: 330793944
    Flag: EXEMPT trivial bugfix
    Change-Id: Ib6860f8950f022689ae0c9ef6b4b6794e727c8f9

--- tests/robotests/src/com/android/settings/bluetooth/BluetoothDetailsCompanionAppsControllerTest.java
+++ tests/robotests/src/com/android/settings/bluetooth/BluetoothDetailsCompanionAppsControllerTest.java
@@ -104,7 +104,6 @@ public class BluetoothDetailsCompanionAppsControllerTest extends
                 associationId,
                 /* userId */ 0,
                 packageName,
-                /* tag */ null,
                 MacAddress.fromString(mCachedDevice.getAddress()),
                 /* displayName */ null,
                 /* deviceProfile */ "",
@@ -116,7 +115,8 @@ public class BluetoothDetailsCompanionAppsControllerTest extends
                 /* timeApprovedMs */ System.currentTimeMillis(),
                 /* lastTimeConnected */ Long.MAX_VALUE,
                 /* systemDataSyncFlags */ -1,
-                /* deviceIcon */ null);
+                /* deviceIcon */ null,
+                /* deviceId */ null);
 
         mAssociations.add(association);
         showScreen(mController);
