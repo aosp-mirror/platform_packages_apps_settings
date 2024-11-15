e635f1e0ed4e3c1b0e2b27c122a4a4d1b96594d1
Jacky Wang <jiannan@google.com>
2024-11-15 08:14:51 +0800

    [Catalyst] Support Getter API for settings service
    NO_IFTTT=Catalyst only
    
    Bug: 373895596
    Flag: com.android.settings.flags.catalyst_service
    Test: devtool
    Change-Id: I367f350d2192d1285bf355a9e45c2d9bec11fa44

--- src/com/android/settings/display/PeakRefreshRateSwitchPreference.kt
+++ src/com/android/settings/display/PeakRefreshRateSwitchPreference.kt
@@ -49,6 +49,9 @@ class PeakRefreshRateSwitchPreference :
     override fun storage(context: Context): KeyValueStore =
         PeakRefreshRateStore(context, SettingsSystemStore.get(context))
 
+    override fun getReadPermit(context: Context, myUid: Int, callingUid: Int) =
+        ReadWritePermit.ALLOW
+
     override fun getWritePermit(context: Context, value: Boolean?, myUid: Int, callingUid: Int) =
         ReadWritePermit.ALLOW
 
