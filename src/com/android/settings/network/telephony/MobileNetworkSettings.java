63469da6f6a257aa653991ae0416fb096bc443b9
tomhsu <tomhsu@google.com>
2024-12-06 10:24:25 +0000

    Remove Satellite SOS entry from AOSP
    Flag: com.android.settings.flags.satellite_oem_settings_ux_migration
    Bug: b/378408877
    Test: make pass
    Test: Manual test
    Change-Id: If47081be0ed0734be841b2fe9129d22a76ed8e1b

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -288,11 +288,7 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings impleme
         if (satelliteSettingPreferenceController != null) {
             satelliteSettingPreferenceController.init(mSubId);
         }
-        final SatelliteSettingSosPreferenceController satelliteSettingSosPreferenceController = use(
-                SatelliteSettingSosPreferenceController.class);
-        if (satelliteSettingSosPreferenceController != null) {
-            satelliteSettingSosPreferenceController.init(mSubId);
-        }
+
         use(ApnPreferenceController.class).init(mSubId);
         use(CarrierPreferenceController.class).init(mSubId);
         use(DataUsagePreferenceController.class).init(mSubId);
