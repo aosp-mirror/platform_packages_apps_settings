74f0acd92bd287e170545db34845858d75d2b4ae
Rupesh Bansal <brup@google.com>
2024-12-09 05:21:02 +0000

    Changing eventFlags to eventFilter
    This is to address the API council feedback
    
    Bug: 379313664
    Flag: EXEMPT minor change
    Test: EXEMPT minor change
    Change-Id: I9f9126b2e9eae3311f9d03d13d201d7518a27a09

--- src/com/android/settings/display/BrightnessLevelPreferenceController.java
+++ src/com/android/settings/display/BrightnessLevelPreferenceController.java
@@ -134,7 +134,7 @@ public class BrightnessLevelPreferenceController extends BasePreferenceControlle
     public void onStart() {
         mContentResolver.registerContentObserver(BRIGHTNESS_ADJ_URI, false, mBrightnessObserver);
         mDisplayManager.registerDisplayListener(mDisplayListener, mHandler, /* eventFlags= */ 0,
-                DisplayManager.PRIVATE_EVENT_FLAG_DISPLAY_BRIGHTNESS);
+                DisplayManager.PRIVATE_EVENT_TYPE_DISPLAY_BRIGHTNESS);
         updatedSummary(mPreference);
     }
 
