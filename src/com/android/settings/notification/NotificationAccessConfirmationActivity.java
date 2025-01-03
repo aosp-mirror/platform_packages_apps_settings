e0510a62eba04140e459cdd8232f35087aa3073b
Matías Hernández <matiashe@google.com>
2025-01-03 13:38:10 +0100

    Remove incorrect call to Window.addFlags with a system flag
    The same flag is being (correctly) added in onCreate, so on top of being wrong this was also completely unnecessary.
    
    Bug: 368579654
    Test: atest NotificationAccessConfirmationActivityTest
    Flag: EXEMPT bugfix
    
    Change-Id: I539fca77a1d136bdd0e4892993020884430b8a4e

--- src/com/android/settings/notification/NotificationAccessConfirmationActivity.java
+++ src/com/android/settings/notification/NotificationAccessConfirmationActivity.java
@@ -41,7 +41,6 @@ import android.os.UserManager;
 import android.service.notification.NotificationListenerService;
 import android.text.TextUtils;
 import android.util.Slog;
-import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
 
@@ -159,20 +158,6 @@ public class NotificationAccessConfirmationActivity extends Activity
         // Consistent with the permission dialog
         // Used instead of p.mCancelable as that is only honored for AlertDialog
         getWindow().setCloseOnTouchOutside(false); 
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        getWindow().addFlags(
-                WindowManager.LayoutParams.SYSTEM_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
-    }
-
-    @Override
-    public void onPause() {
-        getWindow().clearFlags(
-                WindowManager.LayoutParams.SYSTEM_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
-        super.onPause();
     }
 
     private void onAllow() {
